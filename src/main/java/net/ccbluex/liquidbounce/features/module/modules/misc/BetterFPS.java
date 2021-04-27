2215df414545366886b34dc55851484d2617458d
Timeline <53483352+2933968621@users.noreply.github.com>
2021-04-27 12:46:54 +0800

    = =

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/BetterFPS.java
@@ -0,0 +1,15 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/Project-EZ4H/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.misc;
+
+import net.ccbluex.liquidbounce.features.module.Module;
+import net.ccbluex.liquidbounce.features.module.ModuleCategory;
+import net.ccbluex.liquidbounce.features.module.ModuleInfo;
+
+@ModuleInfo(name = "BetterFPS", description = "fps++.", category = ModuleCategory.MISC)
+public class BetterFPS extends Module {
+  
+}
