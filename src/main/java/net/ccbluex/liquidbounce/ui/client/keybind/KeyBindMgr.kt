c25ddc83976635ceb74c2d2dc521dd84d31a06be
liulihaocai <liulihaocai@outlook.com>
2021-04-28 01:16:31 +0800

    i forgot

--- src/main/java/net/ccbluex/liquidbounce/ui/client/keybind/KeyBindMgr.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/keybind/KeyBindMgr.kt
@@ -12,7 +12,7 @@ class KeyBindMgr : GuiScreen() {
 
     init {
         //line1
-        keys.add(KeyInfo(12F,12F,27F,32F,Keyboard.KEY_L/*IDK*/,"`"))
+        keys.add(KeyInfo(12F,12F,27F,32F,Keyboard.KEY_GRAVE,"`"))
         keys.add(KeyInfo(12F+32F*1,12F,27F,32F,Keyboard.KEY_1,"1"))
         keys.add(KeyInfo(12F+32F*2,12F,27F,32F,Keyboard.KEY_2,"2"))
         keys.add(KeyInfo(12F+32F*3,12F,27F,32F,Keyboard.KEY_3,"3"))
@@ -25,7 +25,7 @@ class KeyBindMgr : GuiScreen() {
         keys.add(KeyInfo(12F+32F*10,12F,27F,32F,Keyboard.KEY_0,"0"))
         keys.add(KeyInfo(12F+32F*11,12F,27F,32F,Keyboard.KEY_MINUS,"-"))
         keys.add(KeyInfo(12F+32F*12,12F,27F,32F,Keyboard.KEY_EQUALS,"="))
-        keys.add(KeyInfo(12F+32F*13,12F,27F+32F,32F,Keyboard.KEY_BACKSLASH,"Backspace"))
+        keys.add(KeyInfo(12F+32F*13,12F,27F+32F,32F,Keyboard.KEY_BACK,"Backspace"))
         //line2
         keys.add(KeyInfo(12F,12F+37F*1,32F*1.5F-5F,32F,Keyboard.KEY_TAB,"Tab"))
         keys.add(KeyInfo(12F+32F*1.5F,12F+37F*1,27F,32F,Keyboard.KEY_Q,"Q"))
@@ -38,9 +38,9 @@ class KeyBindMgr : GuiScreen() {
         keys.add(KeyInfo(12F+32F*8.5F,12F+37F*1,27F,32F,Keyboard.KEY_I,"I"))
         keys.add(KeyInfo(12F+32F*9.5F,12F+37F*1,27F,32F,Keyboard.KEY_O,"O"))
         keys.add(KeyInfo(12F+32F*10.5F,12F+37F*1,27F,32F,Keyboard.KEY_P,"P"))
-        keys.add(KeyInfo(12F+32F*11.5F,12F+37F*1,27F,32F,Keyboard.KEY_L/*IDK*/,"["))
-        keys.add(KeyInfo(12F+32F*12.5F,12F+37F*1,27F,32F,Keyboard.KEY_L/*IDK*/,"]"))
-        keys.add(KeyInfo(12F+32F*13.5F,12F+37F*1,32F*1.5F-5F,32F,Keyboard.KEY_L/*IDK*/,"\\"))
+        keys.add(KeyInfo(12F+32F*11.5F,12F+37F*1,27F,32F,Keyboard.KEY_LBRACKET,"["))
+        keys.add(KeyInfo(12F+32F*12.5F,12F+37F*1,27F,32F,Keyboard.KEY_RBRACKET,"]"))
+        keys.add(KeyInfo(12F+32F*13.5F,12F+37F*1,32F*1.5F-5F,32F,Keyboard.KEY_BACKSLASH,"\\"))
         //line3
         keys.add(KeyInfo(12F,12F+37F*2,32F*2F-5F,32F,Keyboard.KEY_TAB,"Caps Lock"))
         keys.add(KeyInfo(12F+32F*2F,12F+37F*2,27F,32F,Keyboard.KEY_A,"A"))
@@ -52,11 +52,11 @@ class KeyBindMgr : GuiScreen() {
         keys.add(KeyInfo(12F+32F*8F,12F+37F*2,27F,32F,Keyboard.KEY_J,"J"))
         keys.add(KeyInfo(12F+32F*9F,12F+37F*2,27F,32F,Keyboard.KEY_K,"K"))
         keys.add(KeyInfo(12F+32F*10F,12F+37F*2,27F,32F,Keyboard.KEY_L,"L"))
-        keys.add(KeyInfo(12F+32F*11F,12F+37F*2,27F,32F,Keyboard.KEY_L/*IDK*/,";"))
-        keys.add(KeyInfo(12F+32F*12F,12F+37F*2,27F,32F,Keyboard.KEY_L/*IDK*/,"'"))
-        keys.add(KeyInfo(12F+32F*13F,12F+37F*2,27F+32F,32F,Keyboard.KEY_L/*IDK*/,"Enter"))
+        keys.add(KeyInfo(12F+32F*11F,12F+37F*2,27F,32F,Keyboard.KEY_SEMICOLON,";"))
+        keys.add(KeyInfo(12F+32F*12F,12F+37F*2,27F,32F,Keyboard.KEY_APOSTROPHE,"'"))
+        keys.add(KeyInfo(12F+32F*13F,12F+37F*2,27F+32F,32F,Keyboard.KEY_RETURN,"Enter"))
         //line4
-        keys.add(KeyInfo(12F,12F+37F*3,32F*2.5F-5F,32F,Keyboard.KEY_L/*IDK*/,"Shift","LShift"))
+        keys.add(KeyInfo(12F,12F+37F*3,32F*2.5F-5F,32F,Keyboard.KEY_LSHIFT,"Shift","LShift"))
         keys.add(KeyInfo(12F+32F*2.5F,12F+37F*3,27F,32F,Keyboard.KEY_Z,"Z"))
         keys.add(KeyInfo(12F+32F*3.5F,12F+37F*3,27F,32F,Keyboard.KEY_Z,"X"))
         keys.add(KeyInfo(12F+32F*4.5F,12F+37F*3,27F,32F,Keyboard.KEY_Z,"C"))
@@ -65,16 +65,16 @@ class KeyBindMgr : GuiScreen() {
         keys.add(KeyInfo(12F+32F*7.5F,12F+37F*3,27F,32F,Keyboard.KEY_Z,"N"))
         keys.add(KeyInfo(12F+32F*8.5F,12F+37F*3,27F,32F,Keyboard.KEY_Z,"M"))
         keys.add(KeyInfo(12F+32F*9.5F,12F+37F*3,27F,32F,Keyboard.KEY_Z,","))
-        keys.add(KeyInfo(12F+32F*10.5F,12F+37F*3,27F,32F,Keyboard.KEY_Z,"."))
-        keys.add(KeyInfo(12F+32F*11.5F,12F+37F*3,27F,32F,Keyboard.KEY_Z,"/"))
-        keys.add(KeyInfo(12F+32F*12.5F,12F+37F*3,32F*2.5F-5F,32F,Keyboard.KEY_L/*IDK*/,"Shift","RShift"))
+        keys.add(KeyInfo(12F+32F*10.5F,12F+37F*3,27F,32F,Keyboard.KEY_PERIOD,"."))
+        keys.add(KeyInfo(12F+32F*11.5F,12F+37F*3,27F,32F,Keyboard.KEY_SLASH,"/"))
+        keys.add(KeyInfo(12F+32F*12.5F,12F+37F*3,32F*2.5F-5F,32F,Keyboard.KEY_RSHIFT,"Shift","RShift"))
         //line5
-        keys.add(KeyInfo(12F,12F+37F*4,32F*1.5F-5F,32F,Keyboard.KEY_L/*IDK*/,"Ctrl","LCtrl"))
-        keys.add(KeyInfo(12F+32F*1.5F,12F+37F*4,32F*1.5F-5F,32F,Keyboard.KEY_L/*IDK*/,"Alt","LAlt"))
+        keys.add(KeyInfo(12F,12F+37F*4,32F*1.5F-5F,32F,Keyboard.KEY_LCONTROL,"Ctrl","LCtrl"))
+        keys.add(KeyInfo(12F+32F*1.5F,12F+37F*4,32F*1.5F-5F,32F,Keyboard.KEY_LMENU,"Alt","LAlt"))
         keys.add(KeyInfo(12F+32F*3F,12F+37F*4,32*8F-5F,32F,Keyboard.KEY_SPACE," ","Space"))
-        keys.add(KeyInfo(12F+32F*11F,12F+37F*4,32F*1.5F-5F,32F,Keyboard.KEY_L/*IDK*/,"Alt","RAlt"))
+        keys.add(KeyInfo(12F+32F*11F,12F+37F*4,32F*1.5F-5F,32F,Keyboard.KEY_RMENU,"Alt","RAlt"))
         keys.add(KeyInfo(12F+32F*12.5F,12F+37F*4,27F,32F,Keyboard.KEY_HOME,"\u0000","Home"))
-        keys.add(KeyInfo(12F+32F*13.5F,12F+37F*4,32F*1.5F-5F,32F,Keyboard.KEY_L/*IDK*/,"Ctrl","RCtrl"))
+        keys.add(KeyInfo(12F+32F*13.5F,12F+37F*4,32F*1.5F-5F,32F,Keyboard.KEY_RCONTROL,"Ctrl","RCtrl"))
     }
 
     override fun initGui() {
