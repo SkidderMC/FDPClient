c434f408e484e2d4875190e26ed7ad78bc39f1e8
Timeline <53483352+2933968621@users.noreply.github.com>
2021-04-28 12:16:54 +0800

    Maybe it doesn't work?
    idk

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/HealthWarn.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/HealthWarn.kt
@@ -2,7 +2,8 @@ package net.ccbluex.liquidbounce.features.module.modules.misc
 
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.event.MotionEvent
+import net.ccbluex.liquidbounce.event.WorldEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
@@ -13,21 +14,30 @@ import net.ccbluex.liquidbounce.value.IntegerValue
 @ModuleInfo(name = "HealthWarn", description = "Show warning when you during low health.", category = ModuleCategory.MISC, array = false)
 class HealthWarn : Module() {
     private val healthValue = IntegerValue("Health", 7, 1, 20)
-    private var canWarn=true
+    private var canWarn = true
 
     override fun onEnable() {
-        canWarn=true
+        canWarn = true
+    }
+    
+    override fun onDisable() {
+        canWarn = true
+    }
+    
+    @EventTarget
+    fun onWorld(event: WorldEvent) {
+        canWarn = true
     }
 
     @EventTarget
-    fun onUpdate(event: UpdateEvent){
+    fun onUpdate(event: MotionEvent){
         if(mc.thePlayer.health <= healthValue.get()){
             if(canWarn){
-                LiquidBounce.hud.addAlert(Alert("HP Warning","YOU ARE AT LOW HP!",NotifyType.WARN,3000))
-                canWarn=false
+                LiquidBounce.hud.addAlert(Alert("HP Warning", "YOU ARE AT LOW HP!", NotifyType.WARN, 3000))
+                canWarn = false
             }
         }else{
-            canWarn=true
+            canWarn = true
         }
     }
 }
