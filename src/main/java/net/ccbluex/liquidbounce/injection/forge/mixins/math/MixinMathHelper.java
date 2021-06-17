4aaf0b95cbca676855cd2ed341f291755e546897
Timeline <53483352+2933968621@users.noreply.github.com>
2021-06-17 18:02:39 +0800

    Update MixinMathHelper.java

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/math/MixinMathHelper.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/math/MixinMathHelper.java
@@ -15,6 +15,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import java.util.function.Supplier;
 
 @Mixin(MathHelper.class)
 @SideOnly(Side.CLIENT)
