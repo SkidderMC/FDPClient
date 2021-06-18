99a2d9f1b05a09e1cb47b6b358a59ff21c0721b6
FengXiao <82002740+FengXiaoSkidGod@users.noreply.github.com>
2021-06-18 21:32:11 +0800

    Update Teams.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/Teams.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/Teams.kt
@@ -3,11 +3,6 @@
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
  * https://github.com/Project-EZ4H/FDPClient/
  */
-/*
- * FDPClient Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
- * https://github.com/Project-EZ4H/FDPClient/
- */
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
 import net.ccbluex.liquidbounce.features.module.Module
