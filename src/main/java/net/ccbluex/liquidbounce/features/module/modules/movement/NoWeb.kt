28e521acaf1be469aa1f2dad449c50a84cad431a
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2021-06-21 17:53:38 +0800

    Update NoWeb.kt
    AAC4 NoWeb

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoWeb.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoWeb.kt
@@ -15,7 +15,7 @@ import net.ccbluex.liquidbounce.value.ListValue
 @ModuleInfo(name = "NoWeb", description = "Prevents you from getting slowed down in webs.", category = ModuleCategory.MOVEMENT)
 class NoWeb : Module() {
 
-    private val modeValue = ListValue("Mode", arrayOf("None", "AAC", "LAAC", "Rewi"), "None")
+    private val modeValue = ListValue("Mode", arrayOf("None", "OldAAC", "LAAC", "Rewi", "AAC4"), "None")
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
@@ -24,7 +24,7 @@ class NoWeb : Module() {
 
         when (modeValue.get().toLowerCase()) {
             "none" -> mc.thePlayer.isInWeb = false
-            "aac" -> {
+            "oldaac" -> {
                 mc.thePlayer.jumpMovementFactor = 0.59f
 
                 if (!mc.gameSettings.keyBindSneak.isKeyDown)
@@ -38,6 +38,16 @@ class NoWeb : Module() {
 
                 if (mc.thePlayer.onGround)
                     mc.thePlayer.jump()
+            }
+            "aac4" -> {
+                mc.timer.timerSpeed = 0.99F
+                mc.thePlayer.jumpMovementFactor = 0.02958f
+                mc.thePlayer.motionY -= 0.00775
+                if (mc.thePlayer.onGround) {
+                    mc.thePlayer.jump()
+                    mc.thePlayer.motionY = 0.4050
+                    mc.timer.timerSpeed = 1.35F
+                }
             }
             "rewi" -> {
                 mc.thePlayer.jumpMovementFactor = 0.42f
