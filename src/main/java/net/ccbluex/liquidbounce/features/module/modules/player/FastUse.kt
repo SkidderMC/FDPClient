8065d5817d76a43354b829dfdb684a49ea23f8bf
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2021-07-07 22:14:50 +0800

    Update FastUse.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/FastUse.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/FastUse.kt
@@ -21,10 +21,11 @@ import net.minecraft.network.play.client.C03PacketPlayer
 
 @ModuleInfo(name = "FastUse", description = "Allows you to use items faster.", category = ModuleCategory.PLAYER)
 class FastUse : Module() {
-    private val modeValue = ListValue("Mode", arrayOf("Instant", "Timer", "CustomDelay"), "NCP")
+    private val modeValue = ListValue("Mode", arrayOf("Instant", "Timer", "CustomDelay", "DelayedInstant"), "DelayedInstant")
     private val timerValue = FloatValue("Timer", 1.22F, 0.1F, 2.0F)
-    private val durationValue = IntegerValue("InstantMinUseDuration", 14, 0, 100)
-    private val repeatValue = IntegerValue("RepeatTime", 35, 0, 100)
+    private val durationValue = IntegerValue("InstantDelay", 14, 0, 35)
+    //private val repeatValue = IntegerValue("RepeatTime", 35, 0, 100)
+    //#stucks code LMAO
     private val delayValue = IntegerValue("Delay", 0, 0, 300)
 
     private val msTimer = MSTimer()
@@ -45,7 +46,7 @@ class FastUse : Module() {
         if (usingItem is ItemFood || usingItem is ItemBucketMilk || usingItem is ItemPotion) {
             when (modeValue.get().toLowerCase()) {
                 "instant" -> if (mc.thePlayer.itemInUseDuration > durationValue.get()) {
-                    repeat(repeatValue.get()) {
+                    repeat(36-mc.thePlayer.itemInUseDuration) {
                         mc.netHandler.addToSendQueue(C03PacketPlayer(mc.thePlayer.onGround))
                     }
 
