e48867a3a9fa8750f3bec38fbd188117c61d5edd
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2021-07-09 15:38:18 +0800

    Delete src/main directory

--- src/main/java/net/ccbluex/liquidbounce/features/command/commands/UsernameCommand.kt
+++
@@ -1,9 +0,0 @@
-package net.ccbluex.liquidbounce.features.command.commands
-
-import net.ccbluex.liquidbounce.features.command.Command
-
-class UsernameCommand : Command("username", arrayOf("un")) {
-    override fun execute(args: Array<String>) {
-        chat("Username: "+mc.thePlayer.name)
-    }
-}
--- src/main/java/net/ccbluex/liquidbounce/utils/MathUtils.kt
+++
@@ -1,8 +0,0 @@
-package net.ccbluex.liquidbounce.utils
-
-object MathUtils {
-    @JvmStatic
-    fun radians(degrees: Double):Double{
-        return degrees * Math.PI / 180
-    }
-}
