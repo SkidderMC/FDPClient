f768ee8c8dcc1fbdd6e2278f0a4e5854d6cf3a83
FengXiao <82002740+FengXiaoSkidGod@users.noreply.github.com>
2021-07-09 12:51:42 +0800

    ModuleInfo

--- src/main/java/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -148,7 +148,8 @@ class ModuleManager : Listenable {
             AntiAim::class.java,
             NewClickGui::class.java,
             SuperKnockback::class.java,
-            AutoReport::class.java
+            AutoReport::class.java,
+            ModuleInfo::class.java
         )
 
         registerModule(Fucker)
