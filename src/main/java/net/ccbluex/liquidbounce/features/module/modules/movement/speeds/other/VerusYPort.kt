56cf58b5811655867c2654c4e6605e6a9195ddf7
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2021-07-21 11:25:15 +0800

    Update VerusYPort.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/VerusYPort.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/VerusYPort.kt
@@ -5,15 +5,16 @@ import net.ccbluex.liquidbounce.utils.MovementUtils
 
 class VerusYPort : SpeedMode("VerusYPort") {
     override fun onUpdate() {
-        if (mc.thePlayer!!.isInWater) return
-        if (MovementUtils.isMoving() && mc.thePlayer!!.onGround) {
-	mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY + 0.42, mc.thePlayer.posZ);
-	MovementUtils.strafe(0.6075)
-        }else if(MovementUtils.isMoving()) MovementUtils.strafe()
-	else {
-	    mc.thePlayer.motionZ = 0.0
-	    mc.thePlayer.motionX = 0.0
-        }
+  	if (mc.thePlayer!!.isInWater) return
+	if (MovementUtils.isMoving() && mc.thePlayer!!.onGround) {
+	    mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY + 0.42, mc.thePlayer.posZ)
+	    mc.thePlayer.motionY = 0.0
+	    MovementUtils.strafe(0.6075f)
+	}else if(MovementUtils.isMoving()) MovementUtils.strafe()
+	    else {
+		mc.thePlayer.motionZ = 0.0
+		mc.thePlayer.motionX = 0.0
+	    }
     }
 
     override fun onDisable() {
