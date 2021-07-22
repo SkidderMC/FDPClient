3ff2c2b62e75b58b8d8ed5f22c916f0039e471e8
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2021-07-22 20:22:13 +0800

    Update SafeWalk.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/SafeWalk.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/SafeWalk.kt
@@ -16,10 +16,26 @@ import net.ccbluex.liquidbounce.value.BoolValue
 class SafeWalk : Module() {
 
     private val airSafeValue = BoolValue("AirSafe", false)
+    private val onlyVoidValue = BoolValue("OnlyPredictVoid", false)
 
     @EventTarget
     fun onMove(event: MoveEvent) {
-        if (airSafeValue.get() || mc.thePlayer.onGround)
-            event.isSafeWalk = true
+        if(onlyVoidValue.get() && !checkVoid()) {
+            return
+        }else{
+            if(airSafeValue.get() || mc.thePlayer.onGround)
+                event.isSafeWalk = true
+        }
+    }
+
+    private fun checkVoid():Boolean {
+        var i= (-(mc.thePlayer.posY-1.4857625)).toInt()
+        var dangerous=true
+		while(i<=0) {
+			dangerous = mc.theWorld.getCollisionBoxes(mc.thePlayer.entityBoundingBox.offset(mc.thePlayer.motionX*1.4, i, mc.thePlayer.motionZ*1.4)).isEmpty()
+			i++
+			if(!dangerous) break
+		}
+        return dangerous
     }
 }
