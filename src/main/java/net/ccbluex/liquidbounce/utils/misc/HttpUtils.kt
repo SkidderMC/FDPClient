2dcb0dfe38e25b096ccf259ca8fa4c714d375556
guimc233 <guimc_awa233@outlook.com>
2021-07-29 17:44:19 +0800

    Replace "Project-EZ4H" to "UnlegitMC""

--- src/main/java/net/ccbluex/liquidbounce/utils/misc/HttpUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/misc/HttpUtils.kt
@@ -1,7 +1,7 @@
 /*
  * FDPClient Hacked Client
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
- * https://github.com/Project-EZ4H/FDPClient/
+ * https://github.com/UnlegitMC/FDPClient/
  */
 
 package net.ccbluex.liquidbounce.utils.misc
