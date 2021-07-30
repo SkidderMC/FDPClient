44c883c07aa5be3c9a3926015ce0901b5df695a5
liuli <liulihaocai@outlook.com>
2021-07-30 11:13:47 +0800

    UTF-16 support

--- src/main/java/net/ccbluex/liquidbounce/ui/font/AWTFontRenderer.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/font/AWTFontRenderer.kt
@@ -23,7 +23,7 @@ class AWTFontRenderer(val font: Font, initialize: Boolean = true) {
 
     private val fontHeight: Int
 
-    private val chars=HashMap<Char, FontChar>()
+    private val chars=HashMap<String, FontChar>()
 
     private val fontMetrics: FontMetrics
     private val cacheDir=File(LiquidBounce.fileManager.cacheDir,getFontDirName())
@@ -43,7 +43,7 @@ class AWTFontRenderer(val font: Font, initialize: Boolean = true) {
 
         if(initialize){
             // 先把英文渲染好,其他的被动渲染
-            loadChar(' ')
+            loadChar(" ")
             prepareCharImages('0','9')
             prepareCharImages('a','z')
             prepareCharImages('A','Z')
@@ -72,9 +72,23 @@ class AWTFontRenderer(val font: Font, initialize: Boolean = true) {
 
         GlStateManager.color(red, green, blue, alpha)
 
+        var isLastUTF16=false
+        var highSurrogate='\u0000'
         for(char in text.toCharArray()){
-            val singleWidth=drawChar(char, 0f, 0f)
-            GL11.glTranslatef(singleWidth-8f,0f,0f)
+            if(char in '\ud800'..'\udfff'){
+                if(isLastUTF16){
+                    val utf16Char="$highSurrogate$char"
+                    val singleWidth=drawChar(utf16Char, 0f, 0f)
+                    GL11.glTranslatef(singleWidth-8f,0f,0f)
+                }else{
+                    highSurrogate=char
+                }
+                isLastUTF16 = !isLastUTF16
+            }else{
+                val singleWidth=drawChar("$char", 0f, 0f)
+                GL11.glTranslatef(singleWidth-8f,0f,0f)
+                isLastUTF16=false
+            }
         }
 
         GlStateManager.popMatrix()
@@ -89,7 +103,7 @@ class AWTFontRenderer(val font: Font, initialize: Boolean = true) {
      * @param x        target position x to render
      * @param y        target position y to render
      */
-    private fun drawChar(char: Char, x: Float, y: Float): Int {
+    private fun drawChar(char: String, x: Float, y: Float): Int {
         val fontChar=chars[char] ?: loadChar(char)
         Minecraft.getMinecraft().textureManager.bindTexture(fontChar.resourceLocation)
         Gui.drawModalRectWithCustomSizedTexture(x.toInt(), y.toInt(), 0f, 0f, fontChar.width, fontHeight, fontChar.width.toFloat(), fontHeight.toFloat())
@@ -105,9 +119,22 @@ class AWTFontRenderer(val font: Font, initialize: Boolean = true) {
     fun getStringWidth(text: String): Int {
         var width=0
 
+        var isLastUTF16=false
+        var highSurrogate='\u0000'
         for(char in text.toCharArray()){
-            val fontChar=chars[char] ?: loadChar(char)
-            width+=fontChar.width-8
+            if(char in '\ud800'..'\udfff'){
+                if(isLastUTF16){
+                    val utf16Char="$highSurrogate$char"
+
+                    width+=(chars[utf16Char] ?: loadChar(utf16Char)).width-8
+                }else{
+                    highSurrogate=char
+                }
+                isLastUTF16 = !isLastUTF16
+            }else{
+                width+=(chars["$char"] ?: loadChar("$char")).width-8
+                isLastUTF16=false
+            }
         }
 
         return width/2
@@ -119,21 +146,33 @@ class AWTFontRenderer(val font: Font, initialize: Boolean = true) {
         graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
     }
 
+    private fun charInfo(char: String):String{
+        val charArr=char.toCharArray()
+        if(char.length==1){
+            return "char-${charArr[0].toInt()}"
+        }else if(char.length==2){
+            val first=(charArr[0].toInt()-0xd800)*0x400
+            val second=charArr[1].toInt()-0xdc00
+            return "char-${first+second+0x10000}"
+        }
+        return "NMSL"
+    }
+
     /**
      * 获取char对应的缓存文件
      */
-    private fun getCharCacheFile(char: Char) = File(cacheDir,"char-${char.toInt()}.png")
+    private fun getCharCacheFile(char: String) = File(cacheDir,"${charInfo(char)}.png")
 
     /**
      * @return 通过Char获取的ResourceLocation
      */
-    private fun getResourceLocationByChar(char: Char) = ResourceLocation("fdp/font/${getFontDirName()}/char-${char.toInt()}")
+    private fun getResourceLocationByChar(char: String) = ResourceLocation("fdp/font/${getFontDirName()}/${charInfo(char)}")
 
     /**
      * 渲染字符图片
      */
-    private fun renderCharImage(char: Char): FontChar {
-        var charWidth = fontMetrics.charWidth(char) + 8
+    private fun renderCharImage(char: String): FontChar {
+        var charWidth = fontMetrics.stringWidth(char) + 8
         if (charWidth <= 0)
             charWidth = 7
 
@@ -142,7 +181,7 @@ class AWTFontRenderer(val font: Font, initialize: Boolean = true) {
         putHints(graphics)
         graphics.font = font
         graphics.color = Color.WHITE
-        graphics.drawString(char.toString(), 3, 1 + fontMetrics.ascent)
+        graphics.drawString(char, 3, 1 + fontMetrics.ascent)
 
         return FontChar(char, getResourceLocationByChar(char), fontImage)
     }
@@ -151,7 +190,7 @@ class AWTFontRenderer(val font: Font, initialize: Boolean = true) {
      * @param char 字符
      * 初始化单个字符图片
      */
-    private fun loadChar(char: Char): FontChar {
+    private fun loadChar(char: String): FontChar {
         val fc=loadCharImageFromCache(char)
         chars[char] = fc
         return fc
@@ -161,7 +200,7 @@ class AWTFontRenderer(val font: Font, initialize: Boolean = true) {
      * 从本地缓存读取字符图片
      * 没有则渲染
      */
-    private fun loadCharImageFromCache(char: Char): FontChar {
+    private fun loadCharImageFromCache(char: String): FontChar {
         val charImageFile=getCharCacheFile(char)
         return if(charImageFile.exists()){
             FontChar(char, getResourceLocationByChar(char), ImageIO.read(charImageFile))
@@ -190,7 +229,7 @@ class AWTFontRenderer(val font: Font, initialize: Boolean = true) {
         val stopAscii=stop.toInt().coerceAtLeast(start.toInt())
 
         for (ascii in startAscii until stopAscii){
-            loadChar(ascii.toChar())
+            loadChar("${ascii.toChar()}")
         }
     }
 }
--- src/main/java/net/ccbluex/liquidbounce/ui/font/FontChar.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/font/FontChar.kt
@@ -8,8 +8,9 @@ import java.awt.image.BufferedImage
 /**
  * @param char 对应的字符
  * @param bufImg 渲染出的字符图片
+ * 为了支持UTF16，所以char是string
  */
-class FontChar(val char: Char, val resourceLocation: ResourceLocation, val bufImg: BufferedImage) {
+class FontChar(val char: String, val resourceLocation: ResourceLocation, val bufImg: BufferedImage) {
     val width=bufImg.width
 
     init {
