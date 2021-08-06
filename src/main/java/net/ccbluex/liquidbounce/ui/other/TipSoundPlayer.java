b0f9d018faed441ce2c53f14b6ae2eaaa6b4610a
exit-scammed <85971344+exit-scammed@users.noreply.github.com>
2021-08-06 08:45:32 +0700

    literally a shorter and more effective way to play .wav audio files lol (#27)

--- src/main/java/net/ccbluex/liquidbounce/ui/other/TipSoundPlayer.java
+++ src/main/java/net/ccbluex/liquidbounce/ui/other/TipSoundPlayer.java
@@ -11,27 +11,18 @@ public class TipSoundPlayer {
     }
 
     public void asyncPlay(){
-        new Thread(this::play).start();
+        new Thread(this::playSound).start();
     }
 
-    public void play() {
+    public void playSound() {
         try {
-            AudioInputStream audioInputStream=AudioSystem.getAudioInputStream(file);
-            AudioFormat audioFormat = audioInputStream.getFormat();
-            DataLine.Info dataLine_info = new DataLine.Info(SourceDataLine.class, audioFormat);
-            SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLine_info);
-            byte[] b = new byte[1024];
-            int len = 0;
-            sourceDataLine.open(audioFormat, 1024);
-            sourceDataLine.start();
-            while ((len = audioInputStream.read(b)) > 0) {
-                sourceDataLine.write(b, 0, len);
-            }
-            audioInputStream.close();
-            sourceDataLine.drain();
-            sourceDataLine.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
         }
     }
 }
