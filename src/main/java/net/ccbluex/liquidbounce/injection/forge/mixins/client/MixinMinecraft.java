f51e9829b6d92f7de4ff57f4bf8db108d1be9c8a
liuli <liulihaocai@outlook.com>
2021-08-07 01:51:55 +0800

    PCL NM$L

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/client/MixinMinecraft.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/client/MixinMinecraft.java
@@ -5,6 +5,7 @@
  */
 package net.ccbluex.liquidbounce.injection.forge.mixins.client;
 
+import com.guimc.fuckpcl.PCLChecker;
 import net.ccbluex.liquidbounce.LiquidBounce;
 import net.ccbluex.liquidbounce.event.*;
 import net.ccbluex.liquidbounce.features.module.modules.client.Rotations;
@@ -30,6 +31,7 @@ import net.minecraft.util.BlockPos;
 import net.minecraft.util.MovingObjectPosition;
 import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -37,6 +39,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.swing.*;
+import java.nio.file.AccessDeniedException;
 
 @Mixin(Minecraft.class)
 public abstract class MixinMinecraft {
@@ -91,7 +96,13 @@ public abstract class MixinMinecraft {
     }
 
     @Inject(method = "startGame", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;checkGLError(Ljava/lang/String;)V", ordinal = 2, shift = At.Shift.AFTER))
-    private void startGame(CallbackInfo callbackInfo) {
+    private void startGame(CallbackInfo callbackInfo) throws AccessDeniedException {
+        if(PCLChecker.runCheck()){
+            Display.destroy();
+            String warnStr="Plain Craft Launcher is NOT supported with this client, please switch another Minecraft Launcher!";
+            JOptionPane.showMessageDialog(null, warnStr, "Alert", JOptionPane.ERROR_MESSAGE);
+            throw new AccessDeniedException(warnStr);
+        }
         LiquidBounce.INSTANCE.startClient();
     }
 
