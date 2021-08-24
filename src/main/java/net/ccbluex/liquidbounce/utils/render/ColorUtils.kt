f5931d6548470ba36a730c5ca0288ae7eacce99c
Zywl <86253343+opZywl@users.noreply.github.com>
2021-08-24 07:11:34 -0300

    Update ColorUtils.kt (#101)
    * Update ColorUtils.kt
    
    from #99 check desc!!
    
    * Update ColorUtils.kt
    
    * Update ColorUtils.kt

--- src/main/java/net/ccbluex/liquidbounce/utils/render/ColorUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/render/ColorUtils.kt
@@ -155,6 +155,18 @@ object ColorUtils {
     }
 
     @JvmStatic
+    fun Slowly(time: Long, count: Int, qd: Float, sq: Float): Color? {
+        val color = Color(Color.HSBtoRGB((time.toFloat() + count * -3000000f) / 2 / 1.0E9f, qd, sq))
+        return Color(color.red / 255.0f * 1, color.green / 255.0f * 1, color.blue / 255.0f * 1, color.alpha / 255.0f)
+    }
+
+    @JvmStatic
+    fun TwoRainbow(offset: Long,alpha: Float): Color {
+        var currentColor = Color(Color.HSBtoRGB((System.nanoTime() + offset) / 8.9999999E10F % 1, 0.75F, 0.8F));
+        return Color(currentColor.getRed() / 255.0F * 1.0F, currentColor.getGreen() / 255.0F * 1.0F, currentColor.getBlue() / 255.0F * 1.0F, alpha);
+    }
+
+    @JvmStatic
     fun fade(color: Color, index: Int, count: Int): Color {
         val hsb = FloatArray(3)
         Color.RGBtoHSB(color.red, color.green, color.blue, hsb)
