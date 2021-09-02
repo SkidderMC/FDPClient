779cac78d4a7ec22d362e88047c0cfafa771993a
Zywl <86253343+opZywl@users.noreply.github.com>
2021-09-01 23:19:01 -0300

    Update FriendCommand.kt (#229)
    fix bug

--- src/main/java/net/ccbluex/liquidbounce/features/command/commands/FriendCommand.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/command/commands/FriendCommand.kt
@@ -7,6 +7,7 @@ package net.ccbluex.liquidbounce.features.command.commands
 
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.features.command.Command
+import net.ccbluex.liquidbounce.features.module.modules.misc.AntiBot
 import net.ccbluex.liquidbounce.utils.misc.StringUtils
 import net.ccbluex.liquidbounce.utils.render.ColorUtils
 
