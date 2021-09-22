02e02f1b3ab3397b1c7fcebbc0e02fd5244d9d36
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2021-09-22 20:36:44 +0800

    Update Velocity.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Velocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Velocity.kt
@@ -110,8 +110,8 @@ class Velocity : Module() {
 
         if((onlyGroundValue.get() && !mc.thePlayer.onGround) || (onlyCombatValue.get() && !LiquidBounce.combatManager.inCombat))
             return
-            if(onlyHitVelocityValue.get() && mc.thePlayer.motionY<0.05) return;
-            if(noFireValue.get() && mc.thePlayer.isBurning) return;
+            //if(onlyHitVelocityValue.get() && mc.thePlayer.motionY<0.05) return；
+            if(noFireValue.get() && mc.thePlayer.isBurning) return
         when (modeValue.get().lowercase()) {
             "jump" -> if (mc.thePlayer.hurtTime > 0 && mc.thePlayer.onGround) {
                 mc.thePlayer.motionY = 0.42
@@ -256,8 +256,8 @@ class Velocity : Module() {
         if (packet is S12PacketEntityVelocity) {
             if (mc.thePlayer == null || (mc.theWorld?.getEntityByID(packet.entityID) ?: return) != mc.thePlayer)
                 return
-            if(onlyHitVelocityValue.get() && packet.getMotionY()<0.05) return;
-            if(noFireValue.get() && mc.thePlayer.isBurning) return;
+            if(onlyHitVelocityValue.get() && packet.getMotionY()<400.0) return
+            if(noFireValue.get() && mc.thePlayer.isBurning) return
             velocityTimer.reset()
 
             when (modeValue.get().lowercase()) {
