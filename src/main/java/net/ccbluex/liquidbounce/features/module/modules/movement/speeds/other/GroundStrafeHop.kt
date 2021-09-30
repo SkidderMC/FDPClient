6028279d811284f8ae9976c5937c97fc2d0776c9
Liulihaocai <liuli@tuta.io>
2021-09-30 16:30:16 +0800

    groundstrafehop

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/GroundStrafeHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/GroundStrafeHop.kt
@@ -1,17 +1,15 @@
-/*
- * FDPClient Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
- * https://github.com/UnlegitMC/FDPClient/
- */
 package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.other
 
 import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
 
-class StrafeHop : SpeedMode("StrafeHop") {
+class GroundStrafeHop : SpeedMode("GroundStrafeHop") {
     override fun onPreMotion() {
         if (MovementUtils.isMoving()) {
-            if (mc.thePlayer.onGround) mc.thePlayer.jump() else MovementUtils.strafe()
+            if (mc.thePlayer.onGround) {
+                mc.thePlayer.jump()
+                MovementUtils.strafe()
+            }
         } else {
             mc.thePlayer.motionX = 0.0
             mc.thePlayer.motionZ = 0.0
