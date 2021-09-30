5645d550a13fcbf122cb4e5c69d8ddc4af436e32
ll11l1lIllIl1lll <88377095+ll11l1lIllIl1lll@users.noreply.github.com>
2021-09-30 12:24:30 +0800

    Update FastUse.kt (#377)
    * Update FastUse.kt
    
    好的吧 我可能记错之前FastUse的逻辑了
    
    * Update FastUse.kt
    
    NCP=delayedinstant
    
    * Update FastUse.kt
    
    好的吧 我是傻逼
    
    * IDK

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/FastUse.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/FastUse.kt
@@ -21,7 +21,7 @@ import net.minecraft.network.play.client.C03PacketPlayer
 
 @ModuleInfo(name = "FastUse", category = ModuleCategory.PLAYER)
 class FastUse : Module() {
-    private val modeValue = ListValue("Mode", arrayOf("Instant", "Timer", "CustomDelay", "DelayedInstant","MinemoraTest","NCP","AAC"), "DelayedInstant")
+    private val modeValue = ListValue("Mode", arrayOf("Instant", "Timer", "CustomDelay", "DelayedInstant","MinemoraTest","AAC","NewAAC"), "DelayedInstant")
     private val timerValue = FloatValue("Timer", 1.22F, 0.1F, 2.0F).displayable { modeValue.equals("Timer") }
     private val durationValue = IntegerValue("InstantDelay", 14, 0, 35).displayable { modeValue.equals("DelayedInstant") }
     private val delayValue = IntegerValue("CustomDelay", 0, 0, 300).displayable { modeValue.equals("CustomDelay") }
@@ -66,19 +66,20 @@ class FastUse : Module() {
                         mc.netHandler.addToSendQueue(C03PacketPlayer(mc.thePlayer.onGround))
                     }
                 }
-                    mc.playerController.onStoppedUsingItem(mc.thePlayer)
+                    
+                }
+                "newaac" -> {
+                    mc.timer.timerSpeed = 0.49F
+                    usedTimer = true
+                    repeat(2) {
+                        mc.netHandler.addToSendQueue(C03PacketPlayer(mc.thePlayer.onGround))
+                    }
+
+                    // mc.playerController.onStoppedUsingItem(mc.thePlayer)
                 }
                 "timer" -> {
                     mc.timer.timerSpeed = timerValue.get()
                     usedTimer = true
-                }
-                
-                "ncp" -> if (mc.thePlayer.itemInUseDuration > 14) {
-                     repeat(20) {
-                        mc.netHandler.addToSendQueue(C03PacketPlayer(mc.thePlayer.onGround))
-                    }
-
-                    mc.playerController.onStoppedUsingItem(mc.thePlayer)
                 }
 
                 "minemoratest" -> {
