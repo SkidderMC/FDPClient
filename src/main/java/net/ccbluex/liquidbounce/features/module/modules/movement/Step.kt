590c35c07d9d6e1dc9b10daf7a1001cfb9a4f160
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2021-10-05 20:37:30 +0800

    琉璃你抄了个啥东西艹

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Step.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Step.kt
@@ -29,9 +29,7 @@ class Step : Module() {
      * OPTIONS
      */
 
-    private val modeValue = ListValue("Mode", arrayOf("Vanilla", "Jump", "TimerJump", "NCP", "MotionNCP", "OldNCP", "OldAAC", "LAAC", "AAC3.3.4", "AAC3.6.4", "AAC4.4.0", "Spartan", "Rewinside"), "NCP")
-    //TODO : MotionStep Bypass Matrix(maybe I can covert it to packet step :/ Matrix moment) and WatchCat / Skid NCP( already patched in *New* NCP (Fork) ) Step from Skidma
-    
+    private val modeValue = ListValue("Mode", arrayOf("Vanilla", "Jump",/* "TimerJump",*/ "NCP", "MotionNCP", "OldNCP", "OldAAC", "LAAC", "AAC3.3.4", "AAC3.6.4", "AAC4.4.0", "Spartan", "Rewinside"), "NCP")
     private val heightValue = FloatValue("Height", 1F, 0.6F, 10F)
     private val jumpHeightValue = FloatValue("JumpMotion", 0.42F, 0.37F, 0.42F).displayable { modeValue.equals("Jump") || modeValue.equals("TimerJump") }
     private val delayValue = IntegerValue("Delay", 0, 0, 500)
