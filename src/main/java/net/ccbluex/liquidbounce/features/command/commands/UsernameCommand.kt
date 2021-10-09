fdb8017bdcb97859c1fe84e5adfad3682e3977dd
Liulihaocai <liuli@tuta.io>
2021-10-09 23:34:19 +0800

    clean codez

--- src/main/java/net/ccbluex/liquidbounce/features/command/commands/UsernameCommand.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/command/commands/UsernameCommand.kt
@@ -2,8 +2,8 @@ package net.ccbluex.liquidbounce.features.command.commands
 
 import net.ccbluex.liquidbounce.features.command.Command
 
-class UsernameCommand : Command("username", arrayOf("un")) {
+class UsernameCommand : Command("username", arrayOf("name")) {
     override fun execute(args: Array<String>) {
-        chat("Username: "+mc.thePlayer.name)
+        chat("Username: " + mc.thePlayer.name)
     }
 }
