c1d3cb827ee171aa506b5e75b370e663bd9b3d49
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2021-10-14 22:37:10 +0800

    Update MathUtils.kt

--- src/main/java/net/ccbluex/liquidbounce/utils/MathUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/MathUtils.kt
@@ -8,7 +8,7 @@ object MathUtils {
     
     @JvmStatic
     fun getDistance(x1: Double, y1: Double, x2: Double, y2: Double):Double{
-        return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2))
+        return Math.sqrt(Math.pow(x1-x2,(2).toDouble())+Math.pow(y1-y2,(2).toDouble()))
     }
     //TODO: 两圆相交坐标求解（月底能有时间弄）（TargetStrafe）
 }
