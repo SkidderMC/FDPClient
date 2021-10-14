0c390ed2c3400e32cacef25da64c45e0ecaa1f64
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2021-10-14 22:33:56 +0800

    Update MathUtils.kt

--- src/main/java/net/ccbluex/liquidbounce/utils/MathUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/MathUtils.kt
@@ -5,4 +5,10 @@ object MathUtils {
     fun radians(degrees: Double):Double{
         return degrees * Math.PI / 180
     }
+    
+    @JvmStatic
+    fun getDistance(x1: Double, y1: Double, x2: Double, y2: Double):Double{
+        return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2))
+    }
+    //TODO: 两圆相交坐标求解（月底能有时间弄）（TargetStrafe）
 }
