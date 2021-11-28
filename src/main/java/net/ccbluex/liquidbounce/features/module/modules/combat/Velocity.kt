8b973253bb4d6c41743cb3909e6eff5e6e4153f8
liulihaocai <liulihaocaiqwq@gmail.com>
2021-11-28 20:05:09 +0800

    matrix nofall

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Velocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Velocity.kt
@@ -442,7 +442,7 @@ class Velocity : Module() {
                     }
 
                     if (rspDengerValue.get()) {
-                        val pos = FallingPlayer(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ, packet.motionX / 8000.0, packet.motionY / 8000.0, packet.motionZ / 8000.0, 0f, 0f, 0f).findCollision(60)
+                        val pos = FallingPlayer(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ, packet.motionX / 8000.0, packet.motionY / 8000.0, packet.motionZ / 8000.0, 0f, 0f, 0f, 0f).findCollision(60)
                         if (pos != null && pos.y> (mc.thePlayer.posY - 7)) {
                             return
                         }
