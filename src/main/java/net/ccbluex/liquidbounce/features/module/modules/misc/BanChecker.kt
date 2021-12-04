cdf225a92bebef1cc183731ba79193624ff28723
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2021-12-04 15:19:03 +0800

    bruh

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/BanChecker.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/BanChecker.kt
@@ -1,10 +1,3 @@
-/*
- * LiquidBounce+ Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
- * https://github.com/WYSI-Foundation/LiquidBouncePlus/
- * 
- * This code belongs to WYSI-Foundation. Please give credits when using this in your repository.
- */
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
 import com.google.gson.JsonParser
