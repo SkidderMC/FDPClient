73f0e48dfeeb99e25f72e6426753687543393131
Zywl <86253343+opZywl@users.noreply.github.com>
2021-12-22 23:27:20 -0300

    Better DamageParticles (#655)
    * Better DamageParticles
    
    yeeees
    
    * Update DamageParticle.kt
    
    * Update DamageParticle.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/DamageParticle.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/DamageParticle.kt
@@ -12,6 +12,7 @@ import net.ccbluex.liquidbounce.event.WorldEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.utils.render.ColorUtils
 import net.ccbluex.liquidbounce.utils.EntityUtils
 import net.ccbluex.liquidbounce.value.IntegerValue
 import net.minecraft.client.renderer.GlStateManager
@@ -23,31 +24,39 @@ import kotlin.math.abs
 
 @ModuleInfo(name = "DamageParticle", category = ModuleCategory.RENDER)
 class DamageParticle : Module() {
-    private val healthData = HashMap<Int, Float>()
-    private val particles = ArrayList<SingleParticle>()
+    private val healthData = HashMap<Int, Float> ()
+    private val particles = ArrayList<SingleParticle> ()
 
     private val aliveTicks = IntegerValue("AliveTicks", 20, 10, 50)
     private val sizeValue = IntegerValue("Size", 3, 1, 7)
+    private val colorRedValue = IntegerValue("Red", 68, 0, 255)
+    private val colorGreenValue = IntegerValue("Green", 117, 0, 255)
+    private val colorBlueValue = IntegerValue("Blue", 255, 0, 255)
+    private val colorAlphaValue = IntegerValue("Alpha", 100, 0, 255)
+    private val colorRainbow = BoolValue("Rainbow", false)
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
-        synchronized(particles) {
-            for (entity in mc.theWorld.loadedEntityList) {
-                if (entity is EntityLivingBase && EntityUtils.isSelected(entity, true)) {
-                    val lastHealth = healthData.getOrDefault(entity.entityId, entity.maxHealth)
+        synchronized(particles ) {
+            for(entity in mc.theWorld.loadedEntityList) {
+                if(entity is EntityLivingBase && EntityUtils.isSelected(entity,true)) {
+                    val lastHealth = healthData.getOrDefault(entity.entityId,entity.maxHealth)
                     healthData[entity.entityId] = entity.health
-                    if (lastHealth == entity.health) continue
+                    if(lastHealth == entity.health) continue
 
-                    val prefix = if (lastHealth> entity.health) { "§c❤" } else { "§a§l❤" }
-                    particles.add(SingleParticle(prefix + BigDecimal(abs(lastHealth - entity.health).toDouble()).setScale(1, BigDecimal.ROUND_HALF_UP).toDouble(), entity.posX - 0.5 + Random(System.currentTimeMillis()).nextInt(5).toDouble() * 0.1, entity.entityBoundingBox.minY + (entity.entityBoundingBox.maxY - entity.entityBoundingBox.minY) / 2.0, entity.posZ - 0.5 + Random(System.currentTimeMillis() + 1L).nextInt(5).toDouble() * 0.1)
+                    val prefix = if (!colorRainbow.get()) (if(lastHealth>entity.health){"§c❤"}else{"§a§l❤"}) else (if(lastHealth>entity.health){"-"}else{"+"})
+                    particles.add(SingleParticle(prefix+BigDecimal(abs(lastHealth-entity.health).toDouble()).setScale(1,BigDecimal.ROUND_HALF_UP).toDouble()
+                        ,entity.posX - 0.5 + Random(System.currentTimeMillis()).nextInt(5).toDouble() * 0.1
+                        ,entity.entityBoundingBox.minY + (entity.entityBoundingBox.maxY - entity.entityBoundingBox.minY) / 2.0
+                        ,entity.posZ - 0.5 + Random(System.currentTimeMillis() + 1L).nextInt(5).toDouble() * 0.1)
                     )
                 }
             }
 
-            val needRemove = ArrayList<SingleParticle>()
+            val needRemove = ArrayList<SingleParticle> ()
             for (particle in particles) {
                 particle.ticks++
-                if (particle.ticks> aliveTicks.get()) {
+                if (particle.ticks>aliveTicks.get()) {
                     needRemove.add(particle)
                 }
             }
@@ -81,12 +90,13 @@ class DamageParticle : Module() {
                     particle.str,
                     (-(mc.fontRendererObj.getStringWidth(particle.str) / 2)).toFloat(),
                     (-(mc.fontRendererObj.FONT_HEIGHT - 1)).toFloat(),
-                    0
+                    if (colorRainbow.get()) ColorUtils.rainbowWithAlpha(colorAlphaValue.get()) else Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get(), colorAlphaValue.get())
                 )
                 GL11.glColor4f(187.0f, 255.0f, 255.0f, 1.0f)
                 GL11.glDepthMask(true)
                 GlStateManager.doPolygonOffset(1.0f, 1500000.0f)
                 GlStateManager.disablePolygonOffset()
+                GlStateManager.resetColor()
                 GlStateManager.popMatrix()
             }
         }
