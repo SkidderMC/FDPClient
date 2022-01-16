4e40629e8e64e90e5acc4528018e43587b18bc64
liulihaocai <liulihaocaiqwq@gmail.com>
2022-01-16 20:40:11 +0800

    enhance targetstrafe

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/TargetStrafe.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/TargetStrafe.kt
@@ -12,18 +12,23 @@ import net.ccbluex.liquidbounce.utils.RotationUtils
 import net.ccbluex.liquidbounce.utils.render.RenderUtils
 import net.ccbluex.liquidbounce.value.BoolValue
 import net.ccbluex.liquidbounce.value.FloatValue
+import net.ccbluex.liquidbounce.value.ListValue
 import net.minecraft.entity.EntityLivingBase
+import net.minecraft.network.play.server.S12PacketEntityVelocity
 import org.lwjgl.opengl.GL11
 import java.awt.Color
+import kotlin.math.abs
 import kotlin.math.cos
 import kotlin.math.sin
 
 @ModuleInfo(name = "TargetStrafe", category = ModuleCategory.MOVEMENT)
 class TargetStrafe : Module() {
     private val radiusValue = FloatValue("Radius", 2.0f, 0.1f, 4.0f)
+    private val airRotateValue = FloatValue("AirRotate", 0.0f, 0.0f, 180f)
+    private val airInstaRotateValue = FloatValue("AirInstaRotate", 180f, 0.0f, 180f)
     private val holdSpaceValue = BoolValue("HoldSpace", false)
     private val onlySpeedValue = BoolValue("OnlySpeed", false)
-    private val onlyGroundValue = BoolValue("OnlyGround", false)
+    private val velocityChangeValue = BoolValue("VelocityChange", true)
     private val renderValue = BoolValue("Render", true)
     private var direction = true
     private var yaw = 0f
@@ -45,9 +50,21 @@ class TargetStrafe : Module() {
     fun strafe(event: MoveEvent) {
         val target = LiquidBounce.combatManager.target
         if (canStrafe(target)) {
-            if(mc.thePlayer.onGround || !onlyGroundValue.get()) {
-                yaw = RotationUtils.getRotationsEntity(target).yaw
+            val bestYaw = RotationUtils.getRotationsEntity(target).yaw
+
+            val rotation = if(mc.thePlayer.onGround) {
+                180f
+            } else {
+                airRotateValue.get()
             }
+            val diff = getAngleDifference(bestYaw, yaw)
+            val newYaw = yaw + abs(diff).coerceAtMost(rotation).let { if (diff < 0) -it else it }
+            if(abs(getAngleDifference(bestYaw, newYaw)) > abs(getAngleDifference(bestYaw, yaw + airInstaRotateValue.get()))) {
+                yaw += airInstaRotateValue.get()
+            } else {
+                yaw = newYaw
+            }
+            yaw %= 360
             MovementUtils.setSpeed(event, MovementUtils.getSpeed().toDouble(), yaw, if (direction) 1.0 else -1.0, if (mc.thePlayer.getDistanceToEntity(target) <= radiusValue.get()) 0.0 else 1.0)
         }
     }
@@ -75,7 +92,7 @@ class TargetStrafe : Module() {
             val z = target.lastTickPosZ + (target.posZ - target.lastTickPosZ) * event.partialTicks - mc.renderManager.viewerPosZ
             val radius = radiusValue.get()
             for (i in 0..360 step 5) {
-                RenderUtils.glColor(Color.getHSBColor(if (i <180) { HUD.rainbowStart.get() + (HUD.rainbowStop.get() - HUD.rainbowStart.get()) * (i / 180f) } else { HUD.rainbowStart.get() + (HUD.rainbowStop.get() - HUD.rainbowStart.get()) * (-(i-360) / 180f) }, 0.7f, 1.0f))
+                RenderUtils.glColor(Color.getHSBColor(if (i < 180) { HUD.rainbowStart.get() + (HUD.rainbowStop.get() - HUD.rainbowStart.get()) * (i / 180f) } else { HUD.rainbowStart.get() + (HUD.rainbowStop.get() - HUD.rainbowStart.get()) * (-(i-360) / 180f) }, 0.7f, 1.0f))
                 GL11.glVertex3d(x - sin(i * Math.PI / 180F) * radius, y, z + cos(i * Math.PI / 180F) * radius)
             }
             GL11.glEnd()
@@ -87,6 +104,29 @@ class TargetStrafe : Module() {
             GL11.glEnable(2832)
             GL11.glEnable(3553)
         }
+    }
+
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
+        val packet = event.packet
+        if(packet is S12PacketEntityVelocity && velocityChangeValue.get()) {
+            if (mc.thePlayer == null
+                || (mc.theWorld?.getEntityByID(packet.entityID) ?: return) != mc.thePlayer
+                || LiquidBounce.combatManager.target == null) {
+                return
+            }
+            yaw = RotationUtils.getRotationsEntity(LiquidBounce.combatManager.target).yaw
+        }
+    }
+
+    private fun getAngleDifference(a: Float, b: Float): Float {
+        val aAngle = (a % 360).let {
+            if (it > 180) it - 360 else it
+        }
+        val bAngle = (b % 360).let {
+            if (it > 180) it - 360 else it
+        }
+        return aAngle - bAngle
     }
 
     private fun canStrafe(target: EntityLivingBase?): Boolean {
