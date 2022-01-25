30f5a808752b58035fb3d527cf0ace60cc71a1b2
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-01-25 22:19:48 +0800

    bruh

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Spider.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Spider.kt
@@ -9,6 +9,7 @@ import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.value.FloatValue
 import net.ccbluex.liquidbounce.value.IntegerValue
 import net.ccbluex.liquidbounce.value.ListValue
+import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.minecraft.network.play.client.C03PacketPlayer
 import net.minecraft.network.play.client.C03PacketPlayer.*
 import net.minecraft.block.BlockAir
