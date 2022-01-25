639b788a02a638be24ae85341b15e5edb152ebbd
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-01-25 22:17:16 +0800

    AAC4 Spider && no height limit(set to 0)

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Spider.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Spider.kt
@@ -9,19 +9,24 @@ import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.value.FloatValue
 import net.ccbluex.liquidbounce.value.IntegerValue
 import net.ccbluex.liquidbounce.value.ListValue
+import net.minecraft.network.play.client.C03PacketPlayer
+import net.minecraft.network.play.client.C03PacketPlayer.*
 import net.minecraft.block.BlockAir
 import net.minecraft.util.AxisAlignedBB
+import kotlin.math.cos
+import kotlin.math.sin
 
 @ModuleInfo(name = "Spider", category = ModuleCategory.MOVEMENT)
 class Spider : Module() {
 
-    private val modeValue = ListValue("Mode", arrayOf("Collide", "Motion"/*,"AAC4"*/), "Collide")
-    private val heightValue = IntegerValue("Height", 2, 1, 10)
+    private val modeValue = ListValue("Mode", arrayOf("Collide", "Motion", "AAC4"), "Collide")
+    private val heightValue = IntegerValue("Height", 2, 0, 10)
     private val motionValue = FloatValue("Motion", 0.42F, 0.1F, 1F).displayable { modeValue.equals("Motion") }
 
     private var startHeight = 0.0
     private var groundHeight = 0.0
     private var modifyBB = false
+    private var glitch = false
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
@@ -33,11 +38,14 @@ class Spider : Module() {
             modifyBB = false
             return
         }
+        if(modeValue.get()=="AAC4" && (mc.thePlayer.motionY < 0.0 || mc.thePlayer.onGround)) {
+            glitch = true
+        }
 
         modifyBB = true
 
         when (modeValue.get().lowercase()) {
-            "collide" -> {
+            "collide","aac4" -> {
                 if (mc.thePlayer.onGround) {
                     mc.thePlayer.jump()
                     groundHeight = mc.thePlayer.posY
@@ -48,17 +56,28 @@ class Spider : Module() {
             }
         }
     }
+    
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
+        val packet = event.packet
+        if (packet is C03PacketPlayer && glitch) {
+            glitch = false
+            val yaw = MovementUtils.direction.toFloat()
+            packet.x = packet.x - sin(yaw) * 0.00000001
+            packet.z = packet.z + cos(yaw) * 0.00000001
+        }
+    }
 
     @EventTarget
     fun onBlockBB(event: BlockBBEvent) {
-        if (!mc.thePlayer.isCollidedHorizontally || !mc.gameSettings.keyBindForward.pressed || mc.thePlayer.posY - heightValue.get()> startHeight) {
+        if (!mc.thePlayer.isCollidedHorizontally || !mc.gameSettings.keyBindForward.pressed || (mc.thePlayer.posY - heightValue.get() > startHeight && heightValue.get() > 0)) {
             return
         }
-        if (!modifyBB || mc.thePlayer.motionY> 0.0) return
+        if (!modifyBB || mc.thePlayer.motionY > 0.0) return
 
         when (modeValue.get().lowercase()) {
-            "collide" -> {
-                if (event.block is BlockAir && event.y <= mc.thePlayer.posY && event.y> groundHeight - 0.0156249 && event.y <groundHeight + 0.0156249) {
+            "collide","aac4" -> {
+                if (event.block is BlockAir && event.y <= mc.thePlayer.posY && event.y > groundHeight - 0.0625 && event.y < groundHeight + 0.0625) {
                     event.boundingBox = AxisAlignedBB.fromBounds(event.x.toDouble(), event.y.toDouble(), event.z.toDouble(),
                         event.x + 1.0, event.y + 1.0, event.z + 1.0)
                 }
