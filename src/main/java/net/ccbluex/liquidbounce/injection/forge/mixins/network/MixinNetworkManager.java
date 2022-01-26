7f15235a425daa43d9288308bf7d3ce668c8a65b
liulihaocai <liulihaocaiqwq@gmail.com>
2022-01-26 10:32:45 +0800

    readd proxy feature

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/network/MixinNetworkManager.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/network/MixinNetworkManager.java
@@ -5,24 +5,39 @@
  */
 package net.ccbluex.liquidbounce.injection.forge.mixins.network;
 
-import io.netty.channel.ChannelHandlerContext;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import net.ccbluex.liquidbounce.LiquidBounce;
 import net.ccbluex.liquidbounce.event.PacketEvent;
 import net.ccbluex.liquidbounce.features.module.modules.client.Animations;
+import net.ccbluex.liquidbounce.features.special.ProxyManager;
 import net.ccbluex.liquidbounce.utils.PacketUtils;
+import net.minecraft.network.EnumPacketDirection;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
+import net.minecraft.util.*;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.net.InetAddress;
+import java.net.Proxy;
+
 @Mixin(NetworkManager.class)
 public abstract class MixinNetworkManager {
-//    @Shadow
-//    @Final
-//    public static LazyLoadBase<NioEventLoopGroup> CLIENT_NIO_EVENTLOOP;
+    @Shadow
+    @Final
+    public static LazyLoadBase<NioEventLoopGroup> CLIENT_NIO_EVENTLOOP;
 
     /**
      * show player head in tab bar
@@ -60,38 +75,37 @@ public abstract class MixinNetworkManager {
         }
     }
 
-//    @Overwrite
-//    public static NetworkManager createNetworkManagerAndConnect(InetAddress address, int serverPort, boolean useNativeTransport) {
-//        final NetworkManager networkmanager = new NetworkManager(EnumPacketDirection.CLIENTBOUND);
-//
-//        Bootstrap bootstrap=new Bootstrap();
-//
-//        EventLoopGroup eventLoopGroup;
-//        Proxy proxy=ProxyManager.INSTANCE.getProxy();
-//        if(proxy.type().equals(Proxy.Type.DIRECT)){
-//            eventLoopGroup=CLIENT_NIO_EVENTLOOP.getValue();
-//            bootstrap.channel(NioSocketChannel.class);
-//        }else {
-//            if(!Epoll.isAvailable()||!useNativeTransport){
-//                System.out.println("Something goes wrong! Maybe you can disable proxy. [Epoll="+Epoll.isAvailable()+", UNT="+useNativeTransport+"]");
-//            }
-//            eventLoopGroup=new OioEventLoopGroup(0, (new ThreadFactoryBuilder()).setNameFormat("Netty Client IO #%d").setDaemon(true).build());
-//            bootstrap.channelFactory(new ProxyOioChannelFactory(proxy));
-//        }
-//
-//        bootstrap.group(eventLoopGroup).handler(new ChannelInitializer<Channel>() {
-//            protected void initChannel(Channel channel) {
-//                try {
-//                    channel.config().setOption(ChannelOption.TCP_NODELAY, true);
-//                } catch (ChannelException var3) {
-//                    var3.printStackTrace();
-//                }
-//                channel.pipeline().addLast("timeout", new ReadTimeoutHandler(30)).addLast("splitter", new MessageDeserializer2()).addLast("decoder", new MessageDeserializer(EnumPacketDirection.CLIENTBOUND)).addLast("prepender", new MessageSerializer2()).addLast("encoder", new MessageSerializer(EnumPacketDirection.SERVERBOUND)).addLast("packet_handler", networkmanager);
-//            }
-//        });
-//
-//        bootstrap.connect(address, serverPort).syncUninterruptibly();
-//
-//        return networkmanager;
-//    }
+    @Inject(method = "createNetworkManagerAndConnect", at = @At("HEAD"), cancellable = true)
+    private static void createNetworkManagerAndConnect(InetAddress address, int serverPort, boolean useNativeTransport, CallbackInfoReturnable<NetworkManager> cir) {
+        if(!ProxyManager.INSTANCE.isEnable()) {
+            return;
+        }
+        final NetworkManager networkmanager = new NetworkManager(EnumPacketDirection.CLIENTBOUND);
+
+        Bootstrap bootstrap = new Bootstrap();
+
+        EventLoopGroup eventLoopGroup;
+        Proxy proxy = ProxyManager.INSTANCE.getProxyInstance();
+        if(!Epoll.isAvailable() || !useNativeTransport){
+            System.out.println("Something goes wrong! Maybe you can disable proxy. [Epoll="+ Epoll.isAvailable()+", UNT="+useNativeTransport+"]");
+        }
+        eventLoopGroup = new OioEventLoopGroup(0, (new ThreadFactoryBuilder()).setNameFormat("Netty Client IO #%d").setDaemon(true).build());
+        bootstrap.channelFactory(new ProxyManager.ProxyOioChannelFactory(proxy));
+
+        bootstrap.group(eventLoopGroup).handler(new ChannelInitializer<Channel>() {
+            protected void initChannel(Channel channel) {
+                try {
+                    channel.config().setOption(ChannelOption.TCP_NODELAY, true);
+                } catch (ChannelException var3) {
+                    var3.printStackTrace();
+                }
+                channel.pipeline().addLast("timeout", new ReadTimeoutHandler(30)).addLast("splitter", new MessageDeserializer2()).addLast("decoder", new MessageDeserializer(EnumPacketDirection.CLIENTBOUND)).addLast("prepender", new MessageSerializer2()).addLast("encoder", new MessageSerializer(EnumPacketDirection.SERVERBOUND)).addLast("packet_handler", networkmanager);
+            }
+        });
+
+        bootstrap.connect(address, serverPort).syncUninterruptibly();
+
+        cir.setReturnValue(networkmanager);
+        cir.cancel();
+    }
 }
