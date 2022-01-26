e2bdc4a2576a86ebdab5df8d7d49b3de1c14838f
liulihaocai <liulihaocaiqwq@gmail.com>
2022-01-26 12:26:38 +0800

    fix proxy compat with viaforge

--- src/main/java/net/ccbluex/liquidbounce/file/configs/SpecialConfig.kt
+++ src/main/java/net/ccbluex/liquidbounce/file/configs/SpecialConfig.kt
@@ -5,12 +5,14 @@ import com.google.gson.JsonParser
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.features.special.AntiForge
 import net.ccbluex.liquidbounce.features.special.AutoReconnect
+import net.ccbluex.liquidbounce.features.special.ProxyManager
 import net.ccbluex.liquidbounce.features.special.ServerSpoof
 import net.ccbluex.liquidbounce.file.FileConfig
 import net.ccbluex.liquidbounce.file.FileManager
 import net.ccbluex.liquidbounce.ui.client.GuiBackground
 import net.ccbluex.liquidbounce.ui.client.altmanager.GuiAltManager
 import java.io.File
+import java.net.Proxy
 
 class SpecialConfig(file: File) : FileConfig(file) {
     var useGlyphFontRenderer = false
@@ -30,6 +32,9 @@ class SpecialConfig(file: File) : FileConfig(file) {
         GuiBackground.particles = false
         GuiAltManager.randomAltField.text = "F%nD%nP%n_%s%s%s"
         useGlyphFontRenderer = false
+        ProxyManager.isEnable = false
+        ProxyManager.proxy = "127.0.0.1:10808"
+        ProxyManager.proxyType = Proxy.Type.SOCKS
 
         if (json.has("prefix")) {
             LiquidBounce.commandManager.prefix = json.get("prefix").asCharacter
@@ -69,6 +74,19 @@ class SpecialConfig(file: File) : FileConfig(file) {
                 ServerSpoof.address = jsonValue.get("address").asString
             }
         }
+        if (json.has("proxy")) {
+            val jsonValue = json.getAsJsonObject("proxy")
+
+            if (jsonValue.has("enable")) {
+                ProxyManager.isEnable = jsonValue.get("enable").asBoolean
+            }
+            if (jsonValue.has("address")) {
+                ProxyManager.proxy = jsonValue.get("address").asString
+            }
+            if (jsonValue.has("type")) {
+                ProxyManager.proxyType = Proxy.Type.valueOf(jsonValue.get("type").asString.uppercase())
+            }
+        }
         if (json.has("background")) {
             val jsonValue = json.getAsJsonObject("background")
 
@@ -100,6 +118,12 @@ class SpecialConfig(file: File) : FileConfig(file) {
         serverSpoofJson.addProperty("enable", ServerSpoof.enable)
         serverSpoofJson.addProperty("address", ServerSpoof.address)
         json.add("serverspoof", serverSpoofJson)
+
+        val proxyJson = JsonObject()
+        proxyJson.addProperty("enable", ProxyManager.isEnable)
+        proxyJson.addProperty("address", ProxyManager.proxy)
+        proxyJson.addProperty("type", ProxyManager.proxyType.name)
+        json.add("proxy", proxyJson)
 
         val backgroundJson = JsonObject()
         backgroundJson.addProperty("enable", GuiBackground.enabled)
