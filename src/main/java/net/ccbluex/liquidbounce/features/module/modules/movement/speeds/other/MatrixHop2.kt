939141a474cea302b6b2c85407e1f7af91bf18f3
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-02-02 16:21:13 +0800

    Update MatrixHop2.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/MatrixHop2.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/MatrixHop2.kt
@@ -17,32 +17,32 @@ class MatrixHop2 : SpeedMode("MatrixHop2") {
         mc.thePlayer.jumpMovementFactor = 0.0266f
         if(!mc.thePlayer.onGround) {
             mc.gameSettings.keyBindJump.pressed = mc.gameSettings.keyBindJump.isKeyDown
-			      if(MovementUtils.getSpeed() < 0.217) {
-				        MovementUtils.strafe(0.217)
-				        mc.thePlayer.jumpMovementFactor = 0.0269f
-			      }
-		    }
+	    if(MovementUtils.getSpeed() < 0.217) {
+		MovementUtils.strafe(0.217f)
+		mc.thePlayer.jumpMovementFactor = 0.0269f
+	    }
+	}
         if(mc.thePlayer.motionY<0) {
-			      mc.timer.timerSpeed =1.16f
-			  if(mc.thePlayer.fallDistance > 1.4)
-				    mc.timer.timerSpeed =1.0f
-		    }else{
-			      mc.timer.timerSpeed =0.95f
-		    }
-		    if(mc.thePlayer.onGround && MovementUtils.isMoving()) {
+	    mc.timer.timerSpeed =1.16f
+	    if(mc.thePlayer.fallDistance > 1.4)
+	        mc.timer.timerSpeed =1.0f
+	}else{
+	    mc.timer.timerSpeed =0.95f
+	}
+	if(mc.thePlayer.onGround && MovementUtils.isMoving()) {
             mc.gameSettings.keyBindJump.pressed = false
-			      mc.timer.timerSpeed =1.3f
-			      if(MovementUtils.getSpeed() < 0.0776)
-				        MovementUtils.strafe(0.0776)
-			      mc.thePlayer.jump()
-			      if(mc.thePlayer.movementInput.moveStrafe == 0) {
-				        MovementUtils.INSTANCE.strafe(MovementUtils.getSpeed()*1.0071)
-			      }
-		    }else if(!MovementUtils.isMoving()) {
-			      mc.timer.timerSpeed =1.0f
-		    }
-		    if(MovementUtils.getSpeed() < 0.22)
-			      MovementUtils.strafe()
+            mc.timer.timerSpeed =1.3f
+	    if(MovementUtils.getSpeed() < 0.0776)
+	        MovementUtils.strafe(0.0776f)
+		mc.thePlayer.jump()
+	    if(mc.thePlayer.movementInput.moveStrafe <= 0.01) {
+		MovementUtils.strafe((MovementUtils.getSpeed()*1.0071).toFloat())
+	    }
+	}else if(!MovementUtils.isMoving()) {
+	    mc.timer.timerSpeed =1.0f
+	}
+	if(MovementUtils.getSpeed() < 0.22)
+	    MovementUtils.strafe()
     }
     
     override fun onDisable() {
@@ -54,20 +54,20 @@ class MatrixHop2 : SpeedMode("MatrixHop2") {
       
         if (!veloBoostValue.get()) return
       
-        if(packet instanceof S12PacketEntityVelocity) {
-			      if (mc.thePlayer == null || mc.theWorld.getEntityByID(packet.getEntityID()) != mc.thePlayer) {
-				        return
-			      }
-			      event.cancelEvent()
+        if(packet is S12PacketEntityVelocity) {
+	    if (mc.thePlayer == null || (mc.theWorld?.getEntityByID(packet.entityID) ?: return) != mc.thePlayer) {
+	        return
+	    }
+	    event.cancelEvent()
             
-			      recX = packet.motionX/8000.0
-			      recZ = packet.motionZ/8000.0
-			      if(Math.sqrt(recX*recX+recZ*recZ) > MovementUtils.getSpeed()) {
-				        MovementUtils.strafe(Math.sqrt(recX*recX+recZ*recZ))
+	    recX = packet.motionX/8000.0
+	    recZ = packet.motionZ/8000.0
+	    if(Math.sqrt(recX*recX+recZ*recZ) > MovementUtils.getSpeed()) {
+	        MovementUtils.strafe(Math.sqrt(recX*recX+recZ*recZ).toFloat())
                 mc.thePlayer.motionY = packet.motionY/8000.0
             }
           
-			      MovementUtils.strafe(MovementUtils.getSpeed()*1.15)
-		    }
+	    MovementUtils.strafe((MovementUtils.getSpeed()*1.15).toFloat())
+	}
     }
 }
