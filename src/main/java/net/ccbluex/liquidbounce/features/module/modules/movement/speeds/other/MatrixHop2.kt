e08bf11f4e87be579aea61385f94fd2244993a89
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-02-02 16:10:50 +0800

    Create MatrixHop2.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/MatrixHop2.kt
@@ -0,0 +1,73 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.other
+
+import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.FloatValue
+import net.minecraft.network.play.server.S12PacketEntityVelocity
+import kotlin.math.sqrt
+
+class MatrixHop2 : SpeedMode("MatrixHop2") {
+    val veloBoostValue = BoolValue("MatrixVelocBoost", true)
+    private var recX = 0.0
+    private var recZ = 0.0
+  
+    override fun onUpdate() {
+        mc.thePlayer.jumpMovementFactor = 0.0266f
+        if(!mc.thePlayer.onGround) {
+            mc.gameSettings.keyBindJump.pressed = mc.gameSettings.keyBindJump.isKeyDown
+			      if(MovementUtils.getSpeed() < 0.217) {
+				        MovementUtils.strafe(0.217)
+				        mc.thePlayer.jumpMovementFactor = 0.0269f
+			      }
+		    }
+        if(mc.thePlayer.motionY<0) {
+			      mc.timer.timerSpeed =1.16f
+			  if(mc.thePlayer.fallDistance > 1.4)
+				    mc.timer.timerSpeed =1.0f
+		    }else{
+			      mc.timer.timerSpeed =0.95f
+		    }
+		    if(mc.thePlayer.onGround && MovementUtils.isMoving()) {
+            mc.gameSettings.keyBindJump.pressed = false
+			      mc.timer.timerSpeed =1.3f
+			      if(MovementUtils.getSpeed() < 0.0776)
+				        MovementUtils.strafe(0.0776)
+			      mc.thePlayer.jump()
+			      if(mc.thePlayer.movementInput.moveStrafe == 0) {
+				        MovementUtils.INSTANCE.strafe(MovementUtils.getSpeed()*1.0071)
+			      }
+		    }else if(!MovementUtils.isMoving()) {
+			      mc.timer.timerSpeed =1.0f
+		    }
+		    if(MovementUtils.getSpeed() < 0.22)
+			      MovementUtils.strafe()
+    }
+    
+    override fun onDisable() {
+        mc.timer.timerSpeed = 1f
+    }
+    
+    override fun onPacket(event: PacketEvent) {
+        val packet = event.packet
+      
+        if (!veloBoostValue.get()) return
+      
+        if(packet instanceof S12PacketEntityVelocity) {
+			      if (mc.thePlayer == null || mc.theWorld.getEntityByID(packet.getEntityID()) != mc.thePlayer) {
+				        return
+			      }
+			      event.cancelEvent()
+            
+			      recX = packet.motionX/8000.0
+			      recZ = packet.motionZ/8000.0
+			      if(Math.sqrt(recX*recX+recZ*recZ) > MovementUtils.getSpeed()) {
+				        MovementUtils.strafe(Math.sqrt(recX*recX+recZ*recZ))
+                mc.thePlayer.motionY = packet.motionY/8000.0
+            }
+          
+			      MovementUtils.strafe(MovementUtils.getSpeed()*1.15)
+		    }
+    }
+}
