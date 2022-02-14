dd945a7355c1fab375132eebb9bb3fbce0a92048
liulihaocai <liulihaocaiqwq@gmail.com>
2022-02-14 22:37:26 +0800

    make NoRotateSet still bypass on some servers

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/NoRotateSet.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/NoRotateSet.kt
@@ -10,8 +10,10 @@ import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.utils.Rotation
 import net.ccbluex.liquidbounce.utils.RotationUtils
 import net.ccbluex.liquidbounce.value.BoolValue
+import net.minecraft.network.play.client.C03PacketPlayer
 import net.minecraft.network.play.client.C03PacketPlayer.C05PacketPlayerLook
 import net.minecraft.network.play.server.S08PacketPlayerPosLook
 
@@ -19,9 +21,12 @@ import net.minecraft.network.play.server.S08PacketPlayerPosLook
 class NoRotateSet : Module() {
 
     private val noLoadingValue = BoolValue("NoLoading", true)
-    private val confirmValue = BoolValue("Confirm", true)
+    private val confirmValue = BoolValue("Confirm", false)
+    private val overwriteTeleportValue = BoolValue("OverwriteTeleport", false)
     private val illegalRotationValue = BoolValue("ConfirmIllegalRotation", false)
     private val noZeroValue = BoolValue("NoZero", false)
+
+    private var lastRotation: Rotation? = null
 
     @EventTarget
     fun onPacket(event: PacketEvent) {
@@ -42,8 +47,15 @@ class NoRotateSet : Module() {
                 }
             }
 
+            if(!overwriteTeleportValue.get()) {
+                lastRotation = Rotation(packet.getYaw(), packet.getPitch())
+            }
             packet.yaw = mc.thePlayer.rotationYaw
             packet.pitch = mc.thePlayer.rotationPitch
+        } else if (lastRotation != null && packet is C03PacketPlayer && packet.rotating) {
+            packet.yaw = lastRotation!!.yaw
+            packet.pitch = lastRotation!!.pitch
+            lastRotation = null
         }
     }
 }
