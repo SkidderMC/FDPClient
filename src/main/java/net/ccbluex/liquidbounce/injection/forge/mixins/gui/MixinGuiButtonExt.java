81eca8036d7dd0619ce370e214be4d85128549a4
liulihaocai <liulihaocaiqwq@gmail.com>
2022-02-19 22:26:51 +0800

    maybe fix Button issues

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiButtonExt.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiButtonExt.java
@@ -1,36 +1,32 @@
 package net.ccbluex.liquidbounce.injection.forge.mixins.gui;
 
-import net.ccbluex.liquidbounce.LiquidBounce;
-import net.ccbluex.liquidbounce.features.module.modules.client.HUD;
-import net.ccbluex.liquidbounce.features.module.modules.client.button.AbstractButtonRenderer;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraftforge.fml.client.config.GuiButtonExt;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiButtonExt.class)
 public abstract class MixinGuiButtonExt extends MixinGuiButton {
 
-    private final AbstractButtonRenderer buttonRenderer = LiquidBounce.moduleManager.getModule(HUD.class).getButtonRenderer((GuiButton)(Object)this);
-
     /**
      * @author CCBlueX
      */
-    @Overwrite
-    public void drawButton(Minecraft mc, int mouseX, int mouseY) {
-        if (visible) {
-            hovered = (mouseX >= this.xPosition && mouseY >= this.yPosition &&
-                    mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height);
-
+    @Override
+    public void drawButton(Minecraft mc, int mouseX, int mouseY, CallbackInfo ci) {
+        if(this.buttonRenderer != null) {
+            if(!visible) {
+                return;
+            }
+            this.hovered = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
             buttonRenderer.render(mouseX, mouseY, mc);
 
             mc.getTextureManager().bindTexture(buttonTextures);
             mouseDragged(mc, mouseX, mouseY);
+            GlStateManager.resetColor();
 
             buttonRenderer.drawButtonText(mc);
-            GlStateManager.resetColor();
+            ci.cancel();
         }
     }
 }
