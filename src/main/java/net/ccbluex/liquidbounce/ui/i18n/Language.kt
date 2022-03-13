d2e4483c09a221c8fe458d0f0f0e6d300b5a98fb
liulihaocai <liulihaocaiqwq@gmail.com>
2022-03-13 10:33:06 +0800

    sth

--- src/main/java/net/ccbluex/liquidbounce/ui/i18n/Language.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/i18n/Language.kt
@@ -1,26 +1,45 @@
 package net.ccbluex.liquidbounce.ui.i18n
 
+import java.io.InputStream
 import java.io.InputStreamReader
 import java.util.*
 
-class Language(val locale: String, defaultLocale: String) {
+class Language(val locale: String) {
 
     private val translateMap = HashMap<String, String>()
 
-    constructor(defaultLocale: String) : this(defaultLocale, defaultLocale)
-
     init {
-        if (defaultLocale != locale) {
-            read(defaultLocale)
+        read(locale)
+    }
+
+    private fun find(): InputStream {
+        val split = locale.replace("-", "_").split("_")
+
+        if(split.size > 1) {
+            val str = split[0].lowercase() + "-" + split[1].uppercase()
+            println("LOCALE $str")
+            LanguageManager::class.java.classLoader.getResourceAsStream("assets/minecraft/fdpclient/translations/${str}/source.properties")?.let {
+                return it
+            }
         }
 
-        read(locale)
+        val str = split[0].lowercase()
+        println("LOCALE $str")
+        LanguageManager::class.java.classLoader.getResourceAsStream("assets/minecraft/fdpclient/translations/${str}/source.properties")?.let {
+            return it
+        }
+
+        LanguageManager::class.java.classLoader.getResourceAsStream("assets/minecraft/fdpclient/translations/source.properties")?.let {
+            return it
+        }
+
+        throw IllegalStateException("Can't find language file! Try sync gitsubmodule if this is a custom build!")
     }
 
     private fun read(locale: String) {
         val prop = Properties()
 
-        prop.load(InputStreamReader(LanguageManager::class.java.classLoader.getResourceAsStream("assets/minecraft/fdpclient/locale/$locale.lang"), Charsets.UTF_8))
+        prop.load(InputStreamReader(find(), Charsets.UTF_8))
 
         for ((key, value) in prop.entries) {
             if (key is String && value is String) {
