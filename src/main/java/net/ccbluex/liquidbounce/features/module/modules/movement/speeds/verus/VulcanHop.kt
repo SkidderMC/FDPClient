926e0d4ec3bbd5e64a61a8d468f3a19825e8fa7e
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-03-19 07:38:59 +0800

    better

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/verus/VulcanHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/verus/VulcanHop.kt
@@ -6,23 +6,30 @@ import net.ccbluex.liquidbounce.utils.MovementUtils
 class VulcanHop : SpeedMode("VulcanHop") {
 
     override fun onUpdate() {
-        mc.thePlayer.jumpMovementFactor = if((mc.gameSettings.keyBindForward.pressed || mc.gameSettings.keyBindBack.pressed)
-            && (mc.gameSettings.keyBindLeft.pressed || mc.gameSettings.keyBindRight.pressed)) {
-            0.0263f
-        } else {
-            0.0265f
+        mc.timer.timerSpeed = 1.00f
+        if (Math.abs(mc.thePlayer.movementInput.moveStrafe) == 0) {
+            mc.thePlayer.jumpMovementFactor = 0.0265f
+        }else {
+            mc.thePlayer.jumpMovementFactor = 0.0244f
         }
         if (!mc.thePlayer.onGround) {
             mc.gameSettings.keyBindJump.pressed = mc.gameSettings.keyBindJump.isKeyDown
         }
-        if (MovementUtils.getSpeed() < 0.215) {
-            MovementUtils.strafe()
+        if (MovementUtils.getSpeed() < 0.215f) {
+            MovementUtils.strafe(0.215f)
         }
         if (mc.thePlayer.onGround && MovementUtils.isMoving()) {
+            mc.timer.timerSpeed = 1.25f
             mc.gameSettings.keyBindJump.pressed = false
             mc.thePlayer.jump()
-            mc.thePlayer.motionX *= 0.99
-            mc.thePlayer.motionZ *= 0.99 // Speed [C] checks
+            MovementUtils.strafe()
+			if(MovementUtils.getSpeed() < 0.5f) {
+			    MovementUtils.strafe(0.485f)
+			}
+        }else if (!MovementUtils.isMoving()) {
+            mc.timer.timerSpeed = 1.00f
+            mc.thePlayer.motionX = 0.0
+            mc.thePlayer.motionZ = 0.0
         }
     }
 }
