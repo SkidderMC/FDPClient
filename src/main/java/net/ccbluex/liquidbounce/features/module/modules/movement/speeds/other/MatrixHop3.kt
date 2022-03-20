2eabfd3b1ccb2e18d7cfc75a7c9aeb0d7917f9db
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-03-20 11:08:37 +0800

    I Love The Great FireWall bruh :(

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/MatrixHop3.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/MatrixHop3.kt
@@ -6,6 +6,12 @@ import net.ccbluex.liquidbounce.utils.MovementUtils
 class MatrixHop3 : SpeedMode("Matrix6.7.0") {
 
     override fun onUpdate() {
+        if(!mc.thePlayer.onGround) {
+			if(mc.thePlayer.motionY > 0) {
+				mc.thePlayer.motionY -= 0.0005
+			}
+			mc.thePlayer.motionY -= 0.0094001145141919810
+		}
         if (!mc.thePlayer.onGround) {
             mc.gameSettings.keyBindJump.pressed = mc.gameSettings.keyBindJump.isKeyDown
             if (MovementUtils.getSpeed() < 0.218) {
@@ -20,6 +26,7 @@ class MatrixHop3 : SpeedMode("Matrix6.7.0") {
         if (mc.thePlayer.onGround && MovementUtils.isMoving()) {
             mc.gameSettings.keyBindJump.pressed = false
             mc.thePlayer.jump()
+            mc.thePlayer.motionY = 0.41050001145141919810
             if (Math.abs(mc.thePlayer.movementInput.moveStrafe) < 0.1) {
                 MovementUtils.strafe((MovementUtils.getSpeed() * 1.00299601145141919810).toFloat())
             }
