518ae99ff24b7aead23603fe3c1de23b2e060746
FengXiao <82002740+FengXiaoSkidGod@users.noreply.github.com>
2022-04-13 10:18:49 +0800

    Update PlayerUtils.kt

--- src/main/java/net/ccbluex/liquidbounce/utils/PlayerUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/PlayerUtils.kt
@@ -1,5 +1,8 @@
 package net.ccbluex.liquidbounce.utils
 
+import net.ccbluex.liquidbounce.utils.MinecraftInstance.mc
+import net.minecraft.util.AxisAlignedBB
+
 object PlayerUtils {
     fun randomUnicode(str: String): String {
         val stringBuilder = StringBuilder()
@@ -11,5 +14,22 @@ object PlayerUtils {
             }
         }
         return stringBuilder.toString()
+    }
+    fun isBlockUnder(): Boolean {
+        if (mc.thePlayer.posY < 0) return false
+        var off = 0
+        while (off < mc.thePlayer.posY.toInt() + 2) {
+            val bb: AxisAlignedBB = mc.thePlayer.getEntityBoundingBox()
+                .offset(0.0, -off.toDouble(), 0.0)
+            if (mc.theWorld.getCollidingBoundingBoxes(
+                    mc.thePlayer,
+                    bb
+                ).isNotEmpty()
+            ) {
+                return true
+            }
+            off += 2
+        }
+        return false
     }
 }
