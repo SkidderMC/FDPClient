78415c0e8b3ae68618b6b6407517f6deb3c8497f
XiGuaHanHan <63942405+xiguagege@users.noreply.github.com>
2022-05-09 21:52:16 +0800

    Fix

--- src/main/java/net/ccbluex/liquidbounce/utils/PlayerUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/PlayerUtils.kt
@@ -1,6 +1,9 @@
 package net.ccbluex.liquidbounce.utils
 
 import net.ccbluex.liquidbounce.utils.MinecraftInstance.mc
+import net.minecraft.item.ItemBucketMilk
+import net.minecraft.item.ItemFood
+import net.minecraft.item.ItemPotion
 import net.minecraft.util.AxisAlignedBB
 
 object PlayerUtils {
@@ -14,6 +17,12 @@ object PlayerUtils {
             }
         }
         return stringBuilder.toString()
+    }
+    fun isUsingFood(): Boolean {
+        val usingItem = mc.thePlayer.itemInUse.item
+        return if (mc.thePlayer.itemInUse != null) {
+            mc.thePlayer.isUsingItem && (usingItem is ItemFood || usingItem is ItemBucketMilk || usingItem is ItemPotion)
+        } else false
     }
     fun isBlockUnder(): Boolean {
         if (mc.thePlayer.posY < 0) return false
