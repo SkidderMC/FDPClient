3a05fda5a185d0483376b3fb51fec88a320b9c7f
Zywl <86253343+opZywl@users.noreply.github.com>
2022-05-21 19:21:00 -0300

    NoInvClose

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/NoInvClose.kt
@@ -0,0 +1,29 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/UnlegitMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.misc
+
+import net.ccbluex.liquidbounce.LiquidBounce
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.value.IntegerValue
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.ui.client.hud.element.elements.Notification
+import net.minecraft.network.play.server.S2EPacketCloseWindow
+import net.minecraft.client.gui.inventory.GuiInventory
+
+@ModuleInfo(name = "NoInvClose", category = ModuleCategory.MISC)
+class NoInvClose : Module() {
+  
+    @EventTarget
+    fun onPacket(event: PacketEvent){
+        if (mc.theWorld == null || mc.thePlayer == null) return
+        
+        if (event.packet is S2EPacketCloseWindow && mc.currentScreen is GuiInventory)
+            event.cancelEvent()
+    }
+}
