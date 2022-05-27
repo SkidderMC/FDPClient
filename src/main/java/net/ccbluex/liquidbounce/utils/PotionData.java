7bab58f779294a3e5a9335c89e061ac433aaf7cc
Zywl <86253343+opZywl@users.noreply.github.com>
2022-05-27 18:54:02 -0300

    [?}

--- src/main/java/net/ccbluex/liquidbounce/utils/PotionData.java
+++ src/main/java/net/ccbluex/liquidbounce/utils/PotionData.java
@@ -3,13 +3,14 @@ package net.ccbluex.liquidbounce.utils.render;
 import net.minecraft.potion.Potion;
 
 public class PotionData {
-    public final potion Potion;
+    public final Potion potion;
     public int maxTimer = 0;
     public float animationX = 0;
-    public final translate Translate;
+    public final Translate translate;
     public final int level;
     
     public PotionData(Potion potion, Translate translate, int level) {
+        
         this.potion = potion;
         this.translate = translate;
         this.level = level;
