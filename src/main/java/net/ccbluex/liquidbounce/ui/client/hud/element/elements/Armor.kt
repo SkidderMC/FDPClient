dad4cf3be3cbb2c21c5347e43c5f9cdb3c924ac3
Zywl <86253343+opZywl@users.noreply.github.com>
2022-05-27 12:58:34 -0300

    fix new armor other day

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Armor.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Armor.kt
@@ -9,17 +9,9 @@ import net.ccbluex.liquidbounce.ui.client.hud.element.Border
 import net.ccbluex.liquidbounce.ui.client.hud.element.Element
 import net.ccbluex.liquidbounce.ui.client.hud.element.ElementInfo
 import net.ccbluex.liquidbounce.ui.client.hud.element.Side
-import net.ccbluex.liquidbounce.ui.font.Fonts
-import net.ccbluex.liquidbounce.utils.render.RenderUtils
-import net.ccbluex.liquidbounce.utils.render.ColorUtils
 import net.ccbluex.liquidbounce.value.ListValue
-import net.ccbluex.liquidbounce.value.FloatValue
-import net.ccbluex.liquidbounce.value.IntegerValue
-
-import net.minecraft.block.material.Material
 import net.minecraft.client.renderer.GlStateManager
 import org.lwjgl.opengl.GL11
-import java.awt.Color
 
 /**
  * CustomHUD Armor element
@@ -28,68 +20,47 @@ import java.awt.Color
  */
 @ElementInfo(name = "Armor")
 class Armor(
-    x: Double = -8.0, 
-    y: Double = 57.0, 
+    x: Double = -8.0,
+    y: Double = 57.0,
     scale: Float = 1F,
     side: Side = Side(Side.Horizontal.MIDDLE, Side.Vertical.DOWN)
 ) : Element(x, y, scale, side) {
-    
-    private val modeValue = ListValue("Alignment", arrayOf("Horizontal", "Vertical"), "Horizontal")
-    private val colorModeValue = ListValue("Text-Color", arrayOf("Custom", "Rainbow"), "Custom")
-    private val brightnessValue = FloatValue("Brightness", 1f, 0f, 1f)
-    private val colorRedValue = IntegerValue("R", 0, 0, 255).displayable { colorModeValue.equals("Custom") }
-    private val colorGreenValue = IntegerValue("G", 160, 0, 255).displayable { colorModeValue.equals("Custom") }
-    private val colorBlueValue = IntegerValue("B", 255, 0, 255).displayable { colorModeValue.equals("Custom") }
-    private val newRainbowIndex = IntegerValue("NewRainbowOffset", 1, 1, 50)
-    private val saturationValue = FloatValue("Saturation", 0.9f, 0f, 1f)
-    private val speed = IntegerValue("AllSpeed", 0, 0, 400)
 
+    private val modeValue = ListValue("Alignment", arrayOf("Horizontal", "Vertical"), "Horizontal")
+    
     /**
      * Draw element
      */
-
-    override fun drawElement(): Border? {
-        var x2 = 0
+    override fun drawElement(partialTicks: Float): Border {
         if (mc.playerController.isNotCreative) {
-            GL11.glPushMatrix()
-
             val renderItem = mc.renderItem
-            val isInsideWater = mc.thePlayer.isInsideOfMaterial(Material.water)
 
             var x = 1
-            var i = 0
-            var y = if (isInsideWater) -10 else 0
-            val colorMode = colorModeValue.get()
-            val color = Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get())
-            val rainbow = colorMode.equals("Rainbow", ignoreCase = true)
-            for (index in 0..3) {
-                if(mc.thePlayer.inventory.armorInventory[index] != null)
-                    x2 += 20
-            }
-            RenderUtils.drawRect(-2f, -4f, 2f + x2, 29f, Color(50, 50, 50, 60))
+            var y = 0
+
+            val mode = modeValue.get()
 
             for (index in 3 downTo 0) {
-                val colorall = when {
-                    rainbow -> 0
-                    colorMode.equals("Rainbow", ignoreCase = true) -> ColorUtils.rainbow()
-                    else -> color
-                }
                 val stack = mc.thePlayer.inventory.armorInventory[index] ?: continue
-                RenderUtils.drawGradientSidewaysV(x.toDouble(), 0.0,x.toDouble() + 18 ,17.0,colorall,Color(140,140,140,40).rgb)
-                Fonts.font32.drawStringWithShadow(((stack.maxDamage - stack.itemDamage)).toString(),x.toFloat() + 4f,20f,colorall)
-                RenderUtils.drawRect(x.toFloat(),25f,x.toFloat() + 18f,26f,Color(140,140,140,220).rgb)
-                RenderUtils.drawRect(x.toFloat(),25f,x.toFloat() + (18f * (stack.maxDamage - stack.itemDamage) / stack.maxDamage),26f,colorall)
-                renderItem.renderItemIntoGUI(stack, x + 1, y)
-                x += 20
-                i += 1
+
+                renderItem.renderItemIntoGUI(stack, x, y)
+                renderItem.renderItemOverlays(mc.fontRendererObj, stack, x, y)
+                if (mode.equals("Horizontal", true)) {
+                    x += 18
+                } else if (mode.equals("Vertical", true)) {
+                    y += 18
+                }
             }
+
             GlStateManager.enableAlpha()
             GlStateManager.disableBlend()
             GlStateManager.disableLighting()
-            GlStateManager.disableCull()
-            GL11.glPopMatrix()
         }
-        return Border(-2f, -4f, 2f + 80, 29f)
 
+        return if (modeValue.equals("Horizontal")) {
+            Border(0F, 0F, 72F, 17F)
+        } else {
+            Border(0F, 0F, 18F, 72F)
+        }
     }
 }
