160936c773d51cb2a309277e88fbb72150c342fc
C00LC0D35 <88516494+C00LC0D35@users.noreply.github.com>
2022-06-07 19:39:16 -0400

    Update Criticals.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Criticals.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/Criticals.kt
@@ -28,7 +28,7 @@ import net.minecraft.stats.StatList
 @ModuleInfo(name = "Criticals", category = ModuleCategory.COMBAT)
 class Criticals : Module() {
 
-    val modeValue = ListValue("Mode", arrayOf("Packet", "NCPPacket", "MiPacket", "Hypixel", "Hypixel2", "VulcanSemi", "AACPacket", "AAC4.3.11OldHYT", "AAC5.0.4", "NoGround", "TPHop", "FakeCollide", "Mineplex", "More", "TestMinemora", "Motion", "Hover"), "packet")
+    val modeValue = ListValue("Mode", arrayOf("Packet", "NCPPacket", "MiPacket", "Hypixel", "Hypixel2", "VulcanSemi", "AACPacket", "AAC4.3.11OldHYT", "AAC5.0.4", "NoGround", "MiniPhase", "NanoPacket", "non-calculable", "invalid", "TPHop", "FakeCollide", "Mineplex", "More", "TestMinemora", "VerusSmart","Motion", "Hover"), "packet")
     private val motionValue = ListValue("MotionMode", arrayOf("RedeSkyLowHop", "Hop", "Jump", "LowJump", "MinemoraTest"), "Jump")
     private val hoverValue = ListValue("HoverMode", arrayOf("AAC4", "AAC4Other", "OldRedesky", "Normal1", "Normal2", "Minis", "Minis2", "TPCollide", "2b2t"), "AAC4")
     private val hoverNoFall = BoolValue("HoverNoFall", true).displayable { modeValue.equals("Hover") }
@@ -131,6 +131,16 @@ class Criticals : Module() {
                     }
                 }
                 
+                "verus" -> {
+                    attacks ++
+                    if (attacks > 4) {
+                        attacks = 0
+                        
+                        sendCriticalPacket(yOffset = 0.001, ground = true)
+                        sendCriticalPacket(ground = false)
+                    }
+                }
+                
                 "hypixel2" -> {
                     sendCriticalPacket(yOffset = 0.05250000001304, ground = false)
                     sendCriticalPacket(yOffset = 0.00150000001304, ground = false)
@@ -174,6 +184,34 @@ class Criticals : Module() {
                     mc.thePlayer.triggerAchievement(StatList.jumpStat)
                     sendCriticalPacket(xOffset = motionX / 3, yOffset = 0.20000004768372, zOffset = motionZ / 3, ground = false)
                     sendCriticalPacket(xOffset = motionX / 1.5, yOffset = 0.12160004615784, zOffset = motionZ / 1.5, ground = false)
+                }
+                
+                "miniphase" -> {
+                    sendCriticalPacket(yOffset = -0.0125, ground = false)
+                    sendCriticalPacket(yOffset =  0.01275, ground = false)
+                    sendCriticalPacket(yOffset = -0.00025, ground = false)
+                }
+
+                "nanopacket" -> {
+                    sendCriticalPacket(yOffset =  0.00973333333333, ground = false)
+                    sendCriticalPacket(yOffset =  0.001, ground = false)
+                    sendCriticalPacket(yOffset = -0.01200000000007, ground = false)
+                    sendCriticalPacket(yOffset = -0.0005, ground = false)
+
+                }
+
+                "non-calculable" -> {
+                    sendCriticalPacket(yOffset =  1E-5, ground = false)
+                    sendCriticalPacket(yOffset =  1E-7, ground = false)
+                    sendCriticalPacket(yOffset = -1E-6, ground = false)
+                    sendCriticalPacket(yOffset = -1E-4, ground = false)
+
+                }
+
+                "invalid" -> {
+                    sendCriticalPacket(yOffset =  1E+27, ground = false)
+                    sendCriticalPacket(yOffset = -1E+68, ground = false)
+                    sendCriticalPacket(yOffset =  1E+41, ground = false)
                 }
 
                 "tphop" -> {
