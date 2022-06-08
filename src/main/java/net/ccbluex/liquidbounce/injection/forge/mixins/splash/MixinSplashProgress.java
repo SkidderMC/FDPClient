de1b50a4134f60576b4d2fbf744e707faa4993ac
fdp <105464811+fdpmc@users.noreply.github.com>
2022-06-08 21:03:30 +0200

    re add?

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/splash/MixinSplashProgress.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/splash/MixinSplashProgress.java
@@ -10,15 +10,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value=SplashProgress.class, remap=false)
 public abstract class MixinSplashProgress {
 
-//    Should we force the splash screen to be shown?
-//
-//    @Shadow(aliases="SplashProgress", remap=false)
-//    private static boolean enabled;
-//
-//    @Inject(method="start", at=@At(value="FIELD", target="Lnet/minecraftforge/fml/client/SplashProgress;enabled:Z", opcode=178, remap=false, ordinal=0), remap=false, require=1, allow=1)
-//    private static void start(CallbackInfo callbackInfo) {
-//        enabled = true;
-//    }
+ 
+
+    @Shadow(aliases="SplashProgress", remap=false)
+    private static boolean enabled;
+
+    @Inject(method="start", at=@At(value="FIELD", target="Lnet/minecraftforge/fml/client/SplashProgress;enabled:Z", opcode=178, remap=false, ordinal=0), remap=false, require=1, allow=1)
+    private static void start(CallbackInfo callbackInfo) {
+       enabled = true;
+    }
 
     @Inject(method="finish", at=@At(value="INVOKE", target="Lorg/lwjgl/opengl/Drawable;makeCurrent()V", shift=At.Shift.AFTER, remap=false, ordinal=0), remap=false, cancellable=true, require=1, allow=1)
     private static void finish(CallbackInfo callbackInfo) {
