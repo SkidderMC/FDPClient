e83febfefc284d4959dbe34d656c702bdeced99d
C00LC0D35 <88516494+C00LC0D35@users.noreply.github.com>
2022-06-07 21:18:46 -0400

    Update Targets.kt

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Targets.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Targets.kt
@@ -332,7 +332,7 @@ class Targets : Element(-46.0, -40.0, 1F, Side(Side.Horizontal.MIDDLE, Side.Vert
         RenderUtils.quickDrawHead(target.skin, 0, 0, size, size)
         GL11.glPopMatrix()
 
-        font.drawString("${target.name}", 40, 13, Color.WHITE.rgb)
+        font.drawString("${target.name}", 40, 8, Color.WHITE.rgb)
 
         // 渐变血量条
         GL11.glEnable(3042)
@@ -340,8 +340,8 @@ class Targets : Element(-46.0, -40.0, 1F, Side(Side.Horizontal.MIDDLE, Side.Vert
         GL11.glBlendFunc(770, 771)
         GL11.glEnable(2848)
         GL11.glShadeModel(7425)
-        val stopPos = (13 + ((135 - font.getStringWidth(decimalFormat.format(target.maxHealth))) * (easingHP / target.maxHealth))).toInt()
-        for (i in 13..stopPos step 5) {
+        val stopPos = (40 + ((135 - font.getStringWidth(decimalFormat.format(target.maxHealth))) * (easingHP / target.maxHealth))).toInt()
+        for (i in 40..stopPos step 5) {
             val x1 = (i + 5).coerceAtMost(stopPos).toDouble()
             RenderUtils.quickDrawGradientSidewaysH(i.toDouble(), (13 + font.FONT_HEIGHT).toDouble(), x1, 45.0,
                 ColorUtils.hslRainbow(i, indexOffset = 10).rgb, ColorUtils.hslRainbow(x1.toInt(), indexOffset = 10).rgb)
