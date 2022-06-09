8a97ddea21de0e6f3b1fa6acd6ee05511f47508b
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-06-09 21:08:00 +0800

    wtf were those push requests

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/verus/VerusHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/verus/VerusHop.kt
@@ -5,36 +5,23 @@ import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.ccbluex.liquidbounce.value.BoolValue
 
 class VerusHop : SpeedMode("VerusHop") {
+    
+    private var wasTimer = false
 
-    private val timerBoost = BoolValue("TimerBoost",true)
-        
-    private var jumps = 0
-    private var lastY = 0.0
-
-    override fun onPreMotion() {
-        
-        if (MovementUtils.isMoving()) {
-            if (timerBoost.get() && (jumps >= 1)) {
-                mc.timer.timerSpeed = if (mc.thePlayer.motionY < 0) { 0.88f } else { 1.25f }
+    override fun onUpdate() {
+        if (wasTimer) {
+            mc.timer.timerSpeed = 1.00f
+            wasTimer = false
+        }
+        if (MovementUtils.isMoving() && mc.thePlayer.onGround) {
+            mc.thePlayer.jump()
+            if (!mc.thePlayer.isAirBorne) {
+                return //Prevent flag with Fly
             }
-            
-            when {
-                mc.thePlayer.onGround -> {
-                    MovementUtils.strafe(0.4825f)
-                    mc.thePlayer.motionY = (0.42f).toDouble()
-                    
-                    if (mc.thePlayer.posY == lastY) {
-                        jumps++
-                    } else {
-                        jumps = 0
-                    }
-                    
-                    lastY = mc.thePlayer.posY
-                }
-                else -> {
-                    MovementUtils.strafe(0.334f)
-                }
-            }
+            mc.timer.timerSpeed = 1.27f
+            wasTimer = true
+            MovementUtils.strafe(0.4821f)
+            mc.thePlayer.motionY = 0.42
         }
     }
 }
