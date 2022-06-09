a626a06ee16cb30f4b4952fe8b0a67dc91b36cfc
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-06-09 21:13:23 +0800

    Update VulcanHop.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/verus/VulcanHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/verus/VulcanHop.kt
@@ -4,9 +4,14 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMod
 import net.ccbluex.liquidbounce.utils.MovementUtils
 
 class VulcanHop : SpeedMode("VulcanHop") {
+	
+    private var wasTimer = false
 
     override fun onUpdate() {
-        mc.timer.timerSpeed = 1.00f
+	if (wasTimer) {
+            mc.timer.timerSpeed = 1.00f
+	    wasTimer = false
+	}
         if (Math.abs(mc.thePlayer.movementInput.moveStrafe) < 0.1f) {
             mc.thePlayer.jumpMovementFactor = 0.026499f
         }else {
@@ -15,16 +20,17 @@ class VulcanHop : SpeedMode("VulcanHop") {
         if (!mc.thePlayer.onGround) {
             mc.gameSettings.keyBindJump.pressed = mc.gameSettings.keyBindJump.isKeyDown
         }
-        if (MovementUtils.getSpeed() < 0.215f) {
+        if (MovementUtils.getSpeed() < 0.215f && !mc.thePlayer.onGround) {
             MovementUtils.strafe(0.215f)
         }
         if (mc.thePlayer.onGround && MovementUtils.isMoving()) {
-            mc.timer.timerSpeed = 1.25f
             mc.gameSettings.keyBindJump.pressed = false
             mc.thePlayer.jump()
 	    if (!mc.thePlayer.isAirBorne) {
                 return //Prevent flag with Fly
             }
+	    mc.timer.timerSpeed = 1.25f
+	    wasTimer = true
             MovementUtils.strafe()
 	    if(MovementUtils.getSpeed() < 0.5f) {
 	        MovementUtils.strafe(0.4849f)
