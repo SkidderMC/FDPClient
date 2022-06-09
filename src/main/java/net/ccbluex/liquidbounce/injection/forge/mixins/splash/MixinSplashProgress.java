e6cd7529309d08b49920b7601ada8f22b50cc8d3
Co丶Dynamic <contionability@gmail.com>
2022-06-09 22:14:27 +0800

    oki

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/splash/MixinSplashProgress.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/splash/MixinSplashProgress.java
@@ -11,15 +11,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MixinSplashProgress {
 
  
-//
-//    @Shadow(aliases="SplashProgress", remap=false)
-//    private static boolean enabled;
-//
-//    @Inject(method="start", at=@At(value="FIELD", target="Lnet/minecraftforge/fml/client/SplashProgress;enabled:Z", opcode=178, remap=false, ordinal=0), remap=false, require=1, allow=1)
-//    private static void start(CallbackInfo callbackInfo) {
-//       enabled = true;
-//    }
-//
+
+      @Shadow(aliases="SplashProgress", remap=false)
+      private static boolean enabled;
+
+      @Inject(method="start", at=@At(value="FIELD", target="Lnet/minecraftforge/fml/client/SplashProgress;enabled:Z", opcode=178, remap=false, ordinal=0), remap=false, require=1, allow=1)
+      private static void start(CallbackInfo callbackInfo) {
+         enabled = true;
+      }
+
     @Inject(method="finish", at=@At(value="INVOKE", target="Lorg/lwjgl/opengl/Drawable;makeCurrent()V", shift=At.Shift.AFTER, remap=false, ordinal=0), remap=false, cancellable=true, require=1, allow=1)
     private static void finish(CallbackInfo callbackInfo) {
         callbackInfo.cancel();
