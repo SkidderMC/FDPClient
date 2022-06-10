03b678d1e042ac01fed950b7b89d7217ccdf3b67
Co丶Dynamic <contionability@gmail.com>
2022-06-10 15:39:29 +0800

    ops

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/vulcan/VulcanHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/vulcan/VulcanHop.kt
@@ -2,6 +2,7 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.vulcan
 
 import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.minecraft.client.settings.GameSettings
 
 class VulcanHop : SpeedMode("VulcanHop") {
 	
