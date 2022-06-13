4a1e2217323ad9cececf583fc6347f5cfa71415a
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-06-13 12:43:34 +0800

    Update Sprint.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Sprint.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Sprint.kt
@@ -71,7 +71,12 @@ class Sprint : Module() {
                         mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SPRINTING))
                         mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SPRINTING))
                     }
-                    "minemora" -> mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY + 0.0000013, mc.thePlayer.posZ)
+                    "minemora" -> {
+                        if (mc.thePlayer.onGround) {
+                            mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY + 0.0000013, mc.thePlayer.posZ)
+                            mc.thePlayer.motionY = 0.0
+                        }
+                    }
                     "limitspeed" -> {
                         if (!allDirectionsLimitSpeedGround.get() || mc.thePlayer.onGround) {
                             MovementUtils.limitSpeedByPercent(allDirectionsLimitSpeedValue.get())
