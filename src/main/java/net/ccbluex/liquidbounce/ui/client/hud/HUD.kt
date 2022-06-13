f4a3a9f1baf3f379a994f948538bc0751cba4b1d
Zywl <86253343+opZywl@users.noreply.github.com>
2022-06-13 12:38:00 -0300

    wait i upload other repo LOL

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/HUD.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/HUD.kt
@@ -32,7 +32,7 @@ open class HUD : MinecraftInstance() {
          */
         fun createDefault(): HUD {
             val text1 = Text(x = 5.0, y = 8.0)
-            text1.displayString.set("Zywl")
+            text1.displayString.set("FDPClient")
             text1.colorModeValue.set("Rainbow")
             text1.rectValue.set("Logo")
             text1.rectColorModeValue.set("Rainbow")
