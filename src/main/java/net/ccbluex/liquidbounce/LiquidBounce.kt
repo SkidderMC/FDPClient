13af09929181e3621fcd68eef64474a7598f9b70
XinHai <102407179+afdera@users.noreply.github.com>
2022-06-17 14:34:16 +0800

    Update LiquidBounce.kt

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -48,7 +48,7 @@ object LiquidBounce {
     var CLIENTTEXT = "Waiting..."
     var Darkmode = true
     const val COLORED_NAME = "§7[§b!§7] §b§lFDPCLIENT §c» "
-    const val CLIENT_CREATOR = "CCBlueX & UnlegitMC TEAM"
+    const val CLIENT_CREATOR = "CCBlueX & UnlegitMC TEAMS"
     const val CLIENT_WEBSITE = "FDPClient.Club"
     const val MINECRAFT_VERSION = "1.8.9"
     const val VERSIONTYPE = "Preview"
