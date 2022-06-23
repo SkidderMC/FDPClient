3e38cc53fb45fd0e4f8cb8ac9b1d6ff054149a07
Co丶Dynamic <contionability@gmail.com>
2022-06-24 07:25:33 +0800

    Again
    * AAC3.1.6Gomme due to server issue
    * Already Patched 3 years ago
    
    * AAC3.3.13
    * Use HighJump instead
    
    * FreeHypixel
    * Due to server issue
    
    * HawkEye
    * Due to server issue

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/verus/Verus5Fly.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/verus/Verus5Fly.kt
@@ -7,6 +7,7 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.minecraft.block.BlockAir
 import net.minecraft.util.AxisAlignedBB
+import net.minecraft.client.settings.GameSettings
 
 class Verus5Fly : FlyMode("Verus5") {
     private var ticks = 0
@@ -16,6 +17,8 @@ class Verus5Fly : FlyMode("Verus5") {
     }
 
     override fun onMove(event: MoveEvent) {
+        mc.gameSettings.keyBindJump.pressed = false
+        mc.gameSettings.keyBindSneak.pressed = false
         if (ticks % 10 == 0 && mc.thePlayer.onGround) {
             MovementUtils.strafe(1f)
             event.y = 0.42
@@ -23,7 +26,7 @@ class Verus5Fly : FlyMode("Verus5") {
             mc.thePlayer.motionY = 0.0
             mc.timer.timerSpeed = 4f
         } else {
-            if (mc.gameSettings.keyBindJump.isKeyDown && ticks % 2 == 1) {
+            if (GameSettings.isKeyDown(mc.gameSettings.keyBindJump) && ticks % 2 == 1) {
                 event.y = 0.5
                 MovementUtils.strafe(0.48f)
                 fly.launchY += 0.5
