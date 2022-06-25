14c08e58c90599e08e4b37b131127dd842c82840
Administrator <lifeerwz@gmail.com>
2022-06-25 01:03:22 -0300

    test lmao

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ItemPhysics.java
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ItemPhysics.java
@@ -1,3 +1,8 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/UnlegitMC/FDPClient/
+ */
 package net.ccbluex.liquidbounce.features.module.modules.render;
 
 import net.ccbluex.liquidbounce.features.module.*;
