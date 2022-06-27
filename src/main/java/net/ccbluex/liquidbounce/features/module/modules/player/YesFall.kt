19249d230c60561ad8b8ca3d3bab202cbfd6b4c8
Administrator <lifeerwz@gmail.com>
2022-06-27 14:35:22 -0300

    a

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/YesFall.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/YesFall.kt
@@ -1,19 +1,21 @@
 package net.ccbluex.liquidbounce.features.module.modules.player
 
-import net.ccbluex.liquidbounce.event.UpdatePlayerEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
-import net.ccbluex.liquidbounce.utils.PacketUtils
-import com.google.common.eventbus.Subscribe
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.event.UpdatePlayerEvent
+import net.ccbluex.liquidbounce.utils.PacketUtils
+
+import com.google.common.eventbus.Subscribe
 import net.minecraft.network.play.client.C03PacketPlayer.C04PacketPlayerPosition
+import kotlin.math.roundToInt
 
 @ModuleInfo(name = "YesFall", category = ModuleCategory.PLAYER)
 class YesFall : Module() {
 
     @Subscribe
     fun onMove(e: UpdatePlayerEvent?) {
-        if (Math.round(mc.thePlayer.fallDistance) % 3 === 0) {
+        if (mc.thePlayer.fallDistance.roundToInt() % 3 === 0) {
             // rip i couldn't make it :((( :cry:
             //i am gaming :sunglasses:
             PacketUtils.sendPacketNoEvent(
@@ -25,16 +27,5 @@ class YesFall : Module() {
                 )
             )
         }
-    } /*@Override
-    public void onEnable() {
-        choices.getAllChoices().forEach(ClientUtils::fancyMessage);
-        ClientUtils.fancyMessage("all choices ^");
-        ClientUtils.fancyMessage(" ");
-        choices.getSelectedChoices().forEach(ClientUtils::fancyMessage);
-        ClientUtils.fancyMessage("selected choices ^");
-        ClientUtils.fancyMessage(" ");
-        choices.toggleChoice(choices.getAllChoices().get(0));
-        choices.getSelectedChoices().forEach(ClientUtils::fancyMessage);
-        ClientUtils.fancyMessage("selected choices ^");
-    }*/
+    }
 }
--- src/main/java/net/ccbluex/liquidbounce/utils/render/RenderUtils.java
+++ src/main/java/net/ccbluex/liquidbounce/utils/render/RenderUtils.java
@@ -35,7 +35,6 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
 import org.lwjgl.util.glu.GLUtessellator;
 import org.lwjgl.util.glu.GLUtessellatorCallbackAdapter;
-import scala.Int;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
@@ -45,7 +44,8 @@ import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.*;
 import java.util.List;
-
+import static akka.actor.Nobody.start;
+import static akka.actor.Nobody.stop;
 import static java.lang.Math.*;
 import static org.lwjgl.opengl.GL11.*;
 
@@ -467,6 +467,35 @@ public final class RenderUtils extends MinecraftInstance {
     }
 
     public static void drawGradientSidewaysV(double left, double top, double right, double bottom, int col1, int col2) {
+        float f = (float) (col1 >> 24 & 255) / 255.0f;
+        float f1 = (float) (col1 >> 16 & 255) / 255.0f;
+        float f2 = (float) (col1 >> 8 & 255) / 255.0f;
+        float f3 = (float) (col1 & 255) / 255.0f;
+        float f4 = (float) (col2 >> 24 & 255) / 255.0f;
+        float f5 = (float) (col2 >> 16 & 255) / 255.0f;
+        float f6 = (float) (col2 >> 8 & 255) / 255.0f;
+        float f7 = (float) (col2 & 255) / 255.0f;
+        GL11.glEnable((int) 3042);
+        GL11.glDisable((int) 3553);
+        GL11.glBlendFunc((int) 770, (int) 771);
+        GL11.glEnable((int) 2848);
+        GL11.glShadeModel((int) 7425);
+        GL11.glPushMatrix();
+        GL11.glBegin((int) 7);
+        GL11.glColor4f((float) f1, (float) f2, (float) f3, (float) f);
+        GL11.glVertex2d((double) left, (double) bottom);
+        GL11.glVertex2d((double) right, (double) bottom);
+        GL11.glColor4f((float) f5, (float) f6, (float) f7, (float) f4);
+        GL11.glVertex2d((double) right, (double) top);
+        GL11.glVertex2d((double) left, (double) top);
+        GL11.glEnd();
+        GL11.glPopMatrix();
+        GL11.glEnable((int) 3553);
+        GL11.glDisable((int) 3042);
+        GL11.glDisable((int) 2848);
+        GL11.glShadeModel((int) 7424);
+        Gui.drawRect(0, 0, 0, 0, 0);
+
         glEnable(GL_BLEND);
         glDisable(GL_TEXTURE_2D);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -493,6 +522,7 @@ public final class RenderUtils extends MinecraftInstance {
 
         glEnd();
     }
+
 
     public static void drawBlockBox(final BlockPos blockPos, final Color color, final boolean outline, final boolean box, final float outlineWidth) {
         final RenderManager renderManager = mc.getRenderManager();
@@ -770,6 +800,39 @@ public final class RenderUtils extends MinecraftInstance {
         glDisable(GL_LINE_SMOOTH);
     }
 
+    public static void brawRect(float left, float top, float right, float bottom, int color) {
+        if (left < right) {
+            float i = left;
+            left = right;
+            right = i;
+        }
+
+        if (top < bottom) {
+            float j = top;
+            top = bottom;
+            bottom = j;
+        }
+
+        float f3 = (float) (color >> 24 & 255) / 255.0F;
+        float f = (float) (color >> 16 & 255) / 255.0F;
+        float f1 = (float) (color >> 8 & 255) / 255.0F;
+        float f2 = (float) (color & 255) / 255.0F;
+        Tessellator tessellator = Tessellator.getInstance();
+        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+        GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+        GlStateManager.color(f, f1, f2, f3);
+        worldrenderer.begin(7, DefaultVertexFormats.POSITION);
+        worldrenderer.pos(left, bottom, 0.0D).endVertex();
+        worldrenderer.pos(right, bottom, 0.0D).endVertex();
+        worldrenderer.pos(right, top, 0.0D).endVertex();
+        worldrenderer.pos(left, top, 0.0D).endVertex();
+        tessellator.draw();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+    }
+
     public static void drawRect(final float x, final float y, final float x2, final float y2, final Color color) {
         drawRect(x, y, x2, y2, color.getRGB());
     }
@@ -864,6 +927,52 @@ public final class RenderUtils extends MinecraftInstance {
         glDepthMask(true);
         glDisable(GL_BLEND);
         glEnable(GL_DEPTH_TEST);
+    }
+
+    public static void lineWidth(final float width) {
+        GL11.glLineWidth(width);
+    }
+    public static void begin(final int glMode) {
+        GL11.glBegin(glMode);
+    }
+    public static void vertex(final double x, final double y) {
+        GL11.glVertex2d(x, y);
+    }
+    public static void end() {
+        GL11.glEnd();
+    }
+
+    public static void enable(final int glTarget) {
+        GL11.glEnable(glTarget);
+    }
+    public static void color(Color color) {
+        if (color == null)
+            color = Color.white;
+        color(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F, color.getAlpha() / 255F);
+    }
+
+    public static void color(final double red, final double green, final double blue, final double alpha) {
+        GL11.glColor4d(red, green, blue, alpha);
+    }
+
+    public static void disable(final int glTarget) {
+        GL11.glDisable(glTarget);
+    }
+
+    public static void lineNoGl(final double firstX, final double firstY, final double secondX, final double secondY, final Color color) {
+        start();
+        if (color != null)
+            color(color);
+        lineWidth(1);
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        begin(GL11.GL_LINES);
+        {
+            vertex(firstX, firstY);
+            vertex(secondX, secondY);
+        }
+        end();
+        GL11.glDisable(GL11.GL_LINE_SMOOTH);
+        stop();
     }
 
     public static void glColor(final int red, final int green, final int blue, final int alpha) {
