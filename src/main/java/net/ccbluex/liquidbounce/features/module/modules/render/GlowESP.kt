4eabd4a17be4a66e393df57fb8553b7f01f3629d
Zywl <86253343+opZywl@users.noreply.github.com>
2022-06-30 12:58:44 -0300

    Update GlowESP.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/GlowESP.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/GlowESP.kt
@@ -5,24 +5,23 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.render
 
-import net.ccbluex.liquidbounce.LiquidBounce
-import net.ccbluex.liquidbounce.launch.data.legacyui.clickgui.files.animations.Animation
-import net.ccbluex.liquidbounce.launch.data.legacyui.clickgui.files.animations.impl.DecelerateAnimation
-import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.event.Render2DEvent
-import net.ccbluex.liquidbounce.event.Render3DEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.LiquidBounce
+import net.ccbluex.liquidbounce.launch.data.legacyui.clickgui.files.animations.Animation
+import net.ccbluex.liquidbounce.launch.data.legacyui.clickgui.files.animations.impl.DecelerateAnimation
 import net.ccbluex.liquidbounce.features.module.modules.client.HUD
-import net.ccbluex.liquidbounce.utils.math.MathUtils
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.Render2DEvent
+import net.ccbluex.liquidbounce.event.Render3DEvent
 import net.ccbluex.liquidbounce.utils.render.ColorUtils
 import net.ccbluex.liquidbounce.utils.render.ESPUtil
 import net.ccbluex.liquidbounce.utils.render.RenderUtils
 import net.ccbluex.liquidbounce.utils.render.ShaderUtil
+import net.ccbluex.liquidbounce.utils.math.MathUtils
 import net.ccbluex.liquidbounce.value.BoolValue
 import net.ccbluex.liquidbounce.value.FloatValue
-
 import net.minecraft.client.gui.ScaledResolution
 import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.client.renderer.OpenGlHelper
