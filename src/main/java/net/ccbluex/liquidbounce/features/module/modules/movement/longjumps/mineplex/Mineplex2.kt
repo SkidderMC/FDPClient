c2fb9ba08c1d6030220efe38f228f0070909856a
DinoFeng <97534094+DinoFengz@users.noreply.github.com>
2022-06-30 16:49:22 +0800

    Works this for half hours

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/longjumps/mineplex/Mineplex2.kt
@@ -0,0 +1,32 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.longjumps.mineplex
+
+import net.ccbluex.liquidbounce.event.JumpEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.longjumps.LongJumpMode
+import net.ccbluex.liquidbounce.utils.MovementUtils
+
+class Mineplex2 : LongJumpMode("Mineplex2") {
+    private var canMineplexBoost = false
+    override fun onEnable() {
+        canMineplexBoost = false
+    }
+    override fun onUpdate(event: UpdateEvent) {
+        if (!canMineplexBoost) return
+
+        mc.thePlayer.jumpMovementFactor = 0.1f
+        if (mc.thePlayer.fallDistance > 1.5f) {
+            mc.thePlayer.jumpMovementFactor = 0f
+            mc.thePlayer.motionY = (-10f).toDouble()
+        }
+
+        MovementUtils.strafe()
+    }
+
+    override fun onJump(event: JumpEvent) {
+        if (mc.thePlayer!!.isCollidedHorizontally) {
+            event.motion = 2.31f
+            canMineplexBoost = true
+            mc.thePlayer!!.onGround = false
+        }
+    }
+}
---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/longjumps/ncp/NCP.kt
@@ -0,0 +1,25 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.longjumps.ncp
+
+import net.ccbluex.liquidbounce.event.JumpEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.longjumps.LongJumpMode
+import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.value.FloatValue
+
+class NCP : LongJumpMode("NCP") {
+    private val ncpBoostValue = FloatValue("${valuePrefix}Boost", 4.25f, 1f, 10f)
+    private var canBoost = false
+    override fun onEnable() {
+        canBoost = true
+    }
+    override fun onUpdate(event: UpdateEvent) {
+        mc.thePlayer.motionX = 0.0
+        mc.thePlayer.motionZ = 0.0
+        MovementUtils.strafe(MovementUtils.getSpeed() * if (canBoost) ncpBoostValue.get() else 1f)
+        if(canBoost) canBoost = false
+    }
+
+    override fun onJump(event: JumpEvent) {
+        canBoost = true
+    }
+}
