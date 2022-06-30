bdf0ca4ce141934f785b07fc5a800a21f9b88376
DinoFeng <97534094+DinoFengz@users.noreply.github.com>
2022-06-30 16:49:22 +0800

    Works this for half hours

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/longjumps/mineplex/Mineplex.kt
@@ -0,0 +1,22 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.longjumps.mineplex
+
+import net.ccbluex.liquidbounce.event.JumpEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.longjumps.LongJumpMode
+import net.ccbluex.liquidbounce.utils.MovementUtils
+
+class Mineplex : LongJumpMode("Mineplex") {
+    private var teleported = false
+    override fun onEnable() {
+        teleported = false
+    }
+    override fun onUpdate(event: UpdateEvent) {
+        mc.thePlayer.motionY += 0.0132099999999999999999999999999
+        mc.thePlayer.jumpMovementFactor = 0.08f
+        MovementUtils.strafe()
+    }
+
+    override fun onJump(event: JumpEvent) {
+        event.motion = event.motion * 4.08f
+    }
+}
