50319e85d8960e59acc79bd8a7707022b572670e
C00LC0D35 <dg636gamez@gmail.com>
2022-07-07 19:18:18 -0400

    Create VerusHop2.kt
    dont change this it much bypasses
    
    Signed-off-by: C00LC0D35 <dg636gamez@gmail.com>

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/verus/VerusHop2.kt
@@ -0,0 +1,39 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.verus
+import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
+import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.value.BoolValue
+
+class VerusHop : SpeedMode("VerusHop") {
+
+    private val timerBoost = BoolValue("TimerBoost",true)
+
+    private var jumps = 0
+    private var lastY = 0.0
+
+    override fun onPreMotion() {
+
+        if (MovementUtils.isMoving()) {
+            if (timerBoost.get() && (jumps >= 1)) {
+                mc.timer.timerSpeed = if (mc.thePlayer.motionY < 0) { 0.88f } else { 1.25f }
+            }
+
+            when {
+                mc.thePlayer.onGround -> {
+                    MovementUtils.strafe(0.4848f)
+                    mc.thePlayer.motionY = (0.42f).toDouble()
+
+                    if (mc.thePlayer.posY == lastY) {
+                        jumps++
+                    } else {
+                        jumps = 0
+                    }
+
+                    lastY = mc.thePlayer.posY
+                }
+                else -> {
+                    MovementUtils.strafe(0.3772f)
+                }
+            }
+        }
+    }
+}
