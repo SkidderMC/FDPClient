a3be4db3649432e75926ef87e49f267946420364
Administrator <lifeerwz@gmail.com>
2022-07-08 19:37:53 -0300

    b

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinRendererLivingEntity.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinRendererLivingEntity.java
@@ -8,10 +8,13 @@ package net.ccbluex.liquidbounce.injection.forge.mixins.render;
 import net.ccbluex.liquidbounce.LiquidBounce;
 import net.ccbluex.liquidbounce.features.module.modules.render.*;
 import net.ccbluex.liquidbounce.utils.EntityUtils;
+import net.ccbluex.liquidbounce.utils.MobsUtils;
+import net.ccbluex.liquidbounce.utils.render.ColorUtils;
 import net.ccbluex.liquidbounce.utils.render.RenderUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.entity.EntityLivingBase;
@@ -25,6 +28,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.awt.*;
+
 @Mixin(RendererLivingEntity.class)
 public abstract class MixinRendererLivingEntity extends MixinRender {
 
@@ -36,7 +41,12 @@ public abstract class MixinRendererLivingEntity extends MixinRender {
         final Chams chams = LiquidBounce.moduleManager.getModule(Chams.class);
         final NoRender noRender = LiquidBounce.moduleManager.getModule(NoRender.class);
 
-        if (chams.getState() && chams.getTargetsValue().get() && EntityUtils.INSTANCE.isSelected(entity, false)) {
+        if (noRender.getState() && noRender.shouldStopRender(entity)) {
+            callbackInfo.cancel();
+            return;
+        }
+
+        if (chams.getState() && chams.getTargetsValue().get() && chams.getLegacyMode().get() && EntityUtils.INSTANCE.isSelected(entity, false)) {
             GL11.glEnable(GL11.GL_POLYGON_OFFSET_FILL);
             GL11.glPolygonOffset(1.0F, -1000000F);
         }
@@ -46,7 +56,7 @@ public abstract class MixinRendererLivingEntity extends MixinRender {
     private <T extends EntityLivingBase> void injectChamsPost(T entity, double x, double y, double z, float entityYaw, float partialTicks, CallbackInfo callbackInfo) {
         final Chams chams = LiquidBounce.moduleManager.getModule(Chams.class);
 
-        if (chams.getState() && chams.getTargetsValue().get() && EntityUtils.INSTANCE.isSelected(entity, false)) {
+        if (chams.getState() && chams.getTargetsValue().get() && chams.getLegacyMode().get() && EntityUtils.INSTANCE.isSelected(entity, false)) {
             GL11.glPolygonOffset(1.0F, 1000000F);
             GL11.glDisable(GL11.GL_POLYGON_OFFSET_FILL);
         }
@@ -65,6 +75,8 @@ public abstract class MixinRendererLivingEntity extends MixinRender {
     protected <T extends EntityLivingBase> void renderModel(T entitylivingbaseIn, float p_77036_2_, float p_77036_3_, float p_77036_4_, float p_77036_5_, float p_77036_6_, float scaleFactor) {
         boolean visible = !entitylivingbaseIn.isInvisible();
         final TrueSight trueSight = LiquidBounce.moduleManager.getModule(TrueSight.class);
+        final Chams chams = (Chams) LiquidBounce.moduleManager.getModule(Chams.class);
+        boolean chamsFlag = chams.getState() && chams.getTargetsValue().get() && !chams.getLegacyMode().get() && MobsUtils.isSelected(entitylivingbaseIn, false);
         boolean semiVisible = !visible && (!entitylivingbaseIn.isInvisibleToPlayer(Minecraft.getMinecraft().thePlayer) || (trueSight.getState() && trueSight.getEntitiesValue().get()));
 
         if(visible || semiVisible) {
@@ -79,6 +91,96 @@ public abstract class MixinRendererLivingEntity extends MixinRender {
                 GlStateManager.blendFunc(770, 771);
                 GlStateManager.alphaFunc(516, 0.003921569F);
             }
+
+            final int blend = 3042;
+            final int depth = 2929;
+            final int srcAlpha = 770;
+            final int srcAlphaPlus1 = srcAlpha + 1;
+            final int polygonOffsetLine = 10754;
+            final int texture2D = 3553;
+            final int lighting = 2896;
+
+            boolean textured = chams.getTexturedValue().get();
+
+            Color chamsColor = new Color(0x00000000);
+
+            switch (chams.getColorModeValue().get()) {
+                case "Custom":
+                    chamsColor = new Color(chams.getColorRedValue().get(), chams.getColorGreenValue().get(), chams.getColorBlueValue().get());
+                    break;
+                case "Slowly":
+                    chamsColor = ColorUtils.slowlyRainbow(System.nanoTime(), 0, chams.getSaturationValue().get(), chams.getBrightnessValue().get());
+                    break;
+                case "AnotherRainbow":
+                    chamsColor = ColorUtils.fade(new Color(chams.getColorRedValue().get(), chams.getColorGreenValue().get(), chams.getColorBlueValue().get(), chams.getColorAlphaValue().get()), 0, 100);
+                    break;
+            }
+
+            chamsColor = ColorUtils.reAlpha(chamsColor, chams.getColorAlphaValue().get());
+
+            if (chamsFlag) {
+                Color chamsColor2 = new Color(0x00000000);
+
+                switch (chams.getBehindColorModeValue().get()) {
+                    case "Same":
+                        chamsColor2 = chamsColor;
+                        break;
+                    case "Opposite":
+                        chamsColor2 = ColorUtils.getOppositeColor(chamsColor);
+                        break;
+                    case "Red":
+                        chamsColor2 = new Color(0xffEF2626);
+                        break;
+                }
+
+                GL11.glPushMatrix();
+                GL11.glEnable(polygonOffsetLine);
+                GL11.glPolygonOffset(1.0F, 1000000.0F);
+                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
+
+                if (!textured) {
+                    GL11.glEnable(blend);
+                    GL11.glDisable(texture2D);
+                    GL11.glDisable(lighting);
+                    GL11.glBlendFunc(srcAlpha, srcAlphaPlus1);
+                    GL11.glColor4f(chamsColor2.getRed() / 255.0F, chamsColor2.getGreen() / 255.0F, chamsColor2.getBlue() / 255.0F, chamsColor2.getAlpha() / 255.0F);
+                }
+
+                GL11.glDisable(depth);
+                GL11.glDepthMask(false);
+            }
+
+            this.mainModel.render(entitylivingbaseIn, p_77036_2_, p_77036_3_, p_77036_4_, p_77036_5_, p_77036_6_, scaleFactor);
+
+            if (chamsFlag) {
+                GL11.glEnable(depth);
+                GL11.glDepthMask(true);
+
+                if (!textured) {
+                    GL11.glColor4f(chamsColor.getRed() / 255.0F, chamsColor.getGreen() / 255.0F, chamsColor.getBlue() / 255.0F, chamsColor.getAlpha() / 255.0F);
+                }
+
+                this.mainModel.render(entitylivingbaseIn, p_77036_2_, p_77036_3_, p_77036_4_, p_77036_5_, p_77036_6_, scaleFactor);
+
+                if (!textured) {
+                    GL11.glEnable(texture2D);
+                    GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+                    GL11.glDisable(blend);
+                    GL11.glEnable(lighting);
+                }
+
+                GL11.glPolygonOffset(1.0f, -1000000.0f);
+                GL11.glDisable(polygonOffsetLine);
+                GL11.glPopMatrix();
+            }
+
+            if(semiVisible) {
+                GlStateManager.disableBlend();
+                GlStateManager.alphaFunc(516, 0.1F);
+                GlStateManager.popMatrix();
+                GlStateManager.depthMask(true);
+            }
+        }
 
             final ESP esp = LiquidBounce.moduleManager.getModule(ESP.class);
             if(esp.getState() && EntityUtils.INSTANCE.isSelected(entitylivingbaseIn, false)) {
@@ -112,7 +214,6 @@ public abstract class MixinRendererLivingEntity extends MixinRender {
 
             this.mainModel.render(entitylivingbaseIn, p_77036_2_, p_77036_3_, p_77036_4_, p_77036_5_, p_77036_6_, scaleFactor);
 
-            final Chams chams = LiquidBounce.moduleManager.getModule(Chams.class);
             if (chams.getState() && chams.getTargetsValue().get() && EntityUtils.INSTANCE.isSelected(entitylivingbaseIn, false)) {
                 GL11.glPushMatrix();
                 GL11.glPushAttrib(1048575);
@@ -142,7 +243,6 @@ public abstract class MixinRendererLivingEntity extends MixinRender {
                 GlStateManager.depthMask(true);
             }
         }
-    }
 
     @Redirect(method={"renderName(Lnet/minecraft/entity/EntityLivingBase;DDD)V"}, at=@At(value="FIELD", target="Lnet/minecraft/client/renderer/entity/RenderManager;playerViewX:F"))
     private float renderName(RenderManager renderManager) {
