ddd7fa2f68a501e9a425f772df80bbe17f43cf11
Administrator <lifeerwz@gmail.com>
2022-07-08 20:02:28 -0300

    t

--- src/main/java/net/ccbluex/liquidbounce/utils/render/RenderUtils.java
+++ src/main/java/net/ccbluex/liquidbounce/utils/render/RenderUtils.java
@@ -215,6 +215,36 @@ public final class RenderUtils extends MinecraftInstance {
         setGlState(GL_CULL_FACE, hasCull);
     }
 
+    public static void drawGradientSideways(double left, double top, double right, double bottom, int col1, int col2) {
+        float f = (float) (col1 >> 24 & 255) / 255.0f;
+        float f1 = (float) (col1 >> 16 & 255) / 255.0f;
+        float f2 = (float) (col1 >> 8 & 255) / 255.0f;
+        float f3 = (float) (col1 & 255) / 255.0f;
+        float f4 = (float) (col2 >> 24 & 255) / 255.0f;
+        float f5 = (float) (col2 >> 16 & 255) / 255.0f;
+        float f6 = (float) (col2 >> 8 & 255) / 255.0f;
+        float f7 = (float) (col2 & 255) / 255.0f;
+        GL11.glEnable((int) 3042);
+        GL11.glDisable((int) 3553);
+        GL11.glBlendFunc((int) 770, (int) 771);
+        GL11.glEnable((int) 2848);
+        GL11.glShadeModel((int) 7425);
+        GL11.glPushMatrix();
+        GL11.glBegin((int) 7);
+        GL11.glColor4f((float) f1, (float) f2, (float) f3, (float) f);
+        GL11.glVertex2d((double) left, (double) top);
+        GL11.glVertex2d((double) left, (double) bottom);
+        GL11.glColor4f((float) f5, (float) f6, (float) f7, (float) f4);
+        GL11.glVertex2d((double) right, (double) bottom);
+        GL11.glVertex2d((double) right, (double) top);
+        GL11.glEnd();
+        GL11.glPopMatrix();
+        GL11.glEnable((int) 3553);
+        GL11.glDisable((int) 3042);
+        GL11.glDisable((int) 2848);
+        GL11.glShadeModel((int) 7424);
+    }
+
     public static void drawShadow(float x, float y, float width, float height) {
         drawTexturedRect(x - 9, y - 9, 9, 9, "paneltopleft");
         drawTexturedRect(x - 9, y + height, 9, 9, "panelbottomleft");
