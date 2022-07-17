23012057cc92166879c6155c41873fb93af4b1ab
FDPUser443 <95628936+FDPUser443@users.noreply.github.com>
2022-07-17 14:59:18 +0300

    Update HypixelHop.kt
    Signed-off-by: FDPUser443 <95628936+FDPUser443@users.noreply.github.com>

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/ncp/HypixelHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/ncp/HypixelHop.kt
@@ -13,30 +13,22 @@ import net.ccbluex.liquidbounce.value.ListValue
 
 
 class HypixelHop : SpeedMode("HypixelHop") {
-  
+
     private val bypassMode = ListValue("${valuePrefix}BypassMode", arrayOf("Safe", "Test"), "Safe")
     private val slowdownValue = FloatValue("${valuePrefix}SlowdownValue", 0.15f, 0.01f, 0.5f)
   
-    private var watchdogMultiplier = 1.0
-  
-    override fun onEnable() {
-        super.onEnable()
-    }
-
-    override fun onDisable() {
-        super.onDisable()
-    }
+    private var watchdogMultiplier = 1.0f
 
     override fun onUpdate() {
         if (MovementUtils.isMoving() && mc.thePlayer.onGround) {
             when (bypassMode.get().lowercase()) {
                 "safe" -> {
-                    watchdogMultiplier = 1.45
+                    watchdogMultiplier = 1.45f
                     mc.thePlayer.motionY = 0.41999998688697815
                 }
                 
                 "test" -> {
-                    watchdogMultiplier = 1.2
+                    watchdogMultiplier = 1.2f
                     mc.thePlayer.motionY = 0.39999998688697815
                 }
             }
@@ -44,19 +36,19 @@ class HypixelHop : SpeedMode("HypixelHop") {
 
         if (watchdogMultiplier > 1) {
             when (bypassMode.get().lowercase()) {
-                "safe" -> watchdogMultiplier -= 0.2
-                "test" -> watchdogMultiplier -= 0.05
+                "safe" -> watchdogMultiplier -= 0.2f
+                "test" -> watchdogMultiplier -= 0.05f
             }
         } else {
-            watchdogMultiplier = 1.0
+            watchdogMultiplier = 1.0f
         }
         
     }
     
     override fun onMove(event: MoveEvent) {
       when (bypassMode.get().lowercase()) {
-         "safe" -> MovementUtils.strafe(( 0.2875 * watchdogMultiplier.toDouble() * ( 1.081237f    - slowdownValue.get()).toDouble()).toFloat())
-         "test" -> MovementUtils.strafe(( 0.2875 * watchdogMultiplier.toDouble() * ( 1.0f         - slowdownValue.get()).toDouble()).toFloat())
+         "safe" -> MovementUtils.strafe( 0.2875f * watchdogMultiplier * ( 1.081237f    - slowdownValue.get()))
+         "test" -> MovementUtils.strafe( 0.2875f * watchdogMultiplier * ( 1.0f         - slowdownValue.get()))
       }
     }
 }
