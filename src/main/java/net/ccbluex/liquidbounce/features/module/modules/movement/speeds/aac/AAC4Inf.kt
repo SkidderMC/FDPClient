2a8b32b65433ff0bf2d87b8d527f262a08c26d31
DinoFengz <kingsley.tan829@gmail.com>
2022-07-19 15:36:48 +0800

    Adding

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/aac/AAC4Inf.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/aac/AAC4Inf.kt
@@ -1,21 +1,20 @@
-package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.other
+package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.aac
 
 import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
 
-class AAC5Fast : SpeedMode("AAC5Fast") {
+class AAC4Inf : SpeedMode("AAC4Inf") {
     override fun onUpdate() {
+        mc.timer.timerSpeed = 1.00f
         if (!MovementUtils.isMoving()) {
             return
         }
         if (mc.thePlayer.onGround) {
             mc.thePlayer.jump()
-            mc.thePlayer.speedInAir = 0.0201F
-            mc.timer.timerSpeed = 0.94F
+            mc.timer.timerSpeed = 1.00f
         }
         if (mc.thePlayer.fallDistance > 0.7 && mc.thePlayer.fallDistance < 1.3) {
-            mc.thePlayer.speedInAir = 0.02F
-            mc.timer.timerSpeed = 1.8F
+            mc.timer.timerSpeed = 1.08f
         }
     }
 
