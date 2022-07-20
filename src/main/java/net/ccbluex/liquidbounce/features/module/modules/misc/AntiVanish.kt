c2330f522c74b9d0e017bfc2a7d571d6b3c61710
C00LC0D35 <dg636gamez@gmail.com>
2022-07-19 20:01:13 -0400

    Update AntiVanish.kt
    Signed-off-by: C00LC0D35 <dg636gamez@gmail.com>

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/AntiVanish.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/AntiVanish.kt
@@ -37,7 +37,7 @@ class AntiVanish : Module() {
 
     private fun vanish(){
         if((System.currentTimeMillis()-lastNotify)>5000){
-            LiquidBounce.hud.addNotification(Notification("Found a vanished entity!", player.name + "is vanish!!!", NotifyType.WARNING, 4000, 500))
+            LiquidBounce.hud.addNotification(Notification("Found a vanished entity!", player.name + " is vanished!!!", NotifyType.WARNING, 4000, 500))
 
         }
         lastNotify=System.currentTimeMillis()
