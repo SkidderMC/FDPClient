714628afd6751e8e0fc6beecd94289313a0c6e0a
Administrator <lifeerwz@gmail.com>
2022-07-21 18:25:43 -0300

    ;-;

--- src/main/java/net/ccbluex/liquidbounce/launch/data/legacyui/clickgui/newVer/element/SearchBox.kt
+++
@@ -1,8 +0,0 @@
-package net.ccbluex.liquidbounce.launch.data.legacyui.clickgui.newVer.element
-
-import net.ccbluex.liquidbounce.ui.font.Fonts
-import net.minecraft.client.gui.GuiTextField
-
-class SearchBox(componentId: Int, x: Int, y: Int, width: Int, height: Int): GuiTextField(componentId, Fonts.font40, x, y, width, height) {
-    override fun getEnableBackgroundDrawing() = false
-}
