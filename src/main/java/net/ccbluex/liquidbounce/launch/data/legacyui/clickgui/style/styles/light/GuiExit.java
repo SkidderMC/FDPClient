5e641c88b27a6ce5808d62ca7bbf555ea8615165
Administrator <lifeerwz@gmail.com>
2022-07-22 20:09:16 -0300

    ;

--- src/main/java/net/ccbluex/liquidbounce/launch/data/legacyui/clickgui/style/styles/light/GuiExit.java
+++ src/main/java/net/ccbluex/liquidbounce/launch/data/legacyui/clickgui/style/styles/light/GuiExit.java
@@ -29,7 +29,7 @@ public class GuiExit extends GuiScreen {
             if(Mouse.isButtonDown(0) && !mouseClicked)
                 mc.displayGuiScreen(null);
         } else mouseClicked = Mouse.isButtonDown(0) && !(mouseX >= width / 2 - 75 && mouseX <= width / 2 - 10 && mouseY >= height / 2 + 20 && mouseY <= height / 2 + 35);
-        //RenderUtils.drawImage(new ResourceLocation("fdpclient/clickgui/rect.png"), width / 2 - 80,height / 2 - 30,150,70);
+        //RenderUtils.drawImage(new ResourceLocation("AimWhere/clickgui/rect.png"), width / 2 - 80,height / 2 - 30,150,70);
         RenderUtils.drawRect(width / 2 - 80, height / 2 - 30, width / 2 + 70, height / 2 + 40,new Color(40,40,40,125).getRGB());
         RenderUtils.drawRect(width / 2 - 75, height / 2 + 20, width / 2 - 10, height / 2 + 35, new Color(19, 138, 225).getRGB());
         RenderUtils.drawRect(width / 2, height / 2 + 20, width / 2 + 65, height / 2 + 35, new Color(241, 54, 35).getRGB());
@@ -37,4 +37,5 @@ public class GuiExit extends GuiScreen {
         Fonts.font40.drawString("Back",width / 2 - 50, height / 2 + 28, -1);
         Fonts.font40.drawString("Exit",width / 2 + 25, height / 2 + 28, -1);
     }
+
 }
