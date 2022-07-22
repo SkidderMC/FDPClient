fdcf8a056da5737347127668380f67d5e96b47c5
Zywl <86253343+opZywl@users.noreply.github.com>
2022-07-22 18:04:01 -0300

    '
    Signed-off-by: Zywl <86253343+opZywl@users.noreply.github.com>

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/button/RoundedButtonRenderer.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/button/RoundedButtonRenderer.kt
@@ -1,7 +1,7 @@
 /*
- * FDPClassic Hacked Client
+ * FDPClient Hacked Client
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
- * https://github.com/fdpclassic/FDPClassic/
+ * https://github.com/SkidderMC/FDPClient/
  */
 package net.ccbluex.liquidbounce.features.module.modules.client.button
 
