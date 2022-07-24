93899ee7893a76ca2d807b0a23b1428bcb35083d
Invalid <114514@gmail.com>
2022-07-24 08:58:53 +0800

    Fix speed

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/IntaveHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/IntaveHop.kt
@@ -13,18 +13,20 @@ class IntaveHop : SpeedMode("IntaveHop") {
     private var jumpTicks = 0
   
     override fun onPreMotion() {
-        if (mc.thePlayer.onGround) {
+        jumpTicks++
+        if (!MovementUtils.isMoving()) {
+            mc.thePlayer.motionX = 0.0
+            mc.thePlayer.motionZ = 0.0
+            jumpTicks = 0
+            return
+        }
+        if (mc.thePlayer.onGround && MovementUtils.isMoving()) {
             MovementUtils.strafe(0.588f)
             mc.thePlayer.jump()
             jumpTicks = 0
-        } else if (jumpTicks == 4) {
+        } else if (jumpTicks == 5) {
             mc.thePlayer.motionY = -0.0784000015258789
-            jumpTicks++
-        } else {
-            jumpTicks++
         }
-
         MovementUtils.strafe()
-
     }
 }
