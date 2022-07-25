3184f0eb22ed795153788520fff1c3fab42681e5
Invalid <114514@gmail.com>
2022-07-25 08:26:33 +0800

    fix

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/AutoHeal.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/AutoHeal.kt
@@ -6,42 +6,40 @@
 package net.ccbluex.liquidbounce.features.module.modules.player
 
 import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.WorldEvent
 import net.ccbluex.liquidbounce.value.FloatValue
 import net.ccbluex.liquidbounce.value.IntegerValue
 import net.ccbluex.liquidbounce.value.BoolValue
-import net.ccbluex.liquidbounce.utils.timer.MSTimer
-import net.minecraft.util.MathHelper
-import net.ccbluex.liquidbounce.event.PacketEvent
-import net.minecraft.network.play.server.S02PacketChat
-import net.ccbluex.liquidbounce.utils.ClientUtils
-import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
-import net.ccbluex.liquidbounce.utils.MovementUtils
-import net.minecraft.client.gui.inventory.GuiContainer
-import net.minecraft.item.ItemStack
+import net.ccbluex.liquidbounce.utils.timer.MSTimer
+import net.ccbluex.liquidbounce.utils.InventoryUtils
 import net.minecraft.network.play.client.C09PacketHeldItemChange
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement
 import net.minecraft.network.play.client.C03PacketPlayer
+import net.minecraft.network.play.server.S09PacketHeldItemChange
+import net.minecraft.client.gui.inventory.GuiContainer
 import net.minecraft.init.Items
+import net.minecraft.item.ItemStack
+import net.minecraft.util.MathHelper
 import net.minecraft.potion.Potion
-import org.apache.commons.lang3.tuple.Pair
 import java.util.*
 
-@ModuleInfo(name = "Heal", category = ModuleCategory.PLAYER)
+@ModuleInfo(name = "AutoHeal", category = ModuleCategory.PLAYER)
 class Heal : Module() {
 
     private val modeValue = ListValue("Mode", arrayOf("Auto", "LegitAuto", "Head"), "Auto")
     private val percent = FloatValue("HealthPercent", 75.0f, 1.0f, 100.0f)
     private val min = IntegerValue("MinDelay", 75, 1, 5000)
     private val max = IntegerValue("MaxDelay", 125, 1, 5000)
-    private val regenSec = FloatValue("RegenSec", 4.6f, 0.0f, 10.0f)
-    private val groundCheck = BoolValue("GroundCheck", false)
-    private val voidCheck = BoolValue("VoidCheck", true)
+    private val regenSec = FloatValue("MinRegenSec", 4.6f, 0.0f, 10.0f)
+    private val groundCheck = BoolValue("OnlyOnGround", false)
     private val waitRegen = BoolValue("WaitRegen", true)
     private val invCheck = BoolValue("InvCheck", false)
-    private val absorpCheck = BoolValue("AbsorpCheck", true)
+    private val absorpCheck = BoolValue("NoAbsorption", true)
     val timer = MSTimer()
     private var eating = -1
     var delay = 0
@@ -62,7 +60,17 @@ class Heal : Module() {
     }
 
     @EventTarget
-    fun onUpdate(event: UpdateEvent?) {
+    fun onPacket(event: PacketEvent) {
+        val packet = event.packet
+        if (eating != -1 && packet is C03PacketPlayer) {
+            eating++
+        } else if (packet is S09PacketHeldItemChange || packet is C09PacketHeldItemChange) {
+            eating = -1
+        }
+    }
+
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
         if (tryHeal) {
             when (modeValue.get().lowercase()) {
             "auto" -> {
@@ -117,7 +125,7 @@ class Heal : Module() {
             isDisable = false
         }
         val absorp = MathHelper.ceiling_double_int(mc.thePlayer.absorptionAmount.toDouble())
-        if ((groundCheck.get() && !mc.thePlayer.onGround) || (voidCheck.get() && !MovementUtils.isBlockUnder()) || (invCheck.get() && mc.currentScreen is GuiContainer) || (absorp != 0 && absorpCheck.get()))
+        if ((groundCheck.get() && !mc.thePlayer.onGround) || (invCheck.get() && mc.currentScreen is GuiContainer) || (absorp > 0 && absorpCheck.get()))
             return
         if (waitRegen.get() && mc.thePlayer.isPotionActive(Potion.regeneration) && mc.thePlayer.getActivePotionEffect(Potion.regeneration).duration > regenSec.get() * 20.0f)
             return
@@ -128,9 +136,6 @@ class Heal : Module() {
         }
     }
 
-    override val tag: String?
-        get() = if (mc.thePlayer == null || mc.thePlayer.health == Float.NaN) modeValue.get() else modeValue.get()+" "+String.format(
-            "%.2f HP",
-            percent.get() / 100.0f * mc.thePlayer.maxHealth
-        )
+    override val tag: String
+        get() = modeValue.get()
 } 
