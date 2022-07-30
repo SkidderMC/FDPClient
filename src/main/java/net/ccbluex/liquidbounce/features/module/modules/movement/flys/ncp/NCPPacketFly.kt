dcd4bd5c018ca46dcef4d683ddcca77a2e213a24
DinoFengz <kingsley.tan829@gmail.com>
2022-07-30 20:54:45 +0800

    retard ncp packet fly

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/ncp/NCPPacketFly.kt
@@ -0,0 +1,29 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/SkidderMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.flys.ncp
+
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
+import net.ccbluex.liquidbounce.value.FloatValue
+import net.minecraft.network.play.client.C03PacketPlayer.C04PacketPlayerPosition
+import kotlin.math.*
+
+class NCPPacketFly : FlyMode("NCPPacket") {
+    private val timerValue = FloatValue("${valuePrefix}Timer", 1.1f, 1.0f, 1.3f)
+    private val speedValue = FloatValue("${valuePrefix}Speed", 0.28f, 0.27f, 0.29f)
+
+    override fun onUpdate(event: UpdateEvent) {
+        val yaw = Math.toRadians(mc.thePlayer.rotationYaw.toDouble())
+        val x = -sin(yaw) * speedValue.get()
+        val z = cos(yaw) * speedValue.get()
+        mc.thePlayer.motionX = 0.0
+        mc.thePlayer.motionY = 0.0
+        mc.thePlayer.motionZ = 0.0
+        mc.timer.timerSpeed = timerValue.get()
+        mc.netHandler.addToSendQueue(C04PacketPlayerPosition(mc.thePlayer.posX + x, mc.thePlayer.motionY , mc.thePlayer.motionZ + z, false))
+        mc.netHandler.addToSendQueue(C04PacketPlayerPosition(mc.thePlayer.posX + x, mc.thePlayer.motionY - 490, mc.thePlayer.motionZ + z, true))
+    }
+}
