88dcc3817d2d55092e42fcb7def4b780ffe4fd7a
DinoFengz <kingsley.tan829@gmail.com>
2022-07-31 20:49:35 +0800

    remove sus sound

--- src/main/java/net/skiddermc/fdpclient/ui/sound/TipSoundManager.kt
+++ src/main/java/net/skiddermc/fdpclient/ui/sound/TipSoundManager.kt
@@ -7,12 +7,10 @@ import java.io.File
 class TipSoundManager {
     var enableSound: TipSoundPlayer
     var disableSound: TipSoundPlayer
-    var littlesound: TipSoundPlayer
 
     init {
         val enableSoundFile = File(FDPClient.fileManager.soundsDir, "enable.wav")
         val disableSoundFile = File(FDPClient.fileManager.soundsDir, "disable.wav")
-        val littlebutterflyFile = File(FDPClient.fileManager.soundsDir, "littlebutterfly.wav")
 
         if (!enableSoundFile.exists()) {
             FileUtils.unpackFile(enableSoundFile, "assets/minecraft/fdpclient/sound/enable.wav")
@@ -21,12 +19,7 @@ class TipSoundManager {
             FileUtils.unpackFile(disableSoundFile, "assets/minecraft/fdpclient/sound/disable.wav")
         }
 
-        if(!littlebutterflyFile.exists()) {
-            FileUtils.unpackFile(littlebutterflyFile, "assets/minecraft/fdpclient/sound/littlebutterfly.wav")
-        }
-
         enableSound = TipSoundPlayer(enableSoundFile)
         disableSound = TipSoundPlayer(disableSoundFile)
-        littlesound = TipSoundPlayer(littlebutterflyFile)
     }
 }
