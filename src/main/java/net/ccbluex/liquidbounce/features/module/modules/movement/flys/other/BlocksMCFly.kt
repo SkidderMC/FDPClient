76cd046426b9986e0e38389ab7746ff16d84e3cc
DinoFengz <kingsley.tan829@gmail.com>
2022-08-01 23:35:52 +0800

    blocksmc Fly

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/other/BlocksMCFly.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/other/BlocksMCFly.kt
@@ -4,22 +4,40 @@ import net.ccbluex.liquidbounce.event.BlockBBEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
 import net.minecraft.block.BlockAir
+import net.minecraft.init.Items
+import net.minecraft.item.Item
+import net.minecraft.item.ItemBlock
 import net.minecraft.util.AxisAlignedBB
+import net.minecraft.util.BlockPos
 
 class BlocksMCFly : FlyMode("BlocksMC") {
     private var blocksBB = false
     private var ticks = 0
+    private var defaultrotation = 0.0f
     override fun onEnable() {
         blocksBB = false
         ticks = 0
+        defaultrotation = mc.thePlayer.rotationPitch
+        mc.gameSettings.keyBindUseItem.pressed = false
         if(mc.thePlayer.onGround) {
             mc.thePlayer.motionY = 0.42
         }
     }
 
+    override fun onDisable() {
+        mc.gameSettings.keyBindUseItem.pressed = false
+    }
+
     override fun onUpdate(event: UpdateEvent) {
-        if(mc.thePlayer.posY >= fly.launchY + 0.8 && mc.thePlayer.onGround) {
-            blocksBB = true
+        if(mc.thePlayer.posY >= fly.launchY + 0.8 && !blocksBB) {
+            if(mc.thePlayer.onGround) {
+                mc.gameSettings.keyBindUseItem.pressed = false
+                mc.thePlayer.rotationPitch = defaultrotation
+                blocksBB = true
+            } else {
+                mc.thePlayer.rotationPitch = 90f
+                mc.gameSettings.keyBindUseItem.pressed = true
+            }
         }
         if(blocksBB) {
             ticks++
