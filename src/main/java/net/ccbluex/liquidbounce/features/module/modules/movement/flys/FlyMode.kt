4b1fd5d0c30feef6fe3c53164e1e7860d152d125
DinoFengz <kingsley.tan829@gmail.com>
2022-08-05 20:28:18 +0800

    BlocksMC Fly INF by DinoFeng

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/FlyMode.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/FlyMode.kt
@@ -22,6 +22,7 @@ abstract class FlyMode(val modeName: String) : MinecraftInstance() {
     open fun onUpdate(event: UpdateEvent) {}
     open fun onMotion(event: MotionEvent) {}
     open fun onPacket(event: PacketEvent) {}
+    open fun onWorld(event: WorldEvent) {}
     open fun onMove(event: MoveEvent) {}
     open fun onBlockBB(event: BlockBBEvent) {}
     open fun onJump(event: JumpEvent) {}
