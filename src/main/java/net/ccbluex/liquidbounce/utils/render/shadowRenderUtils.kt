be2d598111d359ca97d6bb3133ff2c46433fef5a
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-08-06 19:39:52 +0100

    xiguas issues. 西瓜：为什么疯了兄弟？ 这个版本不欢迎你

--- src/main/java/net/ccbluex/liquidbounce/utils/render/shadowRenderUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/render/shadowRenderUtils.kt
@@ -1,7 +1,6 @@
 package net.ccbluex.liquidbounce.utils.render
 
 import net.ccbluex.liquidbounce.LiquidBounce
-import net.ccbluex.liquidbounce.ui.RenderLeave
 import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.*
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats
@@ -23,7 +22,6 @@ object shadowRenderUtils {
     private val DISPLAY_LISTS_2D = IntArray(4)
     @JvmStatic
     fun drawShadowWithCustomAlpha(x: Float, y: Float, width: Float, height: Float, alpha: Float) {
-        if(LiquidBounce.RENDERLEAVE== RenderLeave.LOW) return;
         drawTexturedRectWithCustomAlpha(x - 9, y - 9, 9f, 9f, "paneltopleft", alpha)
         drawTexturedRectWithCustomAlpha(x - 9, y + height, 9f, 9f, "panelbottomleft", alpha)
         drawTexturedRectWithCustomAlpha(x + width, y + height, 9f, 9f, "panelbottomright", alpha)
