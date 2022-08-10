d39b96dcb4ab36631f94ed31b168bc600b55ae17
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-08-10 23:05:59 +0100

    lol

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/button/FLineButtonRenderer.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/button/FLineButtonRenderer.kt
@@ -48,7 +48,7 @@ class FLineButtonRenderer(button: GuiButton) : AbstractButtonRenderer(button) {
 
         lastUpdate = time
 
-            if (buttonShadowValue.equals(True)){
+            if (HUD.buttonShadowValue.equals(true)){
             shadowRenderUtils.drawShadowWithCustomAlpha(button.xPosition.toFloat(), button.yPosition.toFloat(), button.width.toFloat(), button.height.toFloat(), 240f)
             }
     }
