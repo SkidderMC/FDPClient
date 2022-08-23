e0a2707fe4672e2f846c7685af7ce509c4a7f79c
Dg636 <dg636gamez@gmail.com>
2022-08-23 18:18:48 -0400

    Update MatrixBoostFly.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/matrix/MatrixBoostFly.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/matrix/MatrixBoostFly.kt
@@ -5,6 +5,7 @@ import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.ccbluex.liquidbounce.value.FloatValue
+import net.ccbluex.liquidbounce.value.ListValue
 import net.minecraft.network.play.client.C03PacketPlayer
 import net.minecraft.network.play.server.S08PacketPlayerPosLook
 import kotlin.math.cos
@@ -12,7 +13,9 @@ import kotlin.math.sin
 
 
 class MatrixBoostFly : FlyMode("MatrixBoost") {
+    private val bypassMode = ListValue("${valuePrefix}BypassMode", arrayOf("New", "Stable", "Test", "Custom"), "New")
     private val speed = FloatValue("${valuePrefix}Speed", 2.0f, 1.0f, 3.0f)
+    private val customYMotion = FloatValue("${valuePrefix}CustomJumpMotion", 0.6f, 0.2f, 5f)
     private val jumpTimer = FloatValue("${valuePrefix}JumpTimer", 0.1f, 0.1f, 2f)
     private val boostTimer = FloatValue("${valuePrefix}BoostTimer", 1f, 0.5f, 3f)
     private var boostMotion = 0
@@ -20,6 +23,8 @@ class MatrixBoostFly : FlyMode("MatrixBoost") {
     override fun onEnable() {
         boostMotion = 0
     }
+    
+
 
     override fun onUpdate(event: UpdateEvent) {
         if (boostMotion == 0) {
@@ -32,19 +37,35 @@ class MatrixBoostFly : FlyMode("MatrixBoost") {
                     true
                 )
             )
-            mc.netHandler.addToSendQueue(
-                C03PacketPlayer.C04PacketPlayerPosition(
-                    mc.thePlayer.posX + -sin(yaw) * 1.5,
-                    mc.thePlayer.posY + 1,
-                    mc.thePlayer.posZ + cos(yaw) * 1.5,
-                    false
+            if (bypassMode.equals("Test") {
+                mc.netHandler.addToSendQueue(
+                    C03PacketPlayer.C04PacketPlayerPosition(
+                        mc.thePlayer.posX + -sin(yaw) * 1.44,
+                        mc.thePlayer.posY + 1.91,
+                        mc.thePlayer.posZ + cos(yaw) * 1.44,
+                        false
+                    )
                 )
-            )
+            } else {
+                mc.netHandler.addToSendQueue(
+                    C03PacketPlayer.C04PacketPlayerPosition(
+                        mc.thePlayer.posX + -sin(yaw) * 1.5,
+                        mc.thePlayer.posY + 1,
+                        mc.thePlayer.posZ + cos(yaw) * 1.5,
+                        false
+                    )
+                )
+            }
             boostMotion = 1
             mc.timer.timerSpeed = jumpTimer.get()
         } else if (boostMotion == 2) {
             MovementUtils.strafe(speed.get())
-            mc.thePlayer.motionY = 0.8
+            when (bypassMode.get().lowercase()) {
+                "stable" -> mc.thePlayer.motionY = 0.8
+                "new" -> mc.thePlayer.motionY = 0.48
+                "test" -> mc.thePlayer.motionY = 1.91
+                "custom" -> mc.thePlayer.motionY = customYMotion.get().toDouble()
+            }
             boostMotion = 3
         } else if (boostMotion < 5) {
             boostMotion++
