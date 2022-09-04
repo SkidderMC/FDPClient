bc431432507d6b551d24a3c495473d2d155fc037
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-04 16:17:08 +0100

    Update LiquidBounce.kt

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -48,7 +48,7 @@ object LiquidBounce {
     const val CLIENT_CREATOR = "CCBlueX & SkidderMC TEAM"
     const val CLIENT_WEBSITE = "fdpinfo.github.io"
     const val MINECRAFT_VERSION = "1.8.9"
-    const val VERSIONTYPE = "RC1" // Change to RELEASE on release and BETA on beta
+    const val VERSIONTYPE = "RELEASE" // Change to RELEASE on release and BETA on beta
     const val VERSIONNUMBER = "4.6.5" // major build number
     
     @JvmField
