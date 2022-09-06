21018d063b76c00f605a6d39cd52b09871469fb8
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-06 17:53:43 +0100

    perhaps

--- src/main/java/net/ccbluex/liquidbounce/value/ListValue.kt
+++ src/main/java/net/ccbluex/liquidbounce/value/ListValue.kt
@@ -15,6 +15,9 @@ open class ListValue(name: String, val values: Array<String>, value: String) : V
         this.value = value
     }
 
+    constructor(name: String, values: Array<String>, value: String): this(name, values, value, { true } )
+
+
     fun containsValue(string: String): Boolean {
         return Arrays.stream(values).anyMatch { it.equals(string, ignoreCase = true) }
     }
@@ -34,35 +37,6 @@ open class ListValue(name: String, val values: Array<String>, value: String) : V
         if (element.isJsonPrimitive) changeValue(element.asString)
     }
 }
-open class ListValueInf(name: String, val values: Array<String>, value: String, displayable: () -> Boolean) : Value<String>(name, value, displayable) {
-
-    constructor(name: String, values: Array<String>, value: String): this(name, values, value, { true } )
-
-    @JvmField
-    var openList = false
-
-    init {
-        this.value = value
-    }
-
-    fun contains(string: String?): Boolean {
-        return Arrays.stream(values).anyMatch { s: String -> s.equals(string, ignoreCase = true) }
-    }
-
-    override fun changeValue(value: String) {
-        for (element in values) {
-            if (element.equals(value, ignoreCase = true)) {
-                this.value = element
-                break
-            }
-        }
-    }
-
-    override fun toJson() = JsonPrimitive(value)
-
-    override fun fromJson(element: JsonElement) {
-        if (element.isJsonPrimitive) changeValue(element.asString)
-    }
 
 
-}
+
