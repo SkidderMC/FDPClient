89bc15985ceac38449bdf5a8abaa105c413048e8
NotAiden030 <110329566+NotAiden030@users.noreply.github.com>
2022-09-07 08:43:56 +0800

    Hypixel beta nofall testing

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/nofalls/other/HypixelNofall.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/nofalls/other/HypixelNofall.kt
@@ -6,8 +6,8 @@ import net.minecraft.network.play.client.C03PacketPlayer
 
 class HypixelNofall : NoFallMode("Hypixel") {
     override fun onPacket(event: PacketEvent) {
-        if(event.packet is C03PacketPlayer && mc.thePlayer != null && mc.thePlayer.fallDistance > 1.5) {
-            event.packet.onGround = mc.thePlayer.ticksExisted % 2 == 0
+        if(event.packet is C03PacketPlayer && mc.thePlayer != null && mc.thePlayer.fallDistance > 5.0) {
+            event.packet.onGround = mc.thePlayer.ticksExisted % 5.0 == 0
         }
     }
 }
