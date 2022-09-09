0732f7067b50db7212678d85d63a6cf8a4f3426a
Dg636 <dg636gamez@gmail.com>
2022-09-09 19:10:49 -0400

    Update Matrix663Nofall.kt
    Signed-off-by: Dg636 <dg636gamez@gmail.com>

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/nofalls/matrix/Matrix663Nofall.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/nofalls/matrix/Matrix663Nofall.kt
@@ -4,23 +4,36 @@ import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.player.nofalls.NoFallMode
 import net.ccbluex.liquidbounce.utils.PacketUtils
+import net.ccbluex.liquidbounce.utils.misc.FallingPlayer
 import net.ccbluex.liquidbounce.value.BoolValue
 import net.minecraft.network.play.client.C03PacketPlayer
 
 class Matrix663Nofall : NoFallMode("Matrix6.6.3") {
     private var matrixSend = false
+    
     private val matrixSafe = BoolValue("SafeNoFall", true)
+    
+    
+    private var firstNfall = true
+    private var nearGround = false
     
     override fun onEnable() {
         matrixSend = false
+        firstNfall = true
+        nearGround = false
     }
     override fun onDisable() {
-        mc.timer.timerSpeed =1f
+        mc.timer.timerSpeed = 1f
     }
     override fun onNoFall(event: UpdateEvent) {
-        if (mc.thePlayer.fallDistance - mc.thePlayer.motionY > 3) {
+        
+        val fallingPlayer = FallingPlayer(mc.thePlayer)
+        val collLoc = fallingPlayer.findCollision(60) // null -> too far to calc or fall pos in void
+
+        if (mc.thePlayer.fallDistance - mc.thePlayer.motionY > 3 || (abs((collLoc?.y ?: 0) - mc.thePlayer.posY) < 3 && mc.thePlayer.fallDistance - mc.thePlayer.motionY > 1)) {
             mc.thePlayer.fallDistance = 0.0f
             matrixSend = true
+            
             if (matrixSafe.get()) {
                 mc.timer.timerSpeed = 0.3f
                 mc.thePlayer.motionX *= 0.5
@@ -36,9 +49,13 @@ class Matrix663Nofall : NoFallMode("Matrix6.6.3") {
     override fun onPacket(event: PacketEvent) {
         if (event.packet is C03PacketPlayer && matrixSend) {
             matrixSend = false
-            event.cancelEvent()
-            PacketUtils.sendPacketNoEvent(C03PacketPlayer.C04PacketPlayerPosition(event.packet.x, event.packet.y, event.packet.z, true))
-            PacketUtils.sendPacketNoEvent(C03PacketPlayer.C04PacketPlayerPosition(event.packet.x, event.packet.y, event.packet.z, false))
+            val fallingPlayer = FallingPlayer(mc.thePlayer)
+            val collLoc = fallingPlayer.findCollision(60) // null -> too far to calc or fall pos in void
+            if (abs((collLoc?.y ?: 0) - mc.thePlayer.posY) > 2) {
+                event.cancelEvent()
+                mc.netHandler.addToSendQueue(C03PacketPlayer.C04PacketPlayerPosition(event.packet.x, event.packet.y, event.packet.z, true))
+                mc.netHandler.addToSendQueue(C03PacketPlayer.C04PacketPlayerPosition(event.packet.x, event.packet.y, event.packet.z, false))
+            }
         }
 
     }
