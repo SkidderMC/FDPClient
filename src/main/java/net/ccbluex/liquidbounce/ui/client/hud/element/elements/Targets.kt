356aad948eb8eb6f0b48323f67b961d63bc56290
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-10 16:33:24 +0100

    test

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Targets.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Targets.kt
@@ -1398,6 +1398,74 @@ open class Targets : Element(-46.0, -40.0, 1F, Side(Side.Horizontal.MIDDLE, Side
         )
     }
 
+    private fun drawSparklingWater(target: EntityLivingBase) {
+        // background
+        RenderUtils.drawRoundedCornerRect(
+            -1.5f, 2.5f, 152.5f, 52.5f,
+            5.0f, Color(0, 0, 0, 26).rgb
+        )
+        RenderUtils.drawRoundedCornerRect(
+            -1f, 2f, 152f, 52f,
+            5.0f, Color(0, 0, 0, 26).rgb
+        )
+        RenderUtils.drawRoundedCornerRect(
+            -0.5f, 1.5f, 151.5f, 51.5f,
+            5.0f, Color(0, 0, 0, 40).rgb
+        )
+        RenderUtils.drawRoundedCornerRect(
+            -0f, 1f, 151.0f, 51.0f,
+            5.0f, Color(0, 0, 0, 60).rgb
+        )
+        RenderUtils.drawRoundedCornerRect(
+            0.5f, 0.5f, 150.5f, 50.5f,
+            5.0f, Color(0, 0, 0, 50).rgb
+        )
+        RenderUtils.drawRoundedCornerRect(
+            1f, 0f, 150.0f, 50.0f,
+            5.0f, Color(0, 0, 0, 50).rgb
+        )
+        val hurtPercent = target.hurtPercent
+        val scale = if (hurtPercent == 0f) {
+            1f
+        } else if (hurtPercent < 0.5f) {
+            1 - (0.1f * hurtPercent * 2)
+        } else {
+            0.9f + (0.1f * (hurtPercent - 0.5f) * 2)
+        }
+        val size = 35
+        // head
+
+
+        // target text
+        FontLoaders.F20.DisplayFonts("${target.name}", 45f, 12f, Color.WHITE.rgb, FontLoaders.F20)
+        val df = DecimalFormat("0.00");
+        // armour text
+        FontLoaders.F14.DisplayFonts(
+            "Armor ${(df.format(PlayerUtils.getAr(target) * 100))}%",
+            45f,
+            24f,
+            Color(200, 200, 200).rgb,
+            FontLoaders.F14
+        )//bar
+        RenderUtils.drawRoundedCornerRect(45f, 32f, 145f, 42f, 5f, Color(0, 0, 0, 100).rgb)
+        RenderUtils.drawRoundedCornerRect(
+            45f,
+            32f,
+            45f + (easingHP / target.maxHealth) * 100f,
+            42f,
+            5f,
+            ColorUtils.rainbow().rgb
+        )
+        FontLoaders.F14.DisplayFont2(
+            FontLoaders.F14,
+            "${((df.format((easingHP / target.maxHealth) * 100)))}%",
+            80f,
+            34f,
+            Color(255, 255, 255).rgb,
+            true
+        )
+    }
+
     private fun drawRice(entity: EntityLivingBase) {
         updateAnim(entity.health)
 
