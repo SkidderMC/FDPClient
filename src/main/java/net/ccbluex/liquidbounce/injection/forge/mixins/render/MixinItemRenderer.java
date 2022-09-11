078b0a49165b85a6f154233856f1d12eeea2a230
Dg636 <dg636gamez@gmail.com>
2022-09-10 20:37:27 -0400

    nani?

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinItemRenderer.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinItemRenderer.java
@@ -254,7 +254,7 @@ public abstract class MixinItemRenderer {
                             case "Yeet": {
                                 doItemRenderGLTranslate();
                                 GlStateManager.translate(0.0F, 0.0F, 0.0F);
-                                GlStateManager.rotate(23.0F, 0.0F, 1.0F, 0.0F);
+                                GlStateManager.rotate(45.0F, 0.0F, 1.0F, 0.0F);
                                 float var11 = MathHelper.sin(f1 * f1 * 3.1415927F);
                                 float var12 = MathHelper.sin(MathHelper.sqrt_float(f1) * 3.1415927F);
                                 GlStateManager.rotate(var11 * 0.0F, 0.0F, 1.0F, 0.0F);
