f4cad61331b9c82fe20ca2512dd1bbfd943e3ff9
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-12 18:24:44 +0100

    lol

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/KeyStrokes.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/KeyStrokes.kt
@@ -61,7 +61,7 @@ class KeyStrokes : Element(5.0, 25.0, 1.25F, Side.default()) {
             keyStroke.render(animSpeedValue.get(), backGroundColor, textColor, highLightPercent.get(), outline.get(), outlineBoldValue.get(), fontValue.get(), blurValue.get(), this.renderX.toFloat(), this.renderY.toFloat(), scale, Companion)
         }
         if(keyStyleValue.get().equals("Jello")) {
-            RenderUtils.drawImage(ResourceLocation("fdpclient/misc/keystrokes.png"), -5, -7, 54, 54)
+            RenderUtils.drawImage(ResourceLocation("fdpclient/misc/keystrokes.png"), -4, -4, 54, 54)
         }
 
         return Border(0F, 0F, 47F, 47F)
