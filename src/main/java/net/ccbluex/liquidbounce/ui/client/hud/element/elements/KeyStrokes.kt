5bf483bd4691b7a2b0665e2ecb67571a89e8640e
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-13 02:28:54 +0100

    test the code works before i go further

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/KeyStrokes.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/KeyStrokes.kt
@@ -54,11 +54,7 @@ class KeyStrokes : Element(5.0, 25.0, 1.25F, Side.default()) {
             keys.add(KeyStroke(mc.gameSettings.keyBindAttack, 0, 32, 23, 15).initKeyName("L"))
             keys.add(KeyStroke(mc.gameSettings.keyBindUseItem, 24, 32, 23, 15).initKeyName("R"))
         }
-        if(keyStyleValue.get().equals("Juul")) {
-            keys.add(KeyStroke(mc.gameSettings.keyBindAttack, 0, 32, 23, 15).initKeyName(if (CPSCounter.getCPS(CPSCounter.MouseButton.LEFT).toFloat() != 0f) { CPSCounter.getCPS(CPSCounter.MouseButton.LEFT).toString() + "CPS" } else { "Left" }))
-            keys.add(KeyStroke(mc.gameSettings.keyBindUseItem, 24, 32, 23, 15).initKeyName(if (CPSCounter.getCPS(CPSCounter.MouseButton.RIGHT).toFloat() != 0f) { CPSCounter.getCPS(CPSCounter.MouseButton.RIGHT).toString() + "CPS" } else { "Right" }))
-        }
-    }
+    } 
 
     override fun drawElement(partialTicks: Float): Border {
         val backGroundColor = Color(backGroundRedValue.get(), backGroundGreenValue.get(), backGroundBlueValue.get(), backGroundAlphaValue.get())
@@ -73,6 +69,31 @@ class KeyStrokes : Element(5.0, 25.0, 1.25F, Side.default()) {
         }
         if(keyStyleValue.get().equals("Jello")) {
             RenderUtils.drawImage2(ResourceLocation("fdpclient/misc/keystrokes.png"), -3.5f, -3.5f, 54, 54)
+        }
+        if(keyStyleValue.get().equals("Juul")) {
+            RenderUtils.drawRect(0, 32, 23, 15, if (mc.gameSettings.keyBindAttack.isKeyDown) { Color(95, 95, 105, 255) } else { Color(65, 65, 75, 255) } )
+            RenderUtils.drawRect(24, 32, 23, 15, if (mc.gameSettings.keyBindUseItem.isKeyDown) { Color(95, 95, 105, 255) } else { Color(65, 65, 75, 255) } )
+            
+/*
+
+if (mc.gameSettings.keyBindAttack.isKeyDown) { Color(95, 95, 105, 255) } else { Color(65, 65, 75, 255) } 
+if (mc.gameSettings.keyBindUseItem.isKeyDown) { Color(95, 95, 105, 255) } else { Color(65, 65, 75, 255) } 
+        RenderUtils.drawRect(24, 32, 23, 15, if (mc.gameSettings.keyBindUseItem.isKeyDown) { Color(95, 95, 105, 255) } else { Color(65, 65, 75, 255) } )
+
+
+        if(keyStyleValue.get().equals("Juul")) {
+            keys.add(KeyStroke(mc.gameSettings.keyBindAttack, 0, 32, 23, 15).initKeyName(if (CPSCounter.getCPS(CPSCounter.MouseButton.LEFT).toFloat() != 0f) { CPSCounter.getCPS(CPSCounter.MouseButton.LEFT).toString() + "CPS" } else { "Left" }))
+            keys.add(KeyStroke(mc.gameSettings.keyBindUseItem, 24, 32, 23, 15).initKeyName(if (CPSCounter.getCPS(CPSCounter.MouseButton.RIGHT).toFloat() != 0f) { CPSCounter.getCPS(CPSCounter.MouseButton.RIGHT).toString() + "CPS" } else { "Right" }))
+        }
+
+ //mc.gameSettings.keyBindAttack.isKeyDown
+
+
+
+
+
+ */
+
         }
 
         return Border(0F, 0F, 47F, 47F)
@@ -190,33 +211,13 @@ class KeyStroke(val key: KeyBinding, val posX: Int, val posY: Int, val width: In
         GL11.glPushMatrix()
         GL11.glTranslatef(posX.toFloat(), posY.toFloat(), 0F)
 
-        val highLightColor = Color(255 - ((255 - bgColor.red) * highLightPct).toInt(), 255 - ((255 - bgColor.blue) * highLightPct).toInt(), 255 - ((255 - bgColor.green) * highLightPct).toInt())
-        val clickAlpha = 255 - (255 - bgColor.alpha) * highLightPct
-        val centerX = width / 2
-        val centerY = height / 2
         val nowTime = System.currentTimeMillis()
 
-        val rectColor = if (lastClick && animations.isEmpty()) { Color(65, 65, 75, 255) } else { Color(95, 95, 105, 255) }
+        val rectColor = if (lastClick) { Color(65, 65, 75, 255) } else { Color(95, 95, 105, 255) }
         RenderUtils.drawRect(0F, 0F, width.toFloat(), height.toFloat(), rectColor)
-
-        val removeAble = ArrayList<Long>()
-        for (time in animations) {
-            val pct = (nowTime - time) / (speed.toFloat())
-            if (pct> 1) {
-                removeAble.add(time)
-                continue
-            }
-            RenderUtils.drawLimitedCircle(0F, 0F, width.toFloat(), height.toFloat(), centerX, centerY, (width * 0.7F) * pct, Color(255 - ((255 - highLightColor.red) * pct).toInt(), 255 - ((255 - highLightColor.green) * pct).toInt(), 255 - ((255 - highLightColor.blue) * pct).toInt(), 255 - ((255 - clickAlpha) * pct).toInt()))
-        }
-        for (time in removeAble) {
-            animations.remove(time)
-        }
-        if (!lastClick && key.isKeyDown) {
-            animations.add(nowTime)
-        }
         lastClick = key.isKeyDown
 
-        font.drawString(keyName, centerX - (font.getStringWidth(keyName) / 2) + 1, centerY - (font.FONT_HEIGHT / 2) + 2, textColor.rgb)
+        font.drawString(keyName, width / 2 - (font.getStringWidth(keyName) / 2) + 1, height / 2 - (font.FONT_HEIGHT / 2) + 2, textColor.rgb)
 
         GL11.glPopMatrix()
     }
