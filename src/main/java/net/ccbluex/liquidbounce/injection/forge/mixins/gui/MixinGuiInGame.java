13056fd83383dff907a8db2a771a7af2a83b3570
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-14 17:40:35 +0100

    pov build failed + no bitches

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiInGame.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiInGame.java
@@ -125,7 +125,7 @@ public abstract class MixinGuiInGame extends MixinGui {
         */
 
         if(Minecraft.getMinecraft().getRenderViewEntity() instanceof EntityPlayer) {
-        // val hotbarType = HotbarSettings.getBetterHotbarValue().get();  
+            val hotbarType = HotbarSettings.getBetterHotbarValue().get();  
                 //    if(hud.getState() = true){else {false};
             Minecraft mc = Minecraft.getMinecraft();
 
@@ -139,7 +139,7 @@ public abstract class MixinGuiInGame extends MixinGui {
             GlStateManager.enableRescaleNormal();
             GlStateManager.enableBlend();
             GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-            if(HotbarSettings.getHotbarValue().get() = Rise) {
+            if(hotbarType = "Rise") {
                 GlStateManager.disableTexture2D();
                 RenderUtils.quickDrawRect(i - 91, sr.getScaledHeight() - 22, i + 91, sr.getScaledHeight(), new Color(0, 0, 0, HotbarSettings.INSTANCE.getHotbarAlphaValue().get()));
                 RenderUtils.quickDrawRect(itemX, sr.getScaledHeight() - 22, itemX + 22, sr.getScaledHeight() - 21, ColorUtils.INSTANCE.rainbow());
