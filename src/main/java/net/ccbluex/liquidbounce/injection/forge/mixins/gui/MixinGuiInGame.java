031d807daabfca86518db71e80f6c7c441eb243a
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-14 20:42:50 +0100

    a

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiInGame.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiInGame.java
@@ -109,12 +109,28 @@ public abstract class MixinGuiInGame extends MixinGui {
             }
             this.zLevel = f;
             RenderHelper.enableGUIStandardItemLighting();
+            if(hotbarType == "Rise") {
+                
+            } else if (hotbarType == "Rounded") {
 
+            } else {
+
+            }
             for (int j = 0; j < 9; ++j)
             {
+            if(hotbarType == "Rise") {
                 int k = sr.getScaledWidth() / 2 - 90 + j * 20 + 2;
                 int l = sr.getScaledHeight() - 16 - 3;
                 this.renderHotbarItem(j, k, l, partialTicks, entityplayer);
+            } else if (hotbarType == "Rounded") {
+                int k = sr.getScaledWidth() / 2 - 90 + j * 20 + 2;
+                int l = sr.getScaledHeight() - 19 - (true ? 1 : 0);
+                this.renderHotbarItem(j, k, l, partialTicks, entityPlayer);
+            } else {
+                int k = sr.getScaledWidth() / 2 - 90 + j * 20 + 2;
+                int l = sr.getScaledHeight() - 16 - 3;
+                this.renderHotbarItem(j, k, l, partialTicks, entityplayer);
+            }
             }
 
             RenderHelper.disableStandardItemLighting();
