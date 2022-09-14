708ab4f10643c6a880580107e8522e692ca1f527
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-14 13:09:29 +0100

    a

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiInGame.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiInGame.java
@@ -125,7 +125,7 @@ public abstract class MixinGuiInGame extends MixinGui {
         */
 
         if(Minecraft.getMinecraft().getRenderViewEntity() instanceof EntityPlayer) {
-          val canBetterHotbar = if(hud.getState() = true){if(HotbarSettings.getHotbarValue().get() = "Rise") {true} else if(HotbarSettings.getHotbarValue().get() = "Minecraft") {false}};
+          val canBetterHotbar = if(hud.getState() = true){if(HotbarSettings.getHotbarValue().get() = "Rise") {true} else if(HotbarSettings.getHotbarValue().get() = "Minecraft") {false}} else {false};
 
             Minecraft mc = Minecraft.getMinecraft();
             boolean canBetterHotbar = hud.getState() && HotbarSettings.getBetterHotbarValue().get();
