9fdfb996a5da816a621f6d69a42cbde7c0eff7a6
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-18 02:41:52 +0100

    aaa

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Notifications.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Notifications.kt
@@ -343,22 +343,22 @@ class Notification(
                 if(nTypeError){
                     RenderUtils.drawRoundedRect(-x + 9 + textLength, 1f, kek - 1, -28F - 1, 0F, Color(115,69,75).rgb)
                     RenderUtils.drawRoundedRect(-x + 8 + textLength, 0f, kek, -28F, 0F, Color(89,61,65).rgb)
-                    FontLoaders.m20.DisplayFont2(FontLoaders.m20,title, -x - 4, -25F, Color(249,130,108).rgb, true)
+                    FontLoaders.M20.DisplayFont2(FontLoaders.M20,title, -x - 4, -25F, Color(249,130,108).rgb, true)
                 }
                 if(nTypeInfo) {
                     RenderUtils.drawRoundedRect(-x + 9 + textLength,  1f, kek - 1, -28F - 1, 0F, Color(70,94,115).rgb)
                     RenderUtils.drawRoundedRect(-x + 8 + textLength, 0f, kek, -28F, 0F, Color(61,72,87).rgb)
-                    FontLoaders.m20.DisplayFont2(FontLoaders.m20,title, -x - 4, -25F, Color(119,145,147).rgb, true)
+                    FontLoaders.M20.DisplayFont2(FontLoaders.M20,title, -x - 4, -25F, Color(119,145,147).rgb, true)
                 }
                 if(nTypeSuccess){
                     RenderUtils.drawRoundedRect(-x + 9 + textLength, 1f, kek - 1, -28F - 1, 0F, Color(67,104,67).rgb)
                     RenderUtils.drawRoundedRect(-x + 8 + textLength, 0f, kek, -28F, 0F, Color(55,78,55).rgb)
-                    FontLoaders.m20.DisplayFont2(FontLoaders.m20,title, -x - 4, -25F, Color(10,142,2).rgb, true)
+                    FontLoaders.M20.DisplayFont2(FontLoaders.M20,title, -x - 4, -25F, Color(10,142,2).rgb, true)
                 }
                 if(nTypeWarning){
                     RenderUtils.drawRoundedRect(-x + 9 + textLength, 1f, kek - 1, -28F - 1, 0F, Color(103,103,63).rgb)
                     RenderUtils.drawRoundedRect(-x + 8 + textLength, 0f, kek, -28F, 0F, Color(80,80,57).rgb)
-                    FontLoaders.m20.DisplayFont2(FontLoaders.m20,title, -x - 4, -25F, Color(175,163,0).rgb, true)
+                    FontLoaders.M20.DisplayFont2(FontLoaders.M20,title, -x - 4, -25F, Color(175,163,0).rgb, true)
                 }
 
                 Stencil.erase(true)
@@ -375,7 +375,7 @@ class Notification(
                 GlStateManager.enableAlpha()
                 GL11.glPopMatrix()
 
-                FontLoaders.m20.DisplayFont2(FontLoaders.m20,content, -x - 4, -13F, -1, true)
+                FontLoaders.M20.DisplayFont2(FontLoaders.M20,content, -x - 4, -13F, -1, true)
                 return false
         } 
     return false
