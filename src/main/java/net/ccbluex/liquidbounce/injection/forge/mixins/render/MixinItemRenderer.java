ce360358849e2fbd3c7f6530ac7c7a4ffe16fea0
Dg636 <dg636gamez@gmail.com>
2022-09-17 21:32:31 -0400

    Update MixinItemRenderer.java

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinItemRenderer.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinItemRenderer.java
@@ -263,7 +263,19 @@ public abstract class MixinItemRenderer {
                                 doItemRenderGLScale();
                                 doBlockTransformations();
                                 break;
-                                
+                            }
+                            case "Yeet2": {
+                                doItemRenderGLTranslate();
+                                GlStateManager.translate(0.0F, f * -0.8F, 0.0F);
+                                GlStateManager.rotate(45.0F, 0.0F, 1.0F, 0.0F);
+                                float var11 = MathHelper.sin(f1 * f1 * 3.1415927F);
+                                float var12 = MathHelper.sin(MathHelper.sqrt_float(f1) * 3.1415927F);
+                                GlStateManager.rotate(var11 * 0.0F, 0.0F, 1.0F, 0.0F);
+                                GlStateManager.rotate(var12 * 0.0F, 0.0F, 0.0F, 1.0F);
+                                GlStateManager.rotate(var12 * -20.0F - 9.5F, 1.0F, 0.0F, 0.0F);
+                                doItemRenderGLScale();
+                                doBlockTransformations();
+                                break;
                             }
                         }
                         break;
