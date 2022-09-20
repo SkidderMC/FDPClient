1aded7c554f05b439598410d09480ed23f16b9d8
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-20 11:28:14 +0100

    oh mein gott

--- src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/GuiMainMenu.kt
+++ src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/GuiMainMenu.kt
@@ -14,7 +14,7 @@ import net.minecraft.util.ResourceLocation
 import java.awt.Color
 
 class GuiMainMenu : GuiScreen(), GuiYesNoCallback {
-    fun drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float) {
+    override fun drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float) {
         drawBackground(0)
         mc.displayGuiScreen(ModernGuiMainMenu())
     }
