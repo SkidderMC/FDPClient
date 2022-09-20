45a00f12361c098f61abb8420d252efbed676f1c
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-20 04:52:57 +0100

    free shavacado

--- src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/GuiMainMenu.kt
+++ src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/GuiMainMenu.kt
@@ -9,11 +9,12 @@ import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.launch.data.modernui.mainmenu.*
 import net.ccbluex.liquidbounce.utils.render.RenderUtils;
 import net.minecraft.client.gui.*;
+import net.minecraft.util.ResourceLocation
 import java.awt.Color
 
 public class GuiMainMenu : GuiScreen(), GuiYesNoCallback {
-    override fun drawScreen() {
-        RenderUtils.drawImage(ResourceLocation("fdpclient/background.png"), 0, 0, this.res.getScaledWidth(), this.res.getScaledHeight())
+    fun drawScreen() {
+        RenderUtils.drawImage(ResourceLocation("fdpclient/background.png"), 0, 0, sr.ScaledWidth, sr.ScaledHeight)
         mc.displayGuiScreen(ModernGuiMainMenu())
     }
 
