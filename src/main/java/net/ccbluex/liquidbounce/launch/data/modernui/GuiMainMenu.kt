36454feacaacf79e098e501f1d50f2d107936b97
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-20 05:19:46 +0100

    a

--- src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/GuiMainMenu.kt
+++ src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/GuiMainMenu.kt
@@ -13,10 +13,10 @@ import net.minecraft.util.ResourceLocation
 import java.awt.Color
 
 public class GuiMainMenu : GuiScreen(), GuiYesNoCallback {
-    fun drawScreen() {
+    overide fun drawScreen() {
+        mc.displayGuiScreen(ModernGuiMainMenu())
         val sr = ScaledResolution(mc)
         RenderUtils.drawImage(ResourceLocation("fdpclient/background.png"), 0, 0, sr.scaledWidth, sr.scaledHeight)
-        mc.displayGuiScreen(ModernGuiMainMenu())
     }
 
 }
