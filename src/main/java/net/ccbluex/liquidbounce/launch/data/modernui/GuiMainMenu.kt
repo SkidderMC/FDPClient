7353b6e8e01094fda543df4ae3f1037e6252f8b3
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-20 11:17:08 +0100

    d d d d donkey shit!

--- src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/GuiMainMenu.kt
+++ src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/GuiMainMenu.kt
@@ -7,16 +7,16 @@ package net.ccbluex.liquidbounce.launch.data.modernui
 
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.launch.data.modernui.mainmenu.*
-import net.ccbluex.liquidbounce.utils.render.RenderUtils;
-import net.minecraft.client.gui.*;
+import net.ccbluex.liquidbounce.utils.render.RenderUtils
+import net.ccbluex.liquidbounce.ui.client.GuiBackground
+import net.minecraft.client.gui.*
 import net.minecraft.util.ResourceLocation
 import java.awt.Color
 
-public class GuiMainMenu : GuiScreen(), GuiYesNoCallback {
+class ClassicGuiMainMenu : GuiScreen(), GuiYesNoCallback {
     override fun drawScreen() {
+        drawBackground(0)
         mc.displayGuiScreen(ModernGuiMainMenu())
-        val sr = ScaledResolution(mc)
-        RenderUtils.drawImage(ResourceLocation("fdpclient/background.png"), 0, 0, sr.scaledWidth, sr.scaledHeight)
     }
 
 }
