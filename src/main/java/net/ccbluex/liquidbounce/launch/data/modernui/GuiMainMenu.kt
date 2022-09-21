34ef29eebf0c0221197732542ec5faeb036cd7d6
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-21 19:33:59 +0100

    a

--- src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/GuiMainMenu.kt
+++ src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/GuiMainMenu.kt
@@ -8,6 +8,7 @@ package net.ccbluex.liquidbounce.launch.data.modernui
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.launch.data.modernui.mainmenu.*
 import net.ccbluex.liquidbounce.utils.render.RenderUtils
+import net.ccbluex.liquidbounce.features.module.modules.client.HUD
 import net.ccbluex.liquidbounce.ui.client.GuiBackground
 import net.minecraft.client.gui.*
 import net.minecraft.util.ResourceLocation
@@ -15,7 +16,11 @@ import java.awt.Color
 
 class GuiMainMenu : GuiScreen(), GuiYesNoCallback {
     override fun drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float) {
-        mc.displayGuiScreen(ModernGuiMainMenu())
+        if (Hud.mainMenuStyle.equals("Five")){
+            mc.displayGuiScreen(ModernGuiMainMenu())
+        } else {
+            mc.displayGuiScreen(ClassicGuiMainMenu())
+        }
         drawBackground(0)
     }
 
