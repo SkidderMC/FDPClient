bbb3a5733822b6aa0498ec33ee86fb7f160bee57
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-21 16:51:41 +0100

    a

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/DiscordRPCModule.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/DiscordRPCModule.kt
@@ -8,7 +8,7 @@ package net.ccbluex.liquidbounce.features.module.modules.client
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
-import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.*
 
 @ModuleInfo(name = "DiscordRPC", category = ModuleCategory.CLIENT)
 class DiscordRPCModule : Module() {
