ca60c057f01fe20c49d6fb7de665c3f069adb006
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-22 16:48:56 +0100

    update version number

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -60,7 +60,7 @@ object LiquidBounce {
     }
 
     @JvmField
-    val CLIENT_VERSION = "v5.0.0"
+    val CLIENT_VERSION = "v5.0.1"
 
     @JvmField
     val CLIENT_BRANCH = (gitInfo["git.branch"] ?: "unknown").let {
