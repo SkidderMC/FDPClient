441caac98ed7ab3413aa1be63c69e1c9833f154f
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-22 20:32:47 +0100

    5.0.0b

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -60,7 +60,9 @@ object LiquidBounce {
     }
 
     @JvmField
-    val CLIENT_VERSION = "v5.0.1"
+
+    val CLIENT_VERSION = "v5.0.0b"
+
 
     @JvmField
     val CLIENT_BRANCH = (gitInfo["git.branch"] ?: "unknown").let {
