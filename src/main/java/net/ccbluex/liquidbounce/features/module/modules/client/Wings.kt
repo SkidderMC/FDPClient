2b224dad6c225f6ba644de543b58f3b2926c2afc
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-09-30 15:21:22 +0100

    a

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/Wings.kt
@@ -0,0 +1,27 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/SkidderMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.client
+
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.Render3DEvent
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.utils.RenderWings
+import net.ccbluex.liquidbounce.value.BoolValue
+
+
+@ModuleInfo(name = "Wings", category = ModuleCategory.CLIENT, array = false)
+class Wings : Module() {
+    private val onlyThirdPerson = BoolValue("OnlyThirdPerson", true)
+    @EventTarget
+    fun onRenderPlayer(event: Render3DEvent) {
+        if (onlyThirdPerson.get() && mc.gameSettings.thirdPersonView == 0) return
+        val renderWings = RenderWings()
+        renderWings.renderWings(event.partialTicks)
+    }
+}
+
