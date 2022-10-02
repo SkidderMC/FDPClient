07b5bc6472a08e0cdceab950a2ff96b577f3a10a
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-10-02 08:31:34 +0100

    free shavacado?

--- src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/clickgui/style/styles/JelloStyle.java
+++ src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/clickgui/style/styles/JelloStyle.java
@@ -61,7 +61,7 @@ public class JelloStyle extends Style {
         RenderUtils.newDrawRect((float) panel.getX(), (float) panel.getY() - 3, (float) panel.getX() + panel.getWidth(), (float) panel.getY() + 15, new Color(231,229,230).getRGB());
         GlStateManager.resetColor();
         float textWidth = Fonts.font35.getStringWidth("§f" + StringUtils.stripControlCodes(LanguageManager.INSTANCE.get(panel.getName().replaceAll("%",""))));
-        Fonts.font40.drawString("  " + LanguageManager.INSTANCE.get(panel.getName().replaceAll("%","")), (int) (panel.getX() + 4 /*(panel.getWidth() + 10)*/), panel.getY() + 3, new Color(116,114,115).getRGB());
+        Fonts.font40.drawString("  " + LanguageManager.INSTANCE.get(panel.getName().replaceAll("%","")), (int) (panel.getX() - 3 /*(panel.getWidth() + 10)*/), panel.getY() + 3, new Color(116,114,115).getRGB());
     }
 
     @Override
@@ -87,10 +87,10 @@ public class JelloStyle extends Style {
     @Override
     public void drawModuleElement(int mouseX, int mouseY, ModuleElement moduleElement) {
         Gui.drawRect(moduleElement.getX(), moduleElement.getY() - 1, moduleElement.getX() + moduleElement.getWidth(), moduleElement.getY() + moduleElement.getHeight() + 1, new Color(250,250,250).getRGB());
-        Gui.drawRect(moduleElement.getX(), moduleElement.getY() - 1, moduleElement.getX() + moduleElement.getWidth(), moduleElement.getY() + moduleElement.getHeight() + 1, hoverColor(new Color(100,165,241, moduleElement.slowlyFade)).getRGB());
+      //  Gui.drawRect(moduleElement.getX(), moduleElement.getY() - 1, moduleElement.getX() + moduleElement.getWidth(), moduleElement.getY() + moduleElement.getHeight() + 1, hoverColor(new Color(100,165,241, moduleElement.slowlyFade)).getRGB());
         GlStateManager.resetColor();
         Fonts.font35.drawString("  " + LanguageManager.INSTANCE.get(moduleElement.getDisplayName().replaceAll("%","")), moduleElement.getX() + 5, moduleElement.getY() + 5, Color.BLACK.getRGB());
-        Fonts.font35.drawString("  " + LanguageManager.INSTANCE.get(moduleElement.getDisplayName().replaceAll("%","")), moduleElement.getX() + 5, moduleElement.getY() + 5, hoverColor(new Color(255, 255, 255, moduleElement.slowlyFade)).getRGB());
+     //   Fonts.font35.drawString("   " + LanguageManager.INSTANCE.get(moduleElement.getDisplayName().replaceAll("%","")), moduleElement.getX() + 5, moduleElement.getY() + 5, hoverColor(new Color(255, 255, 255, moduleElement.slowlyFade)).getRGB());
 
         // Draw settings
         final List<Value<?>> moduleValues = moduleElement.getModule().getValues();
