0721bd4152af36eafeb6601a614b55503ad71b10
SnooComics9452 <72796659+SnooComics9452@users.noreply.github.com>
2022-10-04 20:42:02 +0800

    Delete src/main directory

--- src/main/java/net/ccbluex/liquidbounce/launch/LaunchFilterInfo.kt
+++
@@ -1,4 +0,0 @@
-package net.ccbluex.liquidbounce.launch
-
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-annotation class LaunchFilterInfo(val filters: Array<EnumLaunchFilter>)
--- src/main/java/net/ccbluex/liquidbounce/utils/MouseUtils.kt
+++
@@ -1,6 +0,0 @@
-package net.ccbluex.liquidbounce.utils
-
-object MouseUtils {
-    @JvmStatic
-    fun mouseWithinBounds(mouseX: Int, mouseY: Int, x: Float, y: Float, x2: Float, y2: Float) = mouseX >= x && mouseX < x2 && mouseY >= y && mouseY < y2
-}
