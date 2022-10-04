d0cca33639ddc466517fe1dd16d920a8a112dbaa
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-10-04 09:50:10 +0100

    a

--- src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/clickgui/style/styles/TenacityFiveStyle.java
+++ src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/clickgui/style/styles/TenacityFiveStyle.java
@@ -58,10 +58,11 @@ public class TenacityFiveStyle extends Style {
     @Override
     public void drawPanel(int mouseX, int mouseY, Panel panel) {
       // top RenderUtils.drawBorderedRect((float) panel.getX(), (float) panel.getY() - 3, (float) panel.getX() + panel.getWidth(), (float) panel.getY() + 17, 3, new Color(217, 217, 217).getRGB(), new Color(217, 217, 217).getRGB());
-        RenderUtils.drawGradientRoundedRect( panel.getX(), panel.getY() - 3, panel.getX() + panel.getWidth(), panel.getY() + 28 + panel.getFade(), 7, new Color(241,135,251).getRGB(), new Color(67,156,251).getRGB()); 
+        RenderUtils.drawGradientRoundedRect( panel.getX(), panel.getY() - 3, panel.getX() + panel.getWidth(), panel.getY() + 30 + panel.getFade(), 7, new Color(241,135,251).getRGB(), new Color(67,156,251).getRGB()); 
         //19--->22
-        RenderUtils.customRounded((float) panel.getX() + 5, (float) panel.getY() + 17 + panel.getFade(), (float) panel.getX() + panel.getWidth() - 5, (float) panel.getY() + 23 + panel.getFade() + 5, 0f, 0f, 7, 7, new Color(33, 33, 33).getRGB());
-           // bottom RenderUtils.drawBorderedRect((float) panel.getX() - 2, (float) panel.getY() + 19 + panel.getFade(), (float) panel.getX() + panel.getWidth() - 2, (float) panel.getY() + 25 + panel.getFade() + 5, 3, new Color(217, 217, 217).getRGB(), new Color(217, 217, 217).getRGB());
+        if (panel.getFade() > 0) {
+        RenderUtils.customRounded((float) panel.getX() + 4, (float) panel.getY() + 17 + panel.getFade(), (float) panel.getX() + panel.getWidth() - 4, (float) panel.getY() + 23 + panel.getFade() + 5, 0f, 0f, 7, 7, new Color(33, 33, 33).getRGB());
+        }
     //67,156,251, 241,135,251
         GlStateManager.resetColor();
         float textWidth = Fonts.font35.getStringWidth("§f" + StringUtils.stripControlCodes(LanguageManager.INSTANCE.get(panel.getName().replaceAll("%",""))));
