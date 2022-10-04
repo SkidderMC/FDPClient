4f7496508b7f364643615fa68576f29e2e71f04b
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-10-04 08:29:18 +0100

    a

--- src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/clickgui/style/styles/TenacityFiveStyle.java
+++ src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/clickgui/style/styles/TenacityFiveStyle.java
@@ -87,7 +87,7 @@ public class TenacityFiveStyle extends Style {
         //Gui.drawRect(moduleElement.getX() - 1, moduleElement.getY() - 1, moduleElement.getX() + moduleElement.getWidth() + 1, moduleElement.getY() + moduleElement.getHeight() + 1, hoverColor(new Color(58,59,60), moduleElement.hoverTime).getRGB());
         //Gui.drawRect(moduleElement.getX() - 1, moduleElement.getY() - 1, moduleElement.getX() + moduleElement.getWidth() + 1, moduleElement.getY() + moduleElement.getHeight() + 1, hoverColor(new Color(0,0,0,20), moduleElement.hoverTime).getRGB());
         if(moduleElement.getModule().getState() == true){
-                    Gui.drawRect(moduleElement.getX() + 2, moduleElement.getY() - 1, moduleElement.getX() + moduleElement.getWidth() -2, moduleElement.getY() + moduleElement.getHeight() + 1, hoverColor(new Color(0,0,0,20), moduleElement.hoverTime).getRGB());
+                    Gui.drawRect(moduleElement.getX() + 2, moduleElement.getY() - 2, moduleElement.getX() + moduleElement.getWidth() -2, moduleElement.getY() + moduleElement.getHeight() + 1, hoverColor(new Color(0,0,0,20), moduleElement.hoverTime).getRGB());
         } else {
             Gui.drawRect(moduleElement.getX() +2, moduleElement.getY() - 1, moduleElement.getX() + moduleElement.getWidth() - 2, moduleElement.getY() + moduleElement.getHeight() + 1, hoverColor(new Color(33, 33, 33), moduleElement.hoverTime).getRGB());
         }
