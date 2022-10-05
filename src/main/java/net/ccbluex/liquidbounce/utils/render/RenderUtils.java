5e5f0ae1d955fa11974742523abbda03f405be21
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-10-05 05:11:43 +0100

    a

--- src/main/java/net/ccbluex/liquidbounce/utils/render/RenderUtils.java
+++ src/main/java/net/ccbluex/liquidbounce/utils/render/RenderUtils.java
@@ -373,22 +373,13 @@ public final class RenderUtils extends MinecraftInstance {
     }
 
     public static void drawTenacityGradientRect(int left, int top, int height, int width, int startColor, int endColor) {
-                            int rote = 0;
-                            while (1 == 1) {
-                             if(rote == 360){
-                                rote = 1;
-                             } else {
-                                rote = rote + 1;
-                             }
-                            }
-                    // get bigger
-                    int trueSize;
-                    trueSize = Math.max(height, width) + (Math.max(height, width) / 2);
-                    // opengl magical shit that does the rotation
-                    glPushMatrix();
-                    GL11.glRotatef(rote, 0f, 1f, 0f);
-                    drawGradientRect(left - (trueSize / 2), top - (trueSize / 2), trueSize, trueSize, startColor, endColor);
-                    glPopMatrix();
+            // get bigger
+            int trueSize = Math.max(height, width) + (Math.max(height, width) / 2);
+            // opengl magical shit that does the rotation
+            glPushMatrix();
+            GL11.glRotatef(((System.nanoTime() / 5000000 * 4) % 360), 0f, 1f, 0f);
+            drawGradientRect(left - (this.trueSize / 2), top - (trueSize / 2), trueSize, trueSize, startColor, endColor);
+            glPopMatrix();
     }
 
     public static void drawGradientSideways(double left, double top, double right, double bottom, int col1, int col2) {
