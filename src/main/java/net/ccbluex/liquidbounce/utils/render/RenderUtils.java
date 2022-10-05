712f500141d826fdb3d7c6b7557881f06ea20136
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-10-05 08:10:14 +0100

    scheisse

--- src/main/java/net/ccbluex/liquidbounce/utils/render/RenderUtils.java
+++ src/main/java/net/ccbluex/liquidbounce/utils/render/RenderUtils.java
@@ -387,8 +387,8 @@ public final class RenderUtils extends MinecraftInstance {
         int trueSize = Math.max(height, width) * 2;
         // opengl magical shit that does the rotation
         glPushMatrix();
-        GL11.glRotatef(((System.nanoTime() / 10000000) % 360), 0f, 0f, 1f);
-        drawGradientRect(left + (trueSize/2), top + (trueSize/2), trueSize, trueSize, startColor, endColor);
+        GL11.glRotatef(((System.nanoTime() / 20000000) % 360), 0f, 0f, 1f);
+        drawGradientRect(left, top, trueSize, trueSize, startColor, endColor);
         glPopMatrix();
        /* Stencil.dispose(); */
     }
