a0dffe39c702cb03befcda728e6a93015df55c12
hard01 <85803222+HelpingHands99@users.noreply.github.com>
2022-10-15 14:04:38 +0800

    Update Rotation.kt

--- src/main/java/net/ccbluex/liquidbounce/utils/Rotation.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/Rotation.kt
@@ -1,12 +1,6 @@
-/*
- * FDPClient Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
- * https://github.com/SkidderMC/FDPClient/
- */
 package net.ccbluex.liquidbounce.utils
 
 import net.ccbluex.liquidbounce.event.StrafeEvent
-import net.ccbluex.liquidbounce.features.module.modules.client.Rotations
 import net.ccbluex.liquidbounce.utils.block.PlaceInfo
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.MathHelper
@@ -21,9 +15,8 @@ data class Rotation(var yaw: Float, var pitch: Float) {
      * Set rotations to [player]
      */
     fun toPlayer(player: EntityPlayer) {
-        if ((yaw.isNaN() || pitch.isNaN()) && Rotations.nanValue.get()) {
+        if (yaw.isNaN() || pitch.isNaN())
             return
-        }
 
         fixedSensitivity(MinecraftInstance.mc.gameSettings.mouseSensitivity)
 
@@ -37,15 +30,6 @@ data class Rotation(var yaw: Float, var pitch: Float) {
      * @see net.minecraft.client.renderer.EntityRenderer.updateCameraAndRender
      */
     fun fixedSensitivity(sensitivity: Float) {
-        if (Rotations.fixedValue.get() == "None") return
-        if (Rotations.fixedValue.get() == "Old") {
-            val f = sensitivity * 0.6F + 0.2F
-            val gcd = f * f * f * 1.2F
-
-            yaw -= yaw % gcd
-            pitch -= pitch % gcd
-            return
-        }
         val f = sensitivity * 0.6F + 0.2F
         val gcd = f * f * f * 1.2F
 
@@ -70,9 +54,9 @@ data class Rotation(var yaw: Float, var pitch: Float) {
      */
     fun applyStrafeToPlayer(event: StrafeEvent) {
         val player = MinecraftInstance.mc.thePlayer
-        val dif = ((MathHelper.wrapAngleTo180_float(player.rotationYaw - this.yaw -
-                23.5f - 135) +
-                180) / 45).toInt()
+        val dif = ((MathHelper.wrapAngleTo180_float(player.rotationYaw - this.yaw
+                    - 23.5f - 135)
+                + 180) / 45).toInt()
 
         val yaw = this.yaw
 
@@ -88,36 +72,43 @@ data class Rotation(var yaw: Float, var pitch: Float) {
                 calcForward = forward
                 calcStrafe = strafe
             }
+
             1 -> {
                 calcForward += forward
                 calcStrafe -= forward
                 calcForward += strafe
                 calcStrafe += strafe
             }
+
             2 -> {
                 calcForward = strafe
                 calcStrafe = -forward
             }
+
             3 -> {
                 calcForward -= forward
                 calcStrafe -= forward
                 calcForward += strafe
                 calcStrafe -= strafe
             }
+
             4 -> {
                 calcForward = -forward
                 calcStrafe = -strafe
             }
+
             5 -> {
                 calcForward -= forward
                 calcStrafe += forward
                 calcForward -= strafe
                 calcStrafe -= strafe
             }
+
             6 -> {
                 calcForward = -strafe
                 calcStrafe = forward
             }
+
             7 -> {
                 calcForward += forward
                 calcStrafe += forward
