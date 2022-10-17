7e71681e9037449b43ed7c41352f6af2fb9e59c8
dizwi <104644889+dizwi@users.noreply.github.com>
2022-10-17 19:58:58 +0300

    Update VerusMotion.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/glides/other/VerusMotion.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/glides/other/VerusMotion.kt
@@ -6,20 +6,6 @@ import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.ccbluex.liquidbounce.value.FloatValue
 
 class VerusMotion : GlideMode("VerusMotion") {
-	private val speedValue = FloatValue("${valuePrefix}VerusMotionSpeed", 0.3f, 0.1f, 1f)
-
-	override fun onMove(event: MoveEvent) {
-		if (MovementUtils.isMoving()) {
-			mc.gameSettings.keyBindJump.pressed = false
-			if (mc.thePlayer.onGround) {
-				MovementUtils.strafe(speedValue.get())
-				event.y = 0.41999998688698
-			} else {
-				MovementUtils.strafe()
-			}
-		}
-	}
-
 	override fun onUpdate() {
 		mc.thePlayer.motionY = -0.0784000015258789
 	}
