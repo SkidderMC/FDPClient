3c3e7d0b8396db85377b1261c56d7de917eef9fb
hxroy34 <105535568+Zerolysimin@users.noreply.github.com>
2022-10-20 17:49:40 -0700

    Update VerusJumpFly.kt
    Signed-off-by: hxroy34 <105535568+Zerolysimin@users.noreply.github.com>

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/verus/VerusJumpFly.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/verus/VerusJumpFly.kt
@@ -1,33 +1,32 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement.flys.verus
 
+import net.ccbluex.liquidbounce.event.BlockBBEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.utils.PacketUtils
 import net.ccbluex.liquidbounce.value.BoolValue
+import net.minecraft.block.BlockAir
 import net.minecraft.client.settings.GameSettings
-
+import net.minecraft.init.Blocks
+import net.minecraft.item.ItemStack
+import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement
+import net.minecraft.util.AxisAlignedBB
+import net.minecraft.util.BlockPos
 
 class VerusJumpFly : FlyMode("VerusJump") {
 
-    private val airStrafeValue = BoolValue("${valuePrefix}AirStrafe", true)
-
     override fun onUpdate(event: UpdateEvent) {
-        mc.gameSettings.keyBindJump.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindJump)
-        if (mc.thePlayer.onGround && MovementUtils.isMoving()) {
-            mc.thePlayer.jump()
-            if (mc.thePlayer.fallDistance > 1) {
-                mc.thePlayer.motionY = -((mc.thePlayer.posY) - Math.floor(mc.thePlayer.posY))
+        val pos = mc.thePlayer.position.add(0.0, -1.5, 0.0)
+        if(mc.gameSettings.keyBindJump.isKeyDown()) {
+        PacketUtils.sendPacketNoEvent(
+            C08PacketPlayerBlockPlacement(pos, 1,
+                ItemStack(Blocks.stone.getItem(mc.theWorld, pos)), 0.0F, 0.5F + Math.random().toFloat() * 0.44.toFloat(), 0.0F)
+        );
+            if (mc.thePlayer.ticksExisted % 5 == 0) {
+                mc.thePlayer.motionY = 0.42
+                MovementUtils.strafe(0.2F)
             }
-
-            if (mc.thePlayer.motionY == 0.0) {
-                mc.thePlayer.jump()
-                mc.thePlayer.onGround = true
-                mc.thePlayer.fallDistance = 0f
-            }
-
-        } else if(airStrafeValue.get()) {
-            MovementUtils.strafe()
         }
     }
-
 }
