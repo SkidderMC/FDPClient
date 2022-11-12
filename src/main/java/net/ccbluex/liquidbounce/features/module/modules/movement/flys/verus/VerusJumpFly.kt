e106af229fc98567ee7e19e0b86dd627af51c4d9
Dg636 <dg636gamez@gmail.com>
2022-11-11 21:50:43 -0500

    Update VerusJumpFly.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/verus/VerusJumpFly.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/verus/VerusJumpFly.kt
@@ -1,26 +1,34 @@
+
+
 package net.ccbluex.liquidbounce.features.module.modules.movement.flys.verus
 
+import net.ccbluex.liquidbounce.event.BlockBBEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
-import net.ccbluex.liquidbounce.utils.PacketUtils
-import net.minecraft.init.Blocks
-import net.minecraft.item.ItemStack
-import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement
+import net.ccbluex.liquidbounce.value.BoolValue
+import net.minecraft.block.BlockAir
+import net.minecraft.client.settings.GameSettings
+import net.minecraft.util.AxisAlignedBB
 
 class VerusJumpFly : FlyMode("VerusJump") {
 
+    private val airStrafeValue = BoolValue("${valuePrefix}AirStrafe", true)
+
     override fun onUpdate(event: UpdateEvent) {
-        val pos = mc.thePlayer.position.add(0.0, -1.5, 0.0)
-        if(mc.gameSettings.keyBindJump.isKeyDown()) {
-        PacketUtils.sendPacketNoEvent(
-            C08PacketPlayerBlockPlacement(pos, 1,
-                ItemStack(Blocks.stone.getItem(mc.theWorld, pos)), 0.0F, 0.5F + Math.random().toFloat() * 0.44.toFloat(), 0.0F)
-        );
-            if (mc.thePlayer.ticksExisted % 5 == 0) {
-                mc.thePlayer.motionY = 0.42
-                MovementUtils.strafe(0.2F)
-            }
+        mc.gameSettings.keyBindJump.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindJump)
+        if (mc.thePlayer.onGround && MovementUtils.isMoving()) {
+            mc.gameSettings.keyBindJump.pressed = false
+            mc.thePlayer.jump()
+            MovementUtils.strafe(0.48F)
+        } else if(airStrafeValue.get()) {
+            MovementUtils.strafe()
+        }
+    }
+
+    override fun onBlockBB(event: BlockBBEvent) {
+        if (event.block is BlockAir && event.y <= fly.launchY) {
+            event.boundingBox = AxisAlignedBB.fromBounds(event.x.toDouble(), event.y.toDouble(), event.z.toDouble(), event.x + 1.0, fly.launchY, event.z + 1.0)
         }
     }
 }
