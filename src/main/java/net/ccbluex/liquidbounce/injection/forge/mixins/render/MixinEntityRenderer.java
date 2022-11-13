02df72b41dd57026f782e1cbe44335d0949bc446
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-11-13 18:53:55 +0000

    happy?

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinEntityRenderer.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinEntityRenderer.java
@@ -31,6 +31,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import static org.objectweb.asm.Opcodes.GETFIELD;
 
 import java.util.List;
 
