f38970ac03ac91394ed5119cfb9cb411b8ca6ef1
MatrixAura <1446679699@qq.com>
2022-11-21 20:10:17 +0800

    Good luck, MurderDetector!

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/MurderDetector.java
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/MurderDetector.java
@@ -11,14 +11,16 @@ import net.ccbluex.liquidbounce.ui.client.hud.element.elements.NotifyType;
 import net.ccbluex.liquidbounce.utils.ClientUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 
 import java.util.HashMap;
+
+// I didn't test you, so good luck, MurderDetector!
 
 @ModuleInfo(name = "MurderDetector", category = ModuleCategory.MISC)
 public class MurderDetector extends Module {
@@ -53,30 +55,27 @@ public class MurderDetector extends Module {
             Items.iron_shovel,
             Items.wooden_shovel
     };
+    private static boolean mode; // Are you Killer?
     public static HashMap<EntityPlayer, KillerData> killerData = new HashMap<>();
 
     @EventTarget
     public static void onUpdate(UpdateEvent ignored) {
         for (Entity entity : mc.theWorld.loadedEntityList) {
-            if (entity instanceof EntityLivingBase) {
-                EntityLivingBase entityLivingBase = (EntityLivingBase) entity;
-                if (entityLivingBase instanceof EntityPlayer) {
-                    EntityPlayer player = (EntityPlayer) entityLivingBase;
-                    if (player.inventory.getCurrentItem() != null && entityLivingBase != mc.thePlayer) {
-                        MurderDetector murderDetector = new MurderDetector();
-                        if (killerData.get(player) == null) {
-                            if (murderDetector.isWeapon(player.inventory.getCurrentItem().getItem())) {
-                                ClientUtils.INSTANCE.displayChatMessage("§a[%module.MurderDetector.name%]§c " + player.getName() + " is Killer!!!");
-                                LiquidBounce.hud.addNotification(new Notification("§a[%module.MurderDetector.name%]§c", player.getName() + " is Killer!!!", NotifyType.WARNING, 4000, 500));
-                                if (killerData.get(player) == null) killerData.put(player, new KillerData(player));
-                            }
-                        } else {
-                            if (!murderDetector.isWeapon(player.inventory.getCurrentItem().getItem())) {
-                                killerData.remove(player);
-                            }
+            if (entity instanceof EntityPlayer) {
+                EntityPlayer player = (EntityPlayer) entity;
+                if (player.inventory.getCurrentItem() != null && entity != mc.thePlayer) {
+                    if (killerData.get(player) == null) {
+                        if (isWeapon(player.inventory.getCurrentItem().getItem())) {
+                            ClientUtils.INSTANCE.displayChatMessage("§a[%module.MurderDetector.name%]§c " + player.getName() + " is Killer!!!");
+                            LiquidBounce.hud.addNotification(new Notification("§a[%module.MurderDetector.name%]§c", player.getName() + " is Killer!!!", NotifyType.WARNING, 4000, 500));
+                            if (killerData.get(player) == null) killerData.put(player, new KillerData(player));
                         }
-
+                    } else {
+                        if (!isWeapon(player.inventory.getCurrentItem().getItem())) {
+                            killerData.remove(player);
+                        }
                     }
+
                 }
             }
         }
@@ -85,12 +84,21 @@ public class MurderDetector extends Module {
     @Override
     public void onEnable() {
         killerData.clear();
+        for (ItemStack itemStack : mc.thePlayer.inventory.mainInventory) {
+            if (itemStack.getItem() != null && isWeapon(itemStack.getItem())) {
+                mode = true;
+                return;
+            }
+        }
+        mode = false;
     }
 
-    public boolean isWeapon(Item item) {
+    public static boolean isWeapon(Item item) {
+        if (mode) {
+            return item == Items.bow; // Bow: HYP
+        }
         for (int id : itemIds) {
             Item itemId = Item.getItemById(id);
-            //ClientUtils.INSTANCE.displayChatMessage(itemId+":"+item);
             if (item == itemId) {
                 return true;
             }
