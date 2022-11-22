37891127c3475cd1445b99e175b1ab67074de8cf
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-11-22 22:26:50 +0000

    zywl... você criou bugs :(

--- src/main/java/net/ccbluex/liquidbounce/utils/MainMenuButton.java
+++ src/main/java/net/ccbluex/liquidbounce/utils/MainMenuButton.java
@@ -7,7 +7,6 @@ package net.ccbluex.liquidbounce.utils;
 
 import net.ccbluex.liquidbounce.ui.client.gui.mainmenu.ModernGuiMainMenu;
 import net.ccbluex.liquidbounce.ui.client.gui.clickgui.fonts.impl.Fonts;
-import net.ccbluex.liquidbounce.utils.render.BlurUtils;
 import net.ccbluex.liquidbounce.utils.render.RenderUtils;
 
 import java.awt.*;
@@ -44,7 +43,6 @@ public class MainMenuButton {
         this.x = x;
         this.y = y;
         RenderUtils.drawRoundedCornerRect(x-30F,y-30F,x+30f,y+30f,15f,new Color(0,0,0,40).getRGB());
-        BlurUtils.INSTANCE.draw(x-40F,y-40F,80f,80f,20f);
         this.yAnimation = RenderUtils.smoothAnimation(this.yAnimation, RenderUtils.isHovering(mouseX, mouseY, this.x-30f, this.y-30f, this.x + 30.0F, this.y + 30.0F) ? 4.0F : 0.0F, 20.0F, 0.3F);
         Fonts.MAINMENU.MAINMENU30.MAINMENU30.drawString(this.icon, x - (float) Fonts.MAINMENU.MAINMENU30.MAINMENU30.stringWidth(this.icon) / 2.0F, y-6f+(this.yAnimation*-1f), Color.WHITE.getRGB(),false);
         if(this.yAnimation>=0.11) {
