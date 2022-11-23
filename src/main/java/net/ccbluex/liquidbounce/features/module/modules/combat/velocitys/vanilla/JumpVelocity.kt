ae93093d17981cf784959df1141f9970143ae28e
DinoFengz <kingsley.tan829@gmail.com>
2022-11-23 15:00:36 +0800

    PR #634 JumpVelocity Better

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/vanilla/JumpVelocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/vanilla/JumpVelocity.kt
@@ -2,11 +2,24 @@ package net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.vanill
 
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.VelocityMode
+import net.ccbluex.liquidbounce.features.value.FloatValue
+import net.ccbluex.liquidbounce.features.value.ListValue
 
 class JumpVelocity : VelocityMode("Jump") {
+    private val modeValue = ListValue("${valuePrefix}Mode", arrayOf("Motion", "Jump", "Both"), "Motion")
+    private val motionValue = FloatValue("${valuePrefix}Motion", 0.42f, 0.4f, 0.5f)
     override fun onVelocity(event: UpdateEvent) {
         if (mc.thePlayer.hurtTime > 0 && mc.thePlayer.onGround) {
-            mc.thePlayer.motionY = 0.42
+            when(modeValue.get().lowercase()) {
+                "motion" -> mc.thePlayer.motionY = motionValue.get().toDouble()
+
+                "jump" -> mc.thePlayer.jump()
+
+                "both" -> {
+                    mc.thePlayer.motionY = motionValue.get().toDouble()
+                    mc.thePlayer.jump()
+                }
+            }
         }
     }
 }
