1ce25f15125c81fac2ee74a9195bcf582c3eddaa
Dg636 <dg636gamez@gmail.com>
2022-12-07 21:58:48 -0500

    Update HorizonCritical.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/criticals/other/HorizonCritical.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/criticals/other/HorizonCritical.kt
@@ -3,7 +3,7 @@ package net.ccbluex.liquidbounce.features.module.modules.combat.criticals.other
 import net.ccbluex.liquidbounce.event.AttackEvent
 import net.ccbluex.liquidbounce.features.module.modules.combat.criticals.CriticalMode
 
-class BlocksMCCritical : CriticalMode("BlocksMC") {
+class HorizonCritical : CriticalMode("Horizon") {
     override fun onAttack(event: AttackEvent) {
         critical.sendCriticalPacket(yOffset = 0.0001, ground = true)
         critical.sendCriticalPacket(ground = false)
