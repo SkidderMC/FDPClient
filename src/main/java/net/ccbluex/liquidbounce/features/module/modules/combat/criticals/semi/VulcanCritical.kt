cd24508748ca8dcf1e97f51aee71a2e8abc0057f
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-12-11 22:18:56 +0800

    Vulcan updated to 2.7.0 and now it FULLY bypassed

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/criticals/semi/VulcanCritical.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/criticals/semi/VulcanCritical.kt
@@ -3,19 +3,17 @@ package net.ccbluex.liquidbounce.features.module.modules.combat.criticals.semi
 import net.ccbluex.liquidbounce.event.AttackEvent
 import net.ccbluex.liquidbounce.features.module.modules.combat.criticals.CriticalMode
 
-class VulcanSemiCritical : CriticalMode("VulcanSemi") {
+class VulcanCritical : CriticalMode("Vulcan") {
     private var attacks = 0
     override fun onEnable() {
         attacks = 0
     }
     override fun onAttack(event: AttackEvent) {
-        attacks++
-        if (attacks > 6) {
+        attacks++ //Vulcan updated and it fully bypassed, that's pogggg *bruh
+        if (attacks > 6) { //TODO: when you stopped moving it may flags sometimes (doesn't matter)
             critical.sendCriticalPacket(yOffset = 0.2, ground = false)
             critical.sendCriticalPacket(yOffset = 0.1216, ground = false)
             attacks = 0
-        } else {
-            critical.antiDesync = false
         }
     }
 }
