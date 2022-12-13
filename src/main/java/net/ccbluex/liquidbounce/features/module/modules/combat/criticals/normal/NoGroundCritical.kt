d0031bb507634cb3b829b10703dc881c763791e3
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-12-13 20:01:32 +0800

    better NoGround

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/criticals/normal/NoGroundCritical.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/criticals/normal/NoGroundCritical.kt
@@ -8,30 +8,33 @@ import net.ccbluex.liquidbounce.features.value.IntegerValue
 import net.minecraft.network.play.client.C03PacketPlayer
 
 class NoGroundCritical : CriticalMode("NoGround") {
-    private val sNoGround = BoolValue("${valuePrefix}SmartNoGround", true)
-    private val newPacket = BoolValue("${valuePrefix}SmartNoGroundNewPacket", true).displayable {sNoGround.get()}
-    private val packetAmount = IntegerValue("${valuePrefix}SmartNoGroundPacketAmount", 2, 1, 5).displayable {sNoGround.get() && newPacket.get()}
+    private val autoJumpValue = BoolValue("${valuePrefix}AutoJump", false)
+    private val smartValue = BoolValue("${valuePrefix}Smart", true)
+    private val morePacketValue = BoolValue("${valuePrefix}MorePacket", false)
+    private val packetAmount = IntegerValue("${valuePrefix}PacketAmount", 2, 1, 5).displayable {morePacketValue.get()}
     
     private var shouldEdit = false
     override fun onEnable() {
-        mc.thePlayer.jump()
+        if (autoJumpValue.get()) {
+            mc.thePlayer.jump()
+        }
     }
     override fun onAttack(event: AttackEvent) {
-        if (sNoGround.get()){
-            if (newPacket.get()) {
-                repeat(packetAmount.get()) {
-                    critical.sendCriticalPacket(ground = false)
-                }
-            } else {
+        if (morePacketValue.get()) {
+            repeat(packetAmount.get()) {
                 shouldEdit = true
+                critical.sendCriticalPacket(ground = false)
+                shouldEdit = true //make sure to modify one more C03 after attack.
             }
+        } else {
+            shouldEdit = true
         }
     }
 
     override fun onPacket(event: PacketEvent) {
         if(event.packet is C03PacketPlayer) {
-            if (sNoGround.get()){
-                if (!newPacket.get() && shouldEdit) {
+            if (smartValue.get()){
+                if (shouldEdit) {
                     event.packet.onGround = false
                     shouldEdit = false
                 }
