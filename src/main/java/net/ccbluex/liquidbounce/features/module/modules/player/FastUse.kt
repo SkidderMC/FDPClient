86af5a54a0e15aee171c9adbbad34d0ed63aa01a
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2022-12-13 20:59:07 +0800

    Update FastUse.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/FastUse.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/FastUse.kt
@@ -47,7 +47,7 @@ class FastUse : Module() {
     private fun stopUsing() {
         if (viaFixValue.get()) {
             sentPacket = true
-            mc.thePlayer.setItemInUse(mc.thePlayer.itemInUse.item, 31)
+            mc.thePlayer.itemInUseCount = 10
         } else {
             sentPacket = false
             mc.playerController.onStoppedUsingItem(mc.thePlayer)
@@ -72,7 +72,7 @@ class FastUse : Module() {
         lastState = mc.thePlayer.isUsingItem
         
         if (viaFixValue.get() && sentPacket) {
-            mc.thePlayer.setItemInUse(mc.thePlayer.itemInUse.item, 31)
+            mc.thePlayer.itemInUseCount = 10
             return
         }
 
@@ -154,7 +154,7 @@ class FastUse : Module() {
             }
             if (mc.thePlayer.itemInUseDuration >= 30 && viaFixValue.get()) {
                 sentPacket = true
-                mc.thePlayer.setItemInUse(mc.thePlayer.itemInUse.item, 31)
+                mc.thePlayer.itemInUseCount = 10
             }
         }
     }
