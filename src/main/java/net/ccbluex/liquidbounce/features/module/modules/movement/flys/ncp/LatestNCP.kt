76bf3e83df4a73c3a8b8dc62e5a0fa464ceabb24
DinoFengz <kingsley.tan829@gmail.com>
2022-12-16 11:05:53 +0800

    haven't tested Fly ;/

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/ncp/LatestNCP.kt
@@ -0,0 +1,41 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.flys.ncp
+
+import jdk.nashorn.internal.ir.Block
+import net.ccbluex.liquidbounce.event.BlockBBEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
+import net.ccbluex.liquidbounce.features.value.FloatValue
+import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.minecraft.init.Blocks
+import net.minecraft.util.BlockPos
+
+class LatestNCP : FlyMode("LatestNCP") {
+    private val speedValue = FloatValue("Speed", 15f, 10f, 20f)
+    private val timerValue = FloatValue("Timer", 0.8F , 0.5f , 1.0f)
+    private var blockPos: BlockPos? = null
+    private var setBlocks = false
+    private var jumped = false
+
+    override fun onEnable() {
+        blockPos = null
+        setBlocks = false
+        jumped = false
+    }
+
+    override fun onUpdate(event: UpdateEvent) {
+        if(!setBlocks) {
+            blockPos = BlockPos(mc.thePlayer.posX , mc.thePlayer.posY + 2, mc.thePlayer.posZ)
+            mc.theWorld.setBlockState(blockPos, Blocks.barrier.defaultState)
+            setBlocks = true
+        } else {
+            mc.timer.timerSpeed = timerValue.get()
+            if(!jumped) {
+                mc.thePlayer.jump()
+                jumped = true
+            } else {
+                MovementUtils.strafe(speedValue.get())
+            }
+        }
+    }
+
+}
