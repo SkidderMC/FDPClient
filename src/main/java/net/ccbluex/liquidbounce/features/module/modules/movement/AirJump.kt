6fb86587b9a445281122420f047fe1bbfc13b6db
DinoFengz <kingsley.tan829@gmail.com>
2022-12-16 10:32:17 +0800

    yay

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AirJump.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AirJump.kt
@@ -6,19 +6,33 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
 import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.ccbluex.liquidbounce.utils.block.BlockUtils
 import net.minecraft.block.BlockLadder
 import net.minecraft.block.BlockVine
+import net.minecraft.network.play.client.C03PacketPlayer
 import net.minecraft.util.BlockPos
 
 @ModuleInfo(name = "AirJump", category = ModuleCategory.MOVEMENT)
 class AirJump : Module() {
+    private val airSpoofValue = BoolValue("AirSpoof", false)
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
         mc.thePlayer.onGround = true
+    }
+
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
+        val packet = event.packet
+        if(packet is C03PacketPlayer) {
+            if(airSpoofValue.get()) {
+                packet.onGround = true
+            }
+        }
     }
 }
--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AirLadder.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/AirLadder.kt
@@ -10,6 +10,7 @@ import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.features.value.FloatValue
 import net.ccbluex.liquidbounce.utils.block.BlockUtils
 import net.minecraft.block.BlockLadder
 import net.minecraft.block.BlockVine
@@ -17,11 +18,12 @@ import net.minecraft.util.BlockPos
 
 @ModuleInfo(name = "AirLadder", category = ModuleCategory.MOVEMENT)
 class AirLadder : Module() {
+    private val yMotionValue = FloatValue("YMotion", 0.15f, 0.1f, 0.2f)
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
         val block = BlockUtils.getBlock(BlockPos(mc.thePlayer.posX, mc.thePlayer.posY + 1, mc.thePlayer.posZ))
         if ((block is BlockLadder && mc.thePlayer.isCollidedHorizontally) || (block is BlockVine || BlockUtils.getBlock(BlockPos(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ)) is BlockVine)) {
-            mc.thePlayer.motionY = 0.15
+            mc.thePlayer.motionY = yMotionValue.get().toDouble()
             mc.thePlayer.motionX = 0.0
             mc.thePlayer.motionZ = 0.0
         }
