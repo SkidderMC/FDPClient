4dfdb414e4b28353205b0dd21c855ea02d068f78
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-12-22 13:50:01 +0000

    revert the downgraded particles :skull:
    yeah fdp had the same module but better so dont skid the glass one since its worse

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/DamageParticle.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/DamageParticle.kt
@@ -12,7 +12,6 @@ import net.ccbluex.liquidbounce.event.WorldEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
-import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.ccbluex.liquidbounce.utils.EntityUtils
 import net.ccbluex.liquidbounce.utils.render.ColorUtils
 import net.ccbluex.liquidbounce.features.value.IntegerValue
@@ -30,13 +29,12 @@ class DamageParticle : Module() {
 
     private val aliveTicksValue = IntegerValue("AliveTicks", 20, 10, 50)
     private val sizeValue = IntegerValue("Size", 3, 1, 7)
-    private val colorRedValue = IntegerValue("Red", 68, 0, 255).displayable { !colorRainbowValue.get() }
-    private val colorGreenValue = IntegerValue("Green", 117, 0, 255).displayable { !colorRainbowValue.get() }
-    private val colorBlueValue = IntegerValue("Blue", 255, 0, 255).displayable { !colorRainbowValue.get() }
+    private val colourValue = ListValue("ColourType", arrayOf("Damage", "Custom", "Rainbow"), "Custom")
+    private val colorRedValue = IntegerValue("Red", 68, 0, 255).displayable { colourValue.get().equals("Custom") }
+    private val colorGreenValue = IntegerValue("Green", 117, 0, 255).displayable { colourValue.get().equals("Custom") }
+    private val colorBlueValue = IntegerValue("Blue", 255, 0, 255).displayable { colourValue.get().equals("Custom") }
     private val colorAlphaValue = IntegerValue("Alpha", 100, 0, 255)
-    private val colorRainbowValue = BoolValue("Rainbow", false)
-    private val shadowValue = ListValue("Mode", arrayOf("LB", "Default", "Autumn", "Outline", "None"), "None")
-
+    private val shadowValue = ListValue("Mode", arrayOf("LB", "Default", "Autumn", "Outline", "None"), "Outline").displayable { !colourValue.get().equals("Damage") }
     private val healthData = mutableMapOf<Int, Float>()
     private val particles = mutableListOf<SingleParticle>()
 
@@ -44,14 +42,13 @@ class DamageParticle : Module() {
     fun onUpdate(event: UpdateEvent) {
         synchronized(particles) {
             for(entity in mc.theWorld.loadedEntityList) {
-                if(entity is EntityLivingBase && EntityUtils.isSelected(entity,true) && mc.thePlayer.getDistanceToEntity(entity) < 5) {
+                if(entity is EntityLivingBase && EntityUtils.isSelected(entity,true)) {
                     val lastHealth = healthData.getOrDefault(entity.entityId,entity.maxHealth)
                     healthData[entity.entityId] = entity.health
                     if(lastHealth == entity.health) continue
-                    if(lastHealth<entity.health){ return }
-
-                    val prefix = if (!colorRainbowValue.get()) (if(lastHealth>entity.health){"❤"}else{return}) else (if(lastHealth>entity.health){"-"}else{return})
-                    particles.add(SingleParticle(prefix + BigDecimal(abs(lastHealth - entity.health).toDouble()).setScale(1, BigDecimal.ROUND_HALF_UP).toDouble()
+                    val colourPrefix = if (colourValue.get().equals("Damage")) (if(lastHealth>entity.health){"§c"}else{"§a"}) else ""
+                    val prefix = if (!colourValue.get().equals("Rainbow")) (if(lastHealth>entity.health){"❤"}else{"❤"}) else (if(lastHealth>entity.health){"-"}else{"+"})
+                    particles.add(SingleParticle(colourPrefix + prefix + BigDecimal(abs(lastHealth - entity.health).toDouble()).setScale(1, BigDecimal.ROUND_HALF_UP).toDouble()
                         ,entity.posX - 0.5 + Random(System.currentTimeMillis()).nextInt(5).toDouble() * 0.1
                         ,entity.entityBoundingBox.minY + (entity.entityBoundingBox.maxY - entity.entityBoundingBox.minY) / 2.0
                         ,entity.posZ - 0.5 + Random(System.currentTimeMillis() + 1L).nextInt(5).toDouble() * 0.1)
@@ -88,24 +85,26 @@ class DamageParticle : Module() {
                 GlStateManager.translate(n.toFloat(), n2.toFloat(), n3.toFloat())
                 GlStateManager.rotate(-renderManager.playerViewY, 0.0f, 1.0f, 0.0f)
                 val textY = if (mc.gameSettings.thirdPersonView == 2) { -1.0f } else { 1.0f }
+
                 GlStateManager.rotate(renderManager.playerViewX, textY, 0.0f, 0.0f)
                 GlStateManager.scale(-size, -size, size)
                 GL11.glDepthMask(false)
-                val doge = if (!colorRainbowValue.get()) "§c" else ""
                 val x = -(mc.fontRendererObj.getStringWidth(particle.str) / 2)
                 val y = -(mc.fontRendererObj.FONT_HEIGHT - 1)
-                when (shadowValue.get()) {
-                    "LB" -> {mc.fontRendererObj.drawString(particle.str, (x + 1), (y + 1), Color(0, 0, 0, 150).rgb)}
-                    "Autumn" -> {mc.fontRendererObj.drawString(particle.str, (x + 1), (y + 1), Color(20, 20, 20, 150).rgb)}
-                    "Default" -> {mc.fontRendererObj.drawString(particle.str, (x + 0.5f), (y + 0.5f), Color(0, 0, 0, 130).rgb, false)}
-                    "Outline" -> {
-                        mc.fontRendererObj.drawString(particle.str, x + 0.5F, y + 0.5F, Color(0, 0, 0, 130).rgb, false)
-                        mc.fontRendererObj.drawString(particle.str, x - 0.5F, y - 0.5F, Color(0, 0, 0, 130).rgb, false)
-                        mc.fontRendererObj.drawString(particle.str, x + 0.5F, y - 0.5F, Color(0, 0, 0, 130).rgb, false)
-                        mc.fontRendererObj.drawString(particle.str, x - 0.5F, y + 0.5F, Color(0, 0, 0, 130).rgb, false)
+                if(!colourValue.get().equals("Damage")){
+                    when (shadowValue.get()) {
+                        "LB" -> {mc.fontRendererObj.drawString(particle.str, (x + 1), (y + 1), Color(0, 0, 0, 150).rgb)}
+                        "Autumn" -> {mc.fontRendererObj.drawString(particle.str, (x + 1), (y + 1), Color(20, 20, 20, 150).rgb)}
+                        "Default" -> {mc.fontRendererObj.drawString(particle.str, (x + 0.5f), (y + 0.5f), Color(0, 0, 0, 130).rgb, false)}
+                        "Outline" -> {
+                            mc.fontRendererObj.drawString(particle.str, x + 0.5F, y + 0.5F, Color(0, 0, 0, 130).rgb, false)
+                            mc.fontRendererObj.drawString(particle.str, x - 0.5F, y - 0.5F, Color(0, 0, 0, 130).rgb, false)
+                            mc.fontRendererObj.drawString(particle.str, x + 0.5F, y - 0.5F, Color(0, 0, 0, 130).rgb, false)
+                            mc.fontRendererObj.drawString(particle.str, x - 0.5F, y + 0.5F, Color(0, 0, 0, 130).rgb, false)
+                        }
                     }
                 }
-                mc.fontRendererObj.drawString(doge + particle.str, (-(mc.fontRendererObj.getStringWidth(particle.str) / 2)), (-(mc.fontRendererObj.FONT_HEIGHT - 1)), (if (colorRainbowValue.get()) ColorUtils.rainbowWithAlpha(colorAlphaValue.get()) else Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get(), colorAlphaValue.get())).rgb)
+                mc.fontRendererObj.drawString(particle.str, (-(mc.fontRendererObj.getStringWidth(particle.str) / 2)).toFloat(), (-(mc.fontRendererObj.FONT_HEIGHT - 1)).toFloat(), (if (colourValue.get().equals("Rainbow")) ColorUtils.rainbowWithAlpha(colorAlphaValue.get()) else Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get(), colorAlphaValue.get())).rgb, false)
                 GL11.glColor4f(187.0f, 255.0f, 255.0f, 1.0f)
                 GL11.glDepthMask(true)
                 GlStateManager.doPolygonOffset(1.0f, 1500000.0f)
