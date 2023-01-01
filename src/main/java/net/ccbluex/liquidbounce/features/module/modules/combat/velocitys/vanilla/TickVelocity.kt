60b85f6bef0cbb7c24c9f18642ae686e4b9da8fb
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2023-01-01 13:33:49 +0800

    Better Tick Velocity

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/vanilla/TickVelocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/vanilla/TickVelocity.kt
@@ -3,15 +3,23 @@ package net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.vanill
 import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.VelocityMode
+import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.minecraft.network.play.server.S12PacketEntityVelocity
 
 class TickVelocity : VelocityMode("Tick") {
+    private val verticalValue = BoolValue("${valuePrefix}ResetMotionY", true)
+    private val bypassValue = BoolValue("${valuePrefix}TickBypass", true)
     override fun onVelocity(event: UpdateEvent) {
         if(velocity.velocityTick > velocity.velocityTickValue.get()) {
-            if(mc.thePlayer.motionY > 0) mc.thePlayer.motionY = 0.0
+            if(mc.thePlayer.motionY > 0 && verticalValue.get())
+                mc.thePlayer.motionY = 0.0
             mc.thePlayer.motionX = 0.0
             mc.thePlayer.motionZ = 0.0
-            mc.thePlayer.jumpMovementFactor = -0.00001f
+            if(bypassValue.get()) {
+                mc.thePlayer.jumpMovementFactor = -0.001f
+            }else {
+                mc.thePlayer.jumpMovementFactor = 0.0f
+            }
             velocity.velocityInput = false
         }
         if(mc.thePlayer.onGround && velocity.velocityTick > 1) {
