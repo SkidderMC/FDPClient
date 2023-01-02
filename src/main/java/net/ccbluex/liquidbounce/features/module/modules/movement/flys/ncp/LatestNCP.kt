32e0996bcaa92fb13ec48ebaaa8cacdc31f7b7b6
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2023-01-02 20:50:13 +0800

    Update LatestNCP.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/ncp/LatestNCP.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/ncp/LatestNCP.kt
@@ -1,37 +1,30 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement.flys.ncp
 
-import jdk.nashorn.internal.ir.Block
-import net.ccbluex.liquidbounce.event.BlockBBEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
 import net.ccbluex.liquidbounce.features.value.FloatValue
 import net.ccbluex.liquidbounce.utils.MovementUtils
-import net.minecraft.init.Blocks
-import net.minecraft.util.BlockPos
 
 class LatestNCP : FlyMode("LatestNCP") {
-    private val speedValue = FloatValue("Speed", 15f, 10f, 20f)
-    private val timerValue = FloatValue("Timer", 0.8F , 0.5f , 1.0f)
+    private val speedValue = FloatValue("${valuePrefix}Speed", 7f, 1f, 12f)
+    private val timerValue = FloatValue("${valuePrefix}Timer", 0.8F , 0.1f , 1.0f)
     private var jumped = false
 
     override fun onEnable() {
         if(!mc.thePlayer.onGround) {
             fly.state = false
-            //alert
         } else {
             mc.thePlayer.jump()
-            mc.thePlayer.motionY = 0.2
             jumped = true
         }
     }
 
     override fun onUpdate(event: UpdateEvent) {
         mc.timer.timerSpeed = timerValue.get()
-        if(jumped) {
+        if(jumped && !mc.thePlayer.onGround) {
             jumped = false
-            mc.thePlayer.motionY = -0.0784
+            MovementUtils.strafe(speedValue.get())
         }
-        MovementUtils.strafe(speedValue.get())
     }
 
     override fun onDisable() {
