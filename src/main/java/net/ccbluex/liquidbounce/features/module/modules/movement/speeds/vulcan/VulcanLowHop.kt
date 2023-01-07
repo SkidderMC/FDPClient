f4493577c3087bc871c7fbfb84fdeff1b8e714c9
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2023-01-07 08:20:02 +0800

    Vulcan LowHop

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/vulcan/VulcanLowHop.kt
@@ -0,0 +1,44 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.vulcan
+
+import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
+import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.minecraft.client.settings.GameSettings
+
+class VulcanLowHopSpeed : SpeedMode("VulcanLowHop") {
+    private var ticks = 0
+    private var launchY = 0.0
+
+    override fun onUpdate() {
+        ticks++
+        mc.thePlayer.jumpMovementFactor = 0.0245
+        if (mc.thePlayer.onGround && MovementUtils.isMoving()) {
+            mc.thePlayer.jump()
+            ticks = 0
+            MovementUtils.strafe()
+            if (MovementUtils.getSpeed() < 0.5f) {
+                MovementUtils.strafe(0.484f)
+            }
+            launchY = mc.thePlayer.posY
+        }else if (mc.thePlayer.posY > launchY && ticks <= 1) {
+            mc.thePlayer.setPosition(mc.thePlayer.posX, launchY, mc.thePlayer.posZ)
+        }else if (ticks == 5) {
+            mc.thePlayer.motionY = -0.17
+        }
+        if (MovementUtils.getSpeed() < 0.215) {
+            MovementUtils.strafe(0.215f)
+        }else {
+            MovementUtils.strafe()
+        }
+    }
+
+    override fun onDisable() {
+        ticks = 0
+        mc.timer.timerSpeed = 1f
+    }
+    
+    override fun onEnable() {
+        ticks = 0
+        mc.timer.timerSpeed = 1f
+        launchY = mc.thePlayer.posY
+    }
+}
