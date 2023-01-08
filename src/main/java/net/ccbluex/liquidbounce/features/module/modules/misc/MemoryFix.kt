6722124e601a63e81e89e8f4da730c598c502216
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2023-01-08 10:50:24 +0800

    test

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/MemoryFix.kt
@@ -0,0 +1,23 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/SkidderMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.misc
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.event.WorldEvent
+
+@ModuleInfo(name = "MemoryFix",  category = ModuleCategory.MISC)
+class MemoryFix : Module() {
+    override fun onEnable() {
+        Runtime.getRuntime().gc()
+    }
+    
+    @EventTarget
+    fun onWorld(event: WorldEvent) {
+        Runtime.getRuntime().gc()
+    }
+}
