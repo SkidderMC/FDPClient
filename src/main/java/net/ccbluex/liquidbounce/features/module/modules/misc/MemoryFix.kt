7d9c3a4f0ebdc79e63d3a222f4ad34ef754ffa1c
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2023-01-08 10:54:43 +0800

    Update MemoryFix.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/MemoryFix.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/MemoryFix.kt
@@ -8,6 +8,7 @@ package net.ccbluex.liquidbounce.features.module.modules.misc
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.event.WorldEvent
 
 @ModuleInfo(name = "MemoryFix",  category = ModuleCategory.MISC)
