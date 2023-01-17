baa45866397eef0aa6479c7be7c6cdd4cbd2821f
Dg636 <dg636gamez@gmail.com>
2023-01-17 18:51:21 -0500

    Update JumpVelocity.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/vanilla/JumpVelocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/vanilla/JumpVelocity.kt
@@ -11,34 +11,10 @@ class JumpVelocity : VelocityMode("Jump") {
     private val motionValue = FloatValue("${valuePrefix}Motion", 0.42f, 0.4f, 0.5f)
     private val failValue = BoolValue("${valuePrefix}SmartFail", true)
     private val failRateValue = FloatValue("${valuePrefix}FailRate", 0.3f, 0.0f, 1.0f).displayable { failValue.get() }
-    private val failJumpValue = FloatValue("${valuePrefix}FailJumpRate", 0.25f, 0.0f, 1.0f).displayable { failValue.get() }
-    
-    private var doJump = true
-    private var failJump = false
-    private var skipVeloc = false
     
     override fun onVelocity(event: UpdateEvent) {
         if (mc.thePlayer.onGround && (failJump || mc.thePlayer.hurtTime > 6)) {
-            if (failJump) {
-                failJump = false
-            }
-            if (!doJump) {
-                skipVeloc = true
-            }
             if (Math.random() <= failRateValue.get() && failValue.get()) {
-                if (Math.random() <= failJumpValue.get()) {
-                    doJump = true
-                    failJump = true
-                } else {
-                    doJump = false
-                    failJump = false
-                }
-            } else {
-                doJump = true
-                failJump = false
-            }
-            if (skipVeloc) {
-                skipVeloc = false
                 return
             }
             when(modeValue.get().lowercase()) {
