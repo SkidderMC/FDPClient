1ae8c084148f9b20d03891e6fb309b910cf5b46b
opZywl <lifeerwz@gmail.com>
2023-01-18 12:12:14 -0300

    stop add useless things

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Clock.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Clock.kt
@@ -28,44 +28,33 @@ class Clock : Module() {
     private val widthValue = FloatValue("Width", 100f, 30f, 300f)
     private val xValue = FloatValue("X", 0f, 0f, 2000f)
     private val yValue = FloatValue("Y", 0f, 0f, 2000f)
-   // private val dynamicValue = BoolValue("Dynamic", true)
 
     @EventTarget
     fun onRender2D(event: Render2DEvent) {
         val now = LocalDateTime.now()
-        // stops the entire screen being moved xd
         glPushMatrix()
-        // move it to the pos on screen
         glTranslatef(xValue.get(), yValue.get(), 0F)
         when(anaStyleValue.get()){
             "Hands" -> {
                 val minutes: Float = now.minute + (now.second + System.currentTimeMillis() % 1000F / 1000F) / 60.0F
                 val hours: Float = now.hour + minutes / 60F
-                // Minutes
                 simpleHands(3.0f, minutes / 60F * 100F, widthValue.get() * 0.90F, 5F, Color(255, 255, 255))
-                // Hours
                 simpleHands(3.0f, (hours % 12F) / 12F * 100F, widthValue.get() * 0.5F, 5F, Color(255, 255, 255))
             }
             "Android" -> {
                 val minutes: Float = now.minute + (now.second + System.currentTimeMillis() % 1000F / 1000F) / 60.0F
                 val hours: Float = now.hour + minutes / 60F
-                // Minutes
                 simpleHands(3.0f, minutes / 60F * 100F, widthValue.get() * 0.90F, 5F, Color(255, 255, 255))
-                // Hours
                 simpleHands(3.0f, (hours % 12F) / 12F * 100F, widthValue.get() * 0.5F, 5F, Color(255, 255, 255))
 
                 RenderUtils.drawCircle(0f, 0f, widthValue.get(), 3f, 0, 360, Color(255, 255, 255))
             }
-            "Minimal" -> { // maybe remove this since its ugly
-                // get the time
+            "Minimal" -> {
                 val seconds: Float = now.second + System.currentTimeMillis() % 1000F / 1000F
                 val minutes: Float = now.minute + seconds / 60.0F
                 val hours: Float = now.hour + minutes / 60F
-                // Seconds
                 simpleHands(2.0f, seconds / 60F * 100F, widthValue.get(), 0f, Color(255, 255, 255))
-                // Minutes
                 simpleHands(4.0f, minutes / 60F * 100F, widthValue.get() * 0.70F, 0f, Color(255, 255, 255))
-                // Hours
                 simpleHands(6.0f, (hours % 12F) / 12F * 100F, widthValue.get() * 0.5F, 0f, Color(255, 255, 255))
 
                 RenderUtils.drawCircle(0f, 0f, widthValue.get(), 3f, 0, 360, Color(255, 255, 255))
@@ -73,17 +62,14 @@ class Clock : Module() {
             }
             "Classic" -> {
                 RenderUtils.drawFilledCircle(0f, 0f, widthValue.get() + 3, Color(255, 255, 255)) // bg
-                // numbers on clock
                 Fonts.font32.drawCenteredString("XII", 0F, -widthValue.get() + 2F, Color(0, 0, 0).rgb, false)
                 Fonts.font32.drawCenteredString("III",  widthValue.get() - 3, -2F, Color(0, 0, 0).rgb, false)
                 Fonts.font32.drawCenteredString("VI", 0F, widthValue.get() - 7f, Color(0, 0, 0).rgb, false)
                 Fonts.font32.drawCenteredString("IX",  -widthValue.get() + 5F, -2F, Color(0, 0, 0).rgb, false)
-                // draw the hands the time
                 simpleHands(2.0f,  (now.second + System.currentTimeMillis() % 1000F / 1000F) / 60F * 100F, widthValue.get() * 0.90F, 7f, Color(255, 0, 0, 200)) // Seconds
                 simpleHands(4.0f, (now.minute + (now.second + System.currentTimeMillis() % 1000F / 1000F) / 60.0F) / 60F * 100F, widthValue.get() * 0.70F, 5f, Color(0, 0, 0))// Minutes
                 simpleHands(6.0f, ((now.hour + (now.minute + (now.second + System.currentTimeMillis() % 1000F / 1000F) / 60.0F) / 60F) % 12F) / 12F * 100F, widthValue.get() * 0.5F, 5f, Color(0, 0, 0))  // Hours
                 RenderUtils.drawCircle(0f, 0f, widthValue.get() + 2, 6f, 0, 360, Color(0, 0, 0)) // rim circle
-                // shadow
                 glTranslated(-xValue.get().toDouble(), -yValue.get().toDouble(), 0.0)
                 glPushMatrix()
                 ShadowUtils.shadow(15F, { glPushMatrix(); glTranslated(xValue.get().toDouble(), yValue.get().toDouble(), 0.0); RenderUtils.drawCircle(0f, 0f, widthValue.get() + 2, 6f, 0, 360, Color(0, 0, 0, 200)); glPopMatrix(); }, {})
@@ -98,12 +84,9 @@ class Clock : Module() {
     private fun simpleHands(width: Float, percent: Float, length: Float, extend: Float, Colour: Color) {
         RenderUtils.glColor(Colour.rgb)
         glDisable(GL_TEXTURE_2D)
-        // set the width of the line
         glLineWidth(width)
         glBegin(GL_LINES)
-        // start point
         glVertex2f(extend * sin((percent) / 100.0 * Math.PI * 2 + Math.PI).toFloat(), -extend * cos((percent) / 100.0 * Math.PI * 2 + Math.PI).toFloat())
-        // end point
         glVertex2f(-length * sin((percent) / 100.0 * Math.PI * 2 + Math.PI).toFloat(), length * cos((percent) / 100.0 * Math.PI * 2 + Math.PI).toFloat())
         glEnd()
         glEnable(GL_TEXTURE_2D)
