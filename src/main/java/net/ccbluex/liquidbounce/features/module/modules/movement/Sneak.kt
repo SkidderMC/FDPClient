52238ebcc8709ad3b61caea20aa8aae47b5ae234
DinoFengz <kingsley.tan829@gmail.com>
2023-01-25 22:04:41 +0800

    #647

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Sneak.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Sneak.kt
@@ -5,31 +5,65 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
+import net.ccbluex.liquidbounce.event.EventState
 import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.event.MotionEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.ccbluex.liquidbounce.features.value.ListValue
-import net.ccbluex.liquidbounce.injection.forge.mixins.client.IMixinKeyBinding
 import net.minecraft.network.play.client.C0BPacketEntityAction
 
 @ModuleInfo(name = "Sneak", category = ModuleCategory.MOVEMENT)
 class Sneak : Module() {
-    private val modeValue = ListValue("Mode", arrayOf("Vanilla", "Packet"), "Vanilla")
+    private val modeValue = ListValue("Mode", arrayOf("Vanilla", "Vanilla2", "Packet", "NCP"), "Vanilla")
     private val onlySneakValue = BoolValue("OnlySneak", false)
     @EventTarget
-    fun onUpdate(event: UpdateEvent) {
+    fun onMotion(event: MotionEvent) {
         if(onlySneakValue.get() && !mc.gameSettings.keyBindSneak.pressed) return
 
-        when(modeValue.get().lowercase()) {
-            "vanilla" -> {
-                mc.gameSettings.keyBindSneak.pressed = true
+        when(event.eventState) {
+            EventState.PRE -> {
+                when(modeValue.get().lowercase()) {
+                    "vanilla" -> {
+                        mc.gameSettings.keyBindSneak.pressed = true
+                    }
+
+                    "vanilla2" -> {
+                        mc.thePlayer.movementInput.sneak = mc.thePlayer.sendQueue.doneLoadingTerrain
+                    }
+
+                    "packet" -> {
+                        mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SNEAKING))
+                    }
+
+                    "ncp" -> {
+                        mc.thePlayer.movementInput.sneak = mc.thePlayer.sendQueue.doneLoadingTerrain
+                        mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SNEAKING))
+                    }
+                }
             }
 
-            "packet" -> {
-                mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SNEAKING))
+            EventState.POST -> {
+                when(modeValue.get().lowercase()) {
+                    "vanilla" -> {
+                        mc.gameSettings.keyBindSneak.pressed = true
+                    }
+
+                    "vanilla2" -> {
+                        mc.thePlayer.movementInput.sneak = mc.thePlayer.sendQueue.doneLoadingTerrain
+                        mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SNEAKING))
+                    }
+
+                    "packet" -> {
+                        mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SNEAKING))
+                    }
+
+                    "ncp" -> {
+                        mc.netHandler.addToSendQueue(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SNEAKING))
+                    }
+                }
             }
         }
     }
