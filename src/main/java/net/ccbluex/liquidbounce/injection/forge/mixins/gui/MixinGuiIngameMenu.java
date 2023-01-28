e646e33f3776a2a9621e212dee93a588d3f60768
Zywl <86253343+opZywl@users.noreply.github.com>
2023-01-27 21:05:08 -0300

    Update MixinGuiIngameMenu.java

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiIngameMenu.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiIngameMenu.java
@@ -35,4 +35,41 @@ public abstract class MixinGuiIngameMenu extends MixinGuiScreen {
             ServerUtils.connectToLastServer();
         }
     }
+    
+    @Inject(method = "drawScreen", at = @At("RETURN"))
+    private void drawScreen(CallbackInfo callbackInfo) {
+        Fonts.minecraftFont.drawStringWithShadow(
+                "§7Username: §a" + mc.getSession().getUsername(),
+                6f,
+                6f,
+                0xffffff);
+        if (!mc.isIntegratedServerRunning()) {
+            Fonts.minecraftFont.drawStringWithShadow(
+                    "§7IP: §a" + mc.getCurrentServerData().serverIP,
+                    6f,
+                    16f,
+                    0xffffff);
+            Fonts.minecraftFont.drawStringWithShadow(
+                    "§7Brand: §a" + mc.getCurrentServerData().gameVersion,
+                    6f,
+                    26f,
+                    0xffffff);
+            Fonts.minecraftFont.drawStringWithShadow(
+                    "§7Protocol: §a" + mc.getCurrentServerData().version,
+                    6f,
+                    36f,
+                    0xffffff);
+            Fonts.minecraftFont.drawStringWithShadow(
+                    "§7Ping: §a" + mc.getCurrentServerData().pingToServer,
+                    6f,
+                    46f,
+                    0xffffff);
+            Fonts.minecraftFont.drawStringWithShadow(
+                    "§7Players: §a" + mc.getCurrentServerData().populationInfo,
+                    6f,
+                    56f,
+                    0xffffff);
+        }
+    }
+    
 }
