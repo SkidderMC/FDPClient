1e18d61409144b43e11f1c395af3de87788c698f
Co丶Dynamic <78670017+contionability@users.noreply.github.com>
2023-02-17 22:25:25 +0800

    Update MixinGuiIngameMenu.java

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiIngameMenu.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiIngameMenu.java
@@ -21,7 +21,7 @@ public abstract class MixinGuiIngameMenu extends MixinGuiScreen {
 
     @Inject(method = "initGui", at = @At("RETURN"))
     private void initGui(CallbackInfo callbackInfo) {
-        if(!this.mc.isIntegratedServerRunning())
+        if(!this.mc.isIntegratedServerRunning()) {
             this.buttonList.add(new GuiButton(1337, this.width / 2 - 100, this.height / 4 + 128, "%ui.reconnect%"));
             this.buttonList.add(new GuiButton(1068,this.width / 2 - 100,this.height / 4 + 128 + 24,"Switcher"));
         } else {
