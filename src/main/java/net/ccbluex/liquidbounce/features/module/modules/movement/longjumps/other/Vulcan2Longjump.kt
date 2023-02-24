a7154c79c407dd7474ae3b7e5c8dbd119d4f37cb
contionability <contionability@gmail.com>
2023-02-24 18:31:27 +0800

    Update Vulcan2Longjump.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/longjumps/other/Vulcan2Longjump.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/longjumps/other/Vulcan2Longjump.kt
@@ -18,19 +18,17 @@ import kotlin.math.cos
 import kotlin.math.sin
 
 class Vulcan2Longjump : LongJumpMode("Vulcan2") {
-    private val repeatValue = IntegerValue("${valuePrefix}RepeatTimes", 2, 1, 6)
-    private val distanceValue = FloatValue("${valuePrefix}Distance", 7.0f, 2.0f, 8.0f)
-    private val onlyDamageValue = BoolValue("${valuePrefix}OnlyDamage", true)
-    private val selfDamageValue = BoolValue("${valuePrefix}SelfDamage", true)
     private var ticks = 0
     private var cancelFlag = false
-    var coDynamic = "Vulcan LongJump Bypass - by Alan wood 69"
+    private var jumped = false
+    //Vulcan LongJump Bypass - by Alan wood 69
     
     override fun onEnable() {
         longjump.noTimerModify = true
         if(!mc.thePlayer.onGround) {
             onAttemptDisable()
         }
+        jumped = false
         mc.timer.timerSpeed = 0.5f
         ticks = 0
     }
@@ -42,34 +40,41 @@ class Vulcan2Longjump : LongJumpMode("Vulcan2") {
     }
     
     override fun onUpdate(event: UpdateEvent) {
+        if (!jumped)
+            return
+
         mc.timer.timerSpeed = 0.5f
         if (mc.thePlayer.fallDistance > 0 && ticks % 2 == 0 && mc.thePlayer.fallDistance < 2.2) 
             mc.thePlayer.motionY += 0.14
         
-        ticks ++
+        ticks++
         
-        if (ticks == 0) {
-            mc.netHandler.addToSendQueue(C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ, true))
-            mc.netHandler.addToSendQueue(C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY - 0.0784, mc.thePlayer.posZ, true))
-            mc.netHandler.addToSendQueue(C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ, false))
-            MovementUtils.strafe(7.9f)
-            mc.thePlayer.motionY = 0.41999998688698
-            cancelFlag = true
+        when (ticks) {
+            1 -> {
+                mc.netHandler.addToSendQueue(C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ, true))
+                mc.netHandler.addToSendQueue(C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY - 0.0784, mc.thePlayer.posZ, false))
+                mc.netHandler.addToSendQueue(C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ, true))
+                MovementUtils.strafe(7.9f)
+                mc.thePlayer.motionY = 0.41999998688698
+                cancelFlag = true
+            }
             
-        } else if (ticks == 1) {
-            mc.thePlayer.motionY += 0.1
-            MovementUtils.strafe(2.79f)
-        } else if (ticks == 2) {
-            MovementUtils.strafe(2.56f)
-        } else if (ticks == 3) {
-            MovementUtils.strafe(0.49f)
-            mc.thePlayer.onGround = true
-        } else if (ticks == 4) {
-            MovementUtils.strafe(0.59f)
-        } else if (ticks == 5) {
-            MovementUtils.strafe(0.3f)
+            2 -> {
+                mc.thePlayer.motionY += 0.1
+                MovementUtils.strafe(2.79f)
+            }
+            
+            3 -> MovementUtils.strafe(2.56f)
+            
+            4 -> {
+                MovementUtils.strafe(0.49f)
+                mc.thePlayer.onGround = true
+            }
+            
+            5 -> MovementUtils.strafe(0.59f)
+            
+            6 -> MovementUtils.strafe(0.3f)
         }
-            
     }
     
     override fun onPacket(event: PacketEvent) {
@@ -82,15 +87,17 @@ class Vulcan2Longjump : LongJumpMode("Vulcan2") {
     }
     
     override fun onAttemptDisable() {
-     //   if (!waitFlag && !isFlagged) {
-    //        longjump.state = false
-    //    }
+        longjump.state = false
     }
     
     override fun onAttemptJump() {
         ticks = 0
+        jumped = true
     }
     
-
- 
+    override fun onJump(event: JumpEvent) {
+        event.cancelEvent()
+        ticks = 0
+        jumped = true
+    }
 }
