f8b656310d0fb18324a720e6a13cf245183c48f2
Dg636 <dg636gamez@gmail.com>
2023-02-28 22:30:24 -0500

    Update Blink.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Blink.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Blink.kt
@@ -7,7 +7,6 @@ package net.ccbluex.liquidbounce.features.module.modules.player
 
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.Render3DEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
@@ -15,19 +14,12 @@ import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.features.module.modules.render.Breadcrumbs
 import net.ccbluex.liquidbounce.utils.render.RenderUtils
-import net.ccbluex.liquidbounce.utils.timer.MSTimer
+import net.ccbluex.liquidbounce.utils.BlinkUtils
 import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.ccbluex.liquidbounce.features.value.IntegerValue
 import net.minecraft.client.entity.EntityOtherPlayerMP
-import net.minecraft.network.Packet
-import net.minecraft.network.play.INetHandlerPlayServer
-import net.minecraft.network.play.client.*
-import net.minecraft.network.play.client.C03PacketPlayer.C04PacketPlayerPosition
-import net.minecraft.network.play.client.C03PacketPlayer.C06PacketPlayerPosLook
-import net.minecraft.network.play.server.S08PacketPlayerPosLook
 import org.lwjgl.opengl.GL11
 import java.util.*
-import java.util.concurrent.LinkedBlockingQueue
 
 @ModuleInfo(name = "Blink", category = ModuleCategory.PLAYER)
 class Blink : Module() {
@@ -37,11 +29,6 @@ class Blink : Module() {
     private val pulseValue = BoolValue("Pulse", false)
     private val pulseDelayValue = IntegerValue("PulseDelay", 1000, 500, 5000).displayable { pulseValue.get() }
 
-    private val pulseTimer = MSTimer()
-    private val packets = LinkedBlockingQueue<Packet<INetHandlerPlayServer>>()
-    private var fakePlayer: EntityOtherPlayerMP? = null
-    private var disableLogger = false
-    private val positions = LinkedList<DoubleArray>()
 
     override fun onEnable() {
         if (mc.thePlayer == null) return
@@ -56,33 +43,17 @@ class Blink : Module() {
             positions.add(doubleArrayOf(mc.thePlayer.posX, mc.thePlayer.entityBoundingBox.minY + mc.thePlayer.getEyeHeight() / 2, mc.thePlayer.posZ))
             positions.add(doubleArrayOf(mc.thePlayer.posX, mc.thePlayer.entityBoundingBox.minY, mc.thePlayer.posZ))
         }
-        pulseTimer.reset()
+        BlinkUtils.setBlink(pulse = pulseValue.get(), pulseDelay = pulseDelayValue.get(), playerPosition = outboundValue.get(), playerAll = outboundValue.get(), transactions = outboundValue.get(), teleport = inboundValue.get(), velocity = inboundValue.get())
+        BlinkUtils.setBlink(true)
     }
 
     override fun onDisable() {
         if (mc.thePlayer == null) return
-        blink()
+        BlinkUtils.setBlink(false)
         if (fakePlayer != null) {
             mc.theWorld.removeEntityFromWorld(fakePlayer!!.entityId)
             fakePlayer = null
         }
-    }
-
-    @EventTarget
-    fun onPacket(event: PacketEvent) {
-        val packet = event.packet
-        if (mc.thePlayer == null || disableLogger) return
-        if (packet is C03PacketPlayer) { // Cancel all movement stuff
-            event.cancelEvent()
-        }
-        if (packet is C04PacketPlayerPosition || packet is C06PacketPlayerPosLook ||
-            packet is C08PacketPlayerBlockPlacement ||
-            packet is C0APacketAnimation ||
-            packet is C0BPacketEntityAction || packet is C02PacketUseEntity) {
-            event.cancelEvent()
-            packets.add(packet as Packet<INetHandlerPlayServer>)
-        }
-        if (packet is S08PacketPlayerPosLook && inboundValue.get()) event.cancelEvent()
     }
 
     @EventTarget
@@ -95,10 +66,6 @@ class Blink : Module() {
                     mc.thePlayer.posZ
                 )
             )
-        }
-        if (pulseValue.get() && pulseTimer.hasTimePassed(pulseDelayValue.get().toLong())) {
-            blink()
-            pulseTimer.reset()
         }
     }
 
@@ -131,18 +98,6 @@ class Blink : Module() {
     }
 
     override val tag: String
-        get() = packets.size.toString()
-
-    private fun blink() {
-        try {
-            disableLogger = true
-            while (!packets.isEmpty()) {
-                mc.netHandler.addToSendQueue(packets.take())
-            }
-            disableLogger = false
-        } finally {
-            disableLogger = false
-        }
-        synchronized(positions) { positions.clear() }
-    }
+        get() = if (inboundValue.get()) "Inbound" else "" + if (inboundValue.get() && outboundValue.get()) " | " else "" + if (outboundValue.get()) "Outbound" else ""
+    
 }
