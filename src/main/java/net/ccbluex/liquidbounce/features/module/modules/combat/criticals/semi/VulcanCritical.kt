d4c7f5ab7baf6e8281cb507d27c192fc8e3a4f4c
K0itoyuu <76237220+K0itoyuu@users.noreply.github.com>
2023-03-02 13:40:16 +0800

    改进Vulcan Critical
    修复Jump A检测

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/criticals/semi/VulcanCritical.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/criticals/semi/VulcanCritical.kt
@@ -10,9 +10,10 @@ class VulcanCritical : CriticalMode("Vulcan") {
     }
     override fun onAttack(event: AttackEvent) {
         attacks++ //Vulcan updated and it fully bypassed, that's pogggg *bruh
-        if (attacks > 6) { //TODO: when you stopped moving it may flags sometimes (doesn't matter)
-            critical.sendCriticalPacket(yOffset = 0.2, ground = false)
-            critical.sendCriticalPacket(yOffset = 0.1216, ground = false)
+        if (attacks > 7) { //TODO: when you stopped moving it may flags sometimes (doesn't matter)
+            critical.sendCriticalPacket(yOffset = 0.16477328182606651, ground = false)
+            critical.sendCriticalPacket(yOffset = 0.08307781780646721, ground = false)
+            critical.sendCriticalPacket(yOffset = 0.0030162615090425808, ground = false)
             attacks = 0
         }
     }
