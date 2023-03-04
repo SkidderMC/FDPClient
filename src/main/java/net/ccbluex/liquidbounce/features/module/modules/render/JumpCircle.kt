0d68e62c1e5dde6bdf051b8a71255534860e03fc
MatrixAura <1446679699@qq.com>
2023-03-04 13:37:12 +0800

    Fix 2

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/JumpCircle.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/JumpCircle.kt
@@ -1,8 +1,12 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/SkidderMC/FDPClient/
+ */
 package net.ccbluex.liquidbounce.features.module.modules.render
 
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.event.JumpEvent
 import net.ccbluex.liquidbounce.event.Render3DEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
