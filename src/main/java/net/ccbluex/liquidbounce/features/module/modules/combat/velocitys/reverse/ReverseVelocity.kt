2c5543bfa92c42dde3889779beeb351f02e5d062
Dg636 <dg636gamez@gmail.com>
2023-03-10 20:13:53 -0500

    Update ReverseVelocity.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/reverse/ReverseVelocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/reverse/ReverseVelocity.kt
@@ -4,19 +4,22 @@ import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.VelocityMode
 import net.ccbluex.liquidbounce.features.value.FloatValue
+import net.ccbluex.liquidbounce.features.value.IntegerValue
 import net.ccbluex.liquidbounce.utils.MovementUtils
 
 class ReverseVelocity : VelocityMode("Reverse") {
     private val reverseStrengthValue = FloatValue("ReverseStrength", 1F, 0.1F, 1F)
+    private val reverseTimeValue = IntegerValue("ReverseTime", 80, 10, 500)
+    private val reverseDelayValue = IntegerValue("ReverseDelay", 30, 0, 500)
 
     override fun onVelocity(event: UpdateEvent) {
         if (!velocity.velocityInput) {
             return
         }
-
-        if (!mc.thePlayer.onGround) {
+        if (velocity.velocityTimer.hasTimePassed(reverseDelayValue.get().toLong()))
             MovementUtils.strafe(MovementUtils.getSpeed() * reverseStrengthValue.get())
-        } else if (velocity.velocityTimer.hasTimePassed(80L)) {
+        
+        } else if (velocity.velocityTimer.hasTimePassed(reverseTimeValue.get().toLong())) {
             velocity.velocityInput = false
         }
     }
