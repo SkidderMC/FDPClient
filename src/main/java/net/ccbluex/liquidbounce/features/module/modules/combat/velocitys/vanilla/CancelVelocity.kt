391c0d1f4723130380e06bb7613965c6c33e0533
Dg636 <dg636gamez@gmail.com>
2023-03-10 21:20:49 -0500

    sex u

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/vanilla/CancelVelocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/vanilla/CancelVelocity.kt
@@ -13,11 +13,12 @@ class CancelVelocity : VelocityMode("Cancel") {
     override fun onVelocityPacket(event: PacketEvent) {
         event.cancelEvent()
         val packet = event.packet
-        if (!packet is S12PacketEntityVelocity) return
-        if (!cancelVertical.get()) mc.thePlayer.motionY = packet.getMotionY().toDouble() / 8000.0
-        if (!cancelHorizontal.get()) {
-            mc.thePlayer.motionX = packet.getMotionX().toDouble() / 8000.0
-            mc.thePlayer.motionZ = packet.getMotionZ().toDouble() / 8000.0
+        if (packet is S12PacketEntityVelocity) {
+            if (!cancelVertical.get()) mc.thePlayer.motionY = packet.getMotionY().toDouble() / 8000.0
+            if (!cancelHorizontal.get()) {
+                mc.thePlayer.motionX = packet.getMotionX().toDouble() / 8000.0
+                mc.thePlayer.motionZ = packet.getMotionZ().toDouble() / 8000.0
+            }
         }
     }
 }
