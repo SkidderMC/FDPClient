e93b6ef59af51ce10315973964d2700ec350798e
Dg636 <dg636gamez@gmail.com>
2023-03-18 20:48:40 -0400

    Update HypSpoofNofall.kt
    Signed-off-by: Dg636 <dg636gamez@gmail.com>

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/nofalls/other/HypixelFlagNofall.kt
@@ -0,0 +1,32 @@
+package net.ccbluex.liquidbounce.features.module.modules.player.nofalls.other
+
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.modules.player.nofalls.NoFallMode
+import net.ccbluex.liquidbounce.utils.PacketUtils
+import net.ccbluex.liquidbounce.utils.misc.FallingPlayer
+import net.minecraft.network.play.client.C03PacketPlayer
+
+class HypixelFlagNofall : NoFallMode("HypixelFlag") {
+    
+    private var sendPacket = false
+    
+    override fun onUpdate(event: UpdateEvent) {
+        if (mc.thePlayer.onGround) {
+            sendPacket = false
+        }
+    }
+
+    override fun onPacket(event: PacketEvent) {
+        val fallingPlayer = FallingPlayer(mc.thePlayer)
+        val collLoc = fallingPlayer.findCollision(60) // null -> too far to calc or fall pos in void
+        if (abs((collLoc?.y ?: 0) - mc.thePlayer.posY) < 3 && mc.thePlayer.fallDistance > 4 && !sendPacket) {
+            event.cancelEvent()
+            mc.netHandler.addToSendQueue(C03PacketPlayer.C04PacketPlayerPosition(event.packet.x, event.packet.y, event.packet.z, false))
+            mc.netHandler.addToSendQueue(C03PacketPlayer.C04PacketPlayerPosition(event.packet.x, event.packet.y, event.packet.z - 23, true))
+            mc.netHandler.addToSendQueue(C03PacketPlayer.C04PacketPlayerPosition(event.packet.x, event.packet.y, event.packet.z, false))
+            
+            sendPacket = true
+        }
+    }
+}
