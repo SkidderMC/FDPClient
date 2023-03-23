f2c2d952ac699b8e646c5b9fdb549c6b5be2ee6f
Dg636 <dg636gamez@gmail.com>
2023-03-22 22:09:09 -0400

    Update AutoClicker.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/AutoClicker.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/AutoClicker.kt
@@ -70,159 +70,22 @@ class AutoClicker : Module() {
 
     @EventTarget
     fun onRender(event: Render3DEvent) {
-        if ((mc.gameSettings.keyBindAttack.isKeyDown && leftValue.get() && System.currentTimeMillis() - leftLastSwing >= leftDelay && (!leftSwordOnlyValue.get() || mc.thePlayer.heldItem?.item is ItemSword) && mc.playerController.curBlockDamageMP == 0F) 
-            || (mc.gameSettings.keyBindUseItem.isKeyDown && !mc.thePlayer.isUsingItem && rightValue.get() && System.currentTimeMillis() - rightLastSwing >= rightDelay && (!rightBlockOnlyValue.get() || mc.thePlayer.heldItem?.item is ItemBlock) && rightValue.get())
-        ) {
-           
-            
-            when (modeValue.get().lowercase()) {
-                "normal" -> {
-                    cDelay = TimeUtils.randomClickDelay(normalMinCPSValue.get(), normalMaxCPSValue.get()).toInt()
-                }
+        if (mc.gameSettings.keyBindAttack.isKeyDown && normalLeftValue.get() &&
+            System.currentTimeMillis() - normalLeftLastSwing >= normalLeftDelay && (!normalLeftSwordOnlyValue.get() || mc.thePlayer.heldItem?.item is ItemSword) && mc.playerController.curBlockDamageMP == 0F) {
+            KeyBinding.onTick(mc.gameSettings.keyBindAttack.keyCode) // Minecraft Click Handling
 
-                "gaussian" -> {
-                    gaussianUpdateDelay()
-                    cDelay = gaussianClickDelay.toInt()
-                }
-
-                "legitjitter" -> {
-                    when (legitJitterValue.get().lowercase()) {
-                        "jitter1" -> {
-                            if (1 == Random.nextInt(1, 5))
-                                delayNum = 0
-
-                            if (delayNum == 0) {
-                                if (Random.nextInt(1, 3) == 1) {
-                                    cDelay = Random.nextInt(98, 110)
-                                } else if (Random.nextInt(1, 2) == 1) {
-                                    cDelay = Random.nextInt(125, 138)
-                                } else {
-                                    cDelay = Random.nextInt(148, 153)
-                                }
-
-                                delayNum = 1
-                            } else {
-                                if (Random.nextInt(1, 4) !== 1) {
-                                    if (Random.nextBoolean()) {
-                                        cDelay = Random.nextInt(65, 69)
-                                    } else {
-                                        if (Random.nextInt(1, 5) == 1) {
-                                            cDelay = Random.nextInt(81, 87)
-                                        } else {
-                                            cDelay = Random.nextInt(97, 101)
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-                        "jitter2" -> {
-                            if (Random.nextInt(1, 14) <= 3) {
-                                if (Random.nextInt(1, 3) == 1) {
-                                    cDelay = Random.nextInt(98, 102)
-                                } else {
-                                    cDelay = Random.nextInt(114, 117)
-                                }
-                            } else {
-                                if (Random.nextInt(1, 4) == 1) {
-                                    cDelay = Random.nextInt(64, 69)
-                                } else {
-                                    cDelay = Random.nextInt(83, 85)
-                                }
-
-                            }
-                        }
-
-                        "jitter3" -> {
-                            if (Random.nextInt(1, 5) == 1 && delayNum == 0) {
-                                delayNum = 1
-                                if (Random.nextInt(1, 4) == 1) {
-                                    cDelay = Random.nextInt(114, 118)
-                                } else {
-                                    cDelay = Random.nextInt(98, 104)
-                                }
-                            } else {
-                                if (delayNum == 1) {
-                                    delayNum = 0
-                                    cDelay = Random.nextInt(65, 70)
-                                } else {
-                                    cDelay = Random.nextInt(84, 88)
-                                }
-                            }
-                        }
-
-                        "simplejitter" -> {
-                            if (Random.nextInt(1, 5) == 1) {
-                                if (Random.nextBoolean()) {
-                                    cDelay = Random.nextInt(105, 110)
-                                } else {
-                                    cDelay = Random.nextInt(120, 128)
-                                }
-                            } else {
-                                if (Random.nextInt(1, 3) == 1) {
-                                    cDelay = Random.nextInt(76, 79)
-                                } else {
-                                    if (Random.nextInt(1, 3) == 1) {
-                                        cDelay = 78
-                                    } else {
-                                        cDelay = 77
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                "legitbutterfly" -> {
-                    when (legitButterflyValue.get().lowercase()) {
-                        "butterfly1" -> {
-                            if (Random.nextInt(1, 7) == 1) {
-                                cDelay = Random.nextInt(80, 104)
-                            } else {
-                                if (Random.nextInt(1, 7) <= 2) {
-                                    cDelay = 117
-                                } else {
-                                    cDelay = Random.nextInt(114, 119)
-                                }
-                            }
-                        }
-
-                        "butterfly2" -> {
-                            if (Random.nextInt(1, 10) == 1) {
-                                cDelay = Random.nextInt(225, 250)
-                            } else {
-                                if (Random.nextInt(1, 6) == 1) {
-                                    cDelay = Random.nextInt(89, 94)
-                                } else if (Random.nextInt(1, 3) == 1) {
-                                    cDelay = Random.nextInt(95, 103)
-                                } else if (Random.nextInt(1, 3) == 1) {
-                                    cDelay = Random.nextInt(115, 123)
-                                } else {
-                                    if (Random.nextBoolean()) {
-                                        cDelay = Random.nextInt(131, 136)
-                                    } else {
-                                        cDelay = Random.nextInt(165, 174)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-            }
-            if (mc.gameSettings.keyBindUseItem.isKeyDown && !mc.thePlayer.isUsingItem && rightValue.get() && System.currentTimeMillis() - rightLastSwing >= rightDelay && (!rightBlockOnlyValue.get() || mc.thePlayer.heldItem?.item is ItemBlock) && rightValue.get()) {
-                KeyBinding.onTick(mc.gameSettings.keyBindUseItem.keyCode)
-                rightDelay = cDelay.toLong() - 1L
-                rightLastSwing = System.currentTimeMillis()
-            }
-            
-            
-            if (mc.gameSettings.keyBindAttack.isKeyDown && leftValue.get() && System.currentTimeMillis() - leftLastSwing >= leftDelay && (!leftSwordOnlyValue.get() || mc.thePlayer.heldItem?.item is ItemSword) && mc.playerController.curBlockDamageMP == 0F) {
-                KeyBinding.onTick(mc.gameSettings.keyBindAttack.keyCode)
-                leftDelay = cDelay.toLong()
-                leftLastSwing = System.currentTimeMillis()
-            }
+           leftLastSwing = System.currentTimeMillis()
+           leftDelay = updateClicks().toLong()
         }
+           
+           
+        if (mc.gameSettings.keyBindUseItem.isKeyDown && !mc.thePlayer.isUsingItem && rightValue.get() && System.currentTimeMillis() - rightLastSwing >= rightDelay && (!rightBlockOnlyValue.get() || mc.thePlayer.heldItem?.item is ItemBlock) && rightValue.get()) {
+            KeyBinding.onTick(mc.gameSettings.keyBindUseItem.keyCode)
+            
+            rightLastSwing = System.currentTimeMillis()
+            rightDelay = updateClicks.toLong() - 1L
+        }
+            
     }
 
 
@@ -253,5 +116,143 @@ class AutoClicker : Module() {
         gaussianClickDelay = 1000F / (MathUtils.calculateGaussianDistribution(gaussianCpsValue.get().toFloat(), gaussianSigmaValue.get()).toFloat()
             .coerceAtLeast(1F)) // 1000ms = 1s
         return gaussianClickDelay
+    }
+    
+    private fun updateClicks(): Int {
+        when (modeValue.get().lowercase()) {
+            "normal" -> {
+                cDelay = TimeUtils.randomClickDelay(normalMinCPSValue.get(), normalMaxCPSValue.get()).toInt()
+            }
+
+            "gaussian" -> {
+                gaussianUpdateDelay()
+                cDelay = gaussianClickDelay.toInt()
+            }
+
+            "legitjitter" -> {
+                when (legitJitterValue.get().lowercase()) {
+                    "jitter1" -> {
+                        if (1 == Random.nextInt(1, 5))
+                            delayNum = 0
+
+                        if (delayNum == 0) {
+                            if (Random.nextInt(1, 3) == 1) {
+                                cDelay = Random.nextInt(98, 110)
+                            } else if (Random.nextInt(1, 2) == 1) {
+                                cDelay = Random.nextInt(125, 138)
+                            } else {
+                                cDelay = Random.nextInt(148, 153)
+                            }
+
+                            delayNum = 1
+                        } else {
+                            if (Random.nextInt(1, 4) !== 1) {
+                                if (Random.nextBoolean()) {
+                                    cDelay = Random.nextInt(65, 69)
+                                } else {
+                                    if (Random.nextInt(1, 5) == 1) {
+                                        cDelay = Random.nextInt(81, 87)
+                                    } else {
+                                        cDelay = Random.nextInt(97, 101)
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    "jitter2" -> {
+                        if (Random.nextInt(1, 14) <= 3) {
+                            if (Random.nextInt(1, 3) == 1) {
+                                cDelay = Random.nextInt(98, 102)
+                            } else {
+                                cDelay = Random.nextInt(114, 117)
+                            }
+                        } else {
+                            if (Random.nextInt(1, 4) == 1) {
+                                cDelay = Random.nextInt(64, 69)
+                            } else {
+                                cDelay = Random.nextInt(83, 85)
+                            }
+
+                        }
+                    }
+
+                    "jitter3" -> {
+                        if (Random.nextInt(1, 5) == 1 && delayNum == 0) {
+                            delayNum = 1
+                            if (Random.nextInt(1, 4) == 1) {
+                                cDelay = Random.nextInt(114, 118)
+                            } else {
+                                cDelay = Random.nextInt(98, 104)
+                            }
+                        } else {
+                            if (delayNum == 1) {
+                                delayNum = 0
+                                cDelay = Random.nextInt(65, 70)
+                            } else {
+                                cDelay = Random.nextInt(84, 88)
+                            }
+                        }
+                    }
+
+                    "simplejitter" -> {
+                        if (Random.nextInt(1, 5) == 1) {
+                            if (Random.nextBoolean()) {
+                                cDelay = Random.nextInt(105, 110)
+                            } else {
+                                cDelay = Random.nextInt(120, 128)
+                            }
+                        } else {
+                            if (Random.nextInt(1, 3) == 1) {
+                                cDelay = Random.nextInt(76, 79)
+                            } else {
+                                if (Random.nextInt(1, 3) == 1) {
+                                    cDelay = 78
+                                } else {
+                                    cDelay = 77
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            "legitbutterfly" -> {
+                when (legitButterflyValue.get().lowercase()) {
+                    "butterfly1" -> {
+                        if (Random.nextInt(1, 7) == 1) {
+                            cDelay = Random.nextInt(80, 104)
+                        } else {
+                            if (Random.nextInt(1, 7) <= 2) {
+                                cDelay = 117
+                            } else {
+                                cDelay = Random.nextInt(114, 119)
+                            }
+                        }
+                    }
+
+                    "butterfly2" -> {
+                        if (Random.nextInt(1, 10) == 1) {
+                            cDelay = Random.nextInt(225, 250)
+                        } else {
+                            if (Random.nextInt(1, 6) == 1) {
+                                cDelay = Random.nextInt(89, 94)
+                            } else if (Random.nextInt(1, 3) == 1) {
+                                cDelay = Random.nextInt(95, 103)
+                            } else if (Random.nextInt(1, 3) == 1) {
+                                cDelay = Random.nextInt(115, 123)
+                            } else {
+                                if (Random.nextBoolean()) {
+                                    cDelay = Random.nextInt(131, 136)
+                                } else {
+                                    cDelay = Random.nextInt(165, 174)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return cDelay
     }
 }
