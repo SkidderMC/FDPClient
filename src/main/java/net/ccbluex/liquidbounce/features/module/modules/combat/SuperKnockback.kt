7a0e7edbf1531f40d7d97a15f4cfd58c03587f63
Dg636 <dg636gamez@gmail.com>
2023-03-23 22:23:49 -0400

    Update SuperKnockback.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/SuperKnockback.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/SuperKnockback.kt
@@ -13,6 +13,8 @@ import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.utils.Rotation
+import net.ccbluex.liquidbounce.utils.RotationUtils
 import net.ccbluex.liquidbounce.utils.timer.MSTimer
 import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.ccbluex.liquidbounce.features.value.IntegerValue
@@ -24,8 +26,9 @@ import net.minecraft.network.play.client.C03PacketPlayer.*
 @ModuleInfo(name = "SuperKnockback", category = ModuleCategory.COMBAT)
 class SuperKnockback : Module() {
     private val hurtTimeValue = IntegerValue("HurtTime", 10, 0, 10)
-    private val modeValue = ListValue("Mode", arrayOf("Legit", "DelayPacket", "Packet", "SneakPacket"), "DelayPacket")
-    private val onlyMoveValue = BoolValue("OnlyMove", false)
+    private val modeValue = ListValue("Mode", arrayOf("Legit", "Silent", "Packet", "SneakPacket"), "Silent")
+    private val onlyMoveValue = BoolValue("OnlyMove", true)
+    private val onlyMoveForwardValue = BoolValue("OnlyMoveForward", true). displayable { onlyMoveValue.get() }
     private val onlyGroundValue = BoolValue("OnlyGround", false)
     private val delayValue = IntegerValue("Delay", 0, 0, 500)
     
@@ -40,6 +43,11 @@ class SuperKnockback : Module() {
                 (!MovementUtils.isMoving() && onlyMoveValue.get()) || (!mc.thePlayer.onGround && onlyGroundValue.get())) {
                 return
             }
+            
+            if (onlyMoveForwardValue.get() && RotationUtils.getRotationDifference(Rotation(MovementUtils.movingYaw, mc.thePlayer.rotationPitch), Rotation(mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch)) > 35) {
+                return
+            }
+                
             when (modeValue.get().lowercase()) {
                 
                 "legit" -> {
