82451bc9f521ac92efb5a337ae9b37b59669e441
Dg636 <dg636gamez@gmail.com>
2023-06-03 19:27:24 -0400

    ALAN WOOD BYPASS INCC

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/LegitSpeed.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/LegitSpeed.kt
@@ -7,9 +7,14 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.other
 
 import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.features.value.BoolValue
 
 class LegitSpeed : SpeedMode("Legit") {
-    override fun onUpdate() {
+    
+    private val cpuSPEED = BoolValue("Legit-AlanWOOD-CPU-BYPASS-$$$$", true)
+    
+    override fun onStrafe() {
+        if (cpuSPEED.get()) mc.timer.timerSpeed = 1.004
         if (mc.thePlayer.isInWater) return
         if (MovementUtils.isMoving()) {
             if (mc.thePlayer.onGround) mc.thePlayer.jump()
