baf2765acc03552a206dca2da0da0497efe0005a
Dg636 <dg636gamez@gmail.com>
2023-06-06 18:12:09 -0400

    Sus packet

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/LegitReach.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/LegitReach.kt
@@ -9,27 +9,34 @@ import net.ccbluex.liquidbounce.FDPClient
 import net.ccbluex.liquidbounce.event.AttackEvent
 import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.utils.EntityUtils
+import net.ccbluex.liquidbounce.utils.PacketUtils
 import net.ccbluex.liquidbounce.utils.timer.MSTimer
 import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.ccbluex.liquidbounce.features.value.IntegerValue
 import net.minecraft.client.entity.EntityOtherPlayerMP
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.network.play.client.C02PacketUseEntity
+import net.minecraft.network.play.server.*
+import net.minecraft.network.play.INetHandlerPlayClient
 import net.minecraft.world.WorldSettings
 
 object LegitReach : Module(name = "LegitReach", category = ModuleCategory.COMBAT) {
 
     var fakePlayer: EntityOtherPlayerMP? = null
     private val aura = BoolValue("Aura", false)
+    private val mode = ListValue("Mode", arrayOf("FakePlayer", "IntaveTest", "AllIncomingPackets", "TargetPackets"), "FakePlayer")
     private val pulseDelayValue = IntegerValue("PulseDelay", 200, 50, 500)
-    private val Intavetest = BoolValue("IntaveTest", false)
-    private val intavetesthurttime = IntegerValue("Packets", 5, 0, 30).displayable { Intavetest.get() }
+    private val intavetesthurttime = IntegerValue("Packets", 5, 0, 30).displayable { mode.equals("IntaveTest") }
+    
     private val pulseTimer = MSTimer()
     var currentTarget: EntityLivingBase? = null
     private var shown = false
+    
+    private val packets = LinkedList<Packet<INetHandlerPlayClient>>()
 
     val killaura = FDPClient.moduleManager.getModule(KillAura::class.java) as KillAura
 
@@ -39,11 +46,15 @@ object LegitReach : Module(name = "LegitReach", category = ModuleCategory.COMBAT
     }
 
     private fun removeFakePlayer() {
-        try {
-            currentTarget = null
-            (mc.theWorld ?: return).removeEntityFromWorld((fakePlayer ?: return).entityId) ?: return
-            fakePlayer = null
-        } catch (t: NullPointerException) {
+        if (fakePlayer == null) return
+        currentTarget = null
+        mc.theWorld.removeEntity(fakePlayer)
+        fakePlayer = null
+    }
+    
+    private fun clearPackets() {
+        while (!packets.isEmpty()) {
+            PacketUtils.handlePacket(packets.take())
         }
     }
 
@@ -59,84 +70,114 @@ object LegitReach : Module(name = "LegitReach", category = ModuleCategory.COMBAT
 
     @EventTarget
     fun onAttack(event: AttackEvent) {
-        if (fakePlayer == null) {
-            currentTarget = event.targetEntity as EntityLivingBase?
-            shown = false
-        } else {
-            if (event.targetEntity == fakePlayer) {
-                attackEntity(currentTarget ?: return)
-            } else {
-                fakePlayer = null
+        if ( mode.equals("FakePlayer") || mode.equals("IntaveTest") ) {
+            if (fakePlayer == null) {
                 currentTarget = event.targetEntity as EntityLivingBase?
                 shown = false
+            } else {
+                if (event.targetEntity == fakePlayer) {
+                    attackEntity(currentTarget ?: return)
+                    event.cancelEvent()
+                } else {
+                    fakePlayer = null
+                    currentTarget = event.targetEntity as EntityLivingBase?
+                    shown = false
+                }
+            }
+        } else {
+            if (event.targetEntity != currentTarget) {
+                clearPackets()
+                currentTarget = event.targetEntity as EntityLivingBase?
             }
         }
     }
 
     @EventTarget
     fun onUpdate(@Suppress("UNUSED_PARAMETER") event: UpdateEvent?) {
-        if (aura.get() && !killaura.state) {
-            currentTarget = null
-            (mc.theWorld ?: return).removeEntityFromWorld((fakePlayer ?: return).entityId) ?: return
-            fakePlayer = null
-        }
-        if (mc.thePlayer == null)
-            return
-        if (fakePlayer != null && EntityUtils.isRendered(fakePlayer ?: return) && ((currentTarget ?: return).isDead || !EntityUtils.isRendered(
-                        currentTarget ?: return
-                ))
-        ) {
-            removeFakePlayer()
-        }
-        if (currentTarget != null && fakePlayer != null) {
-            (fakePlayer ?: return).health = (currentTarget ?: return).health
-            val indices = (0..4).toList().toIntArray()
-            for (index in indices) {
-                val equipmentInSlot = (currentTarget ?: return).getEquipmentInSlot(index) ?: continue
-                (fakePlayer ?: return).setCurrentItemOrArmor(index, equipmentInSlot)
+        if ( mode.equals("FakePlayer") || mode.equals("IntaveTest") ) {
+            if (aura.get() && !killaura.state) {
+                removeFakePlayer()
             }
-        }
-        if (Intavetest.get() && mc.thePlayer.ticksExisted % intavetesthurttime.get() == 0) {
-            if (fakePlayer != null) {
-                (fakePlayer ?: return).rotationYawHead = (currentTarget ?: return).rotationYawHead
-                (fakePlayer ?: return).renderYawOffset = (currentTarget ?: return).renderYawOffset
-                (fakePlayer ?: return).copyLocationAndAnglesFrom(currentTarget ?: return)
-                (fakePlayer ?: return).rotationYawHead = (currentTarget ?: return).rotationYawHead
+            if (mc.thePlayer == null) return
+            if (fakePlayer != null && EntityUtils.isRendered(fakePlayer ?: return) && ((currentTarget ?: return).isDead || !EntityUtils.isRendered(
+                            currentTarget ?: return
+                    ))
+            ) {
+                removeFakePlayer()
             }
-            pulseTimer.reset()
-        }else   if (!Intavetest.get() && pulseTimer.hasTimePassed(pulseDelayValue.get().toLong())) {
-            if (fakePlayer != null) {
-                (fakePlayer ?: return).rotationYawHead = (currentTarget ?: return).rotationYawHead
-                (fakePlayer ?: return).renderYawOffset = (currentTarget ?: return).renderYawOffset
-                (fakePlayer ?: return).copyLocationAndAnglesFrom(currentTarget ?: return)
-                (fakePlayer ?: return).rotationYawHead = (currentTarget ?: return).rotationYawHead
+            if (currentTarget != null && fakePlayer != null) {
+                (fakePlayer ?: return).health = (currentTarget ?: return).health
+                val indices = (0..4).toList().toIntArray()
+                for (index in indices) {
+                    val equipmentInSlot = (currentTarget ?: return).getEquipmentInSlot(index) ?: continue
+                    (fakePlayer ?: return).setCurrentItemOrArmor(index, equipmentInSlot)
+                }
             }
-            pulseTimer.reset()
-        }
+            if (Intavetest.get() && mc.thePlayer.ticksExisted % intavetesthurttime.get() == 0) {
+                if (fakePlayer != null) {
+                    (fakePlayer ?: return).rotationYawHead = (currentTarget ?: return).rotationYawHead
+                    (fakePlayer ?: return).renderYawOffset = (currentTarget ?: return).renderYawOffset
+                    (fakePlayer ?: return).copyLocationAndAnglesFrom(currentTarget ?: return)
+                    (fakePlayer ?: return).rotationYawHead = (currentTarget ?: return).rotationYawHead
+                }
+                pulseTimer.reset()
+            }else   if (!Intavetest.get() && pulseTimer.hasTimePassed(pulseDelayValue.get().toLong())) {
+                if (fakePlayer != null) {
+                    (fakePlayer ?: return).rotationYawHead = (currentTarget ?: return).rotationYawHead
+                    (fakePlayer ?: return).renderYawOffset = (currentTarget ?: return).renderYawOffset
+                    (fakePlayer ?: return).copyLocationAndAnglesFrom(currentTarget ?: return)
+                    (fakePlayer ?: return).rotationYawHead = (currentTarget ?: return).rotationYawHead
+                }
+                pulseTimer.reset()
+            }
 
-        if (!shown && currentTarget != null && (currentTarget ?: return).uniqueID != null && mc.netHandler.getPlayerInfo(
-                        (currentTarget ?: return).uniqueID ?: return
-                ) != null && mc.netHandler.getPlayerInfo((currentTarget ?: return).uniqueID ?: return).gameProfile != null
-        ) {
-            val faker = EntityOtherPlayerMP(
-                    mc.theWorld ?: return,
-                    mc.netHandler.getPlayerInfo((currentTarget ?: return).uniqueID ?: return).gameProfile ?: return
-            )
+            if (!shown && currentTarget != null && (currentTarget ?: return).uniqueID != null && mc.netHandler.getPlayerInfo(
+                            (currentTarget ?: return).uniqueID ?: return
+                    ) != null && mc.netHandler.getPlayerInfo((currentTarget ?: return).uniqueID ?: return).gameProfile != null
+            ) {
+                val faker = EntityOtherPlayerMP(
+                        mc.theWorld ?: return,
+                        mc.netHandler.getPlayerInfo((currentTarget ?: return).uniqueID ?: return).gameProfile ?: return
+                )
 
-            faker.rotationYawHead = (currentTarget ?: return).rotationYawHead
-            faker.renderYawOffset = (currentTarget ?: return).renderYawOffset
-            faker.copyLocationAndAnglesFrom(currentTarget ?: return)
-            faker.rotationYawHead = (currentTarget ?: return).rotationYawHead
-            faker.health = (currentTarget ?: return).health
-            val indices = (0..4).toList().toIntArray()
-            for (index in indices) {
-                val equipmentInSlot = (currentTarget ?: return).getEquipmentInSlot(index) ?: continue
-                faker.setCurrentItemOrArmor(index, equipmentInSlot)
+                faker.rotationYawHead = (currentTarget ?: return).rotationYawHead
+                faker.renderYawOffset = (currentTarget ?: return).renderYawOffset
+                faker.copyLocationAndAnglesFrom(currentTarget ?: return)
+                faker.rotationYawHead = (currentTarget ?: return).rotationYawHead
+                faker.health = (currentTarget ?: return).health
+                val indices = (0..4).toList().toIntArray()
+                for (index in indices) {
+                    val equipmentInSlot = (currentTarget ?: return).getEquipmentInSlot(index) ?: continue
+                    faker.setCurrentItemOrArmor(index, equipmentInSlot)
+                }
+                (mc.theWorld ?: return).addEntityToWorld(-1337, faker)
+
+                fakePlayer = faker
+                shown = true
             }
-            (mc.theWorld ?: return).addEntityToWorld(-1337, faker)
-
-            fakePlayer = faker
-            shown = true
+        } else {
+            if (pulseTimer.hasTimePassed(pulseDelayValue.get().toLong())) {
+                pulseTimer.reset()
+                clearPackets()
+            }
+        }
+    }
+    
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
+        val packet = event.packet
+        
+        if (mode.equals("TargetPackets")) {
+            if (packet is S12PacketEntityVelocity) {
+                event.cancelEvent()
+                packets.add(packet as Packet<INetHandlerPlayClient>)
+            }
+        } else if (mode.equals("AllIncomingPackets")) {
+            if (packet is Packet<INetHandlerPlayClient?>) {
+                event.cancelEvent()
+                packets.add(packet as Packet<INetHandlerPlayClient>)
+            }
         }
     }
 }
+
