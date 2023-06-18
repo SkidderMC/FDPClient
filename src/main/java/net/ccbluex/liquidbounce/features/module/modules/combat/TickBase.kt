f0e5f846269d2a3ef8670f25a37fefe0e122c2b0
Dg636 <dg636gamez@gmail.com>
2023-06-18 17:49:27 -0400

    Update TickBase.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/TickBase.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/TickBase.kt
@@ -1,5 +1,5 @@
 /*
- * Liquidlite Ghost Client
+ * FDP CLIENT 
  */
 package net.ccbluex.liquidbounce.features.module.modules.combat
 
@@ -8,19 +8,22 @@ import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.value.IntegerValue
 import net.ccbluex.liquidbounce.features.value.FloatValue
 import net.minecraft.entity.EntityLivingBase
 
 object TickBase : Module(name = "TickBase", category = ModuleCategory.COMBAT) {
 
     private var ticks = 0
+
+    val ticksAmount = IntegerValue("BoostTicks", 10, 1, 20)
     val BoostAmount = FloatValue("BoostTimer", 10f, 1f, 50f)
     val ChargeAmount = FloatValue("ChargeTimer", 0.11f, 0.05f, 1f)
 
     @EventTarget
     fun onAttack(event: AttackEvent) {
         if (event.targetEntity is EntityLivingBase && ticks == 0) {
-            ticks = 10
+            ticks = ticksAmount.get()
         }
     }
 
@@ -35,7 +38,7 @@ object TickBase : Module(name = "TickBase", category = ModuleCategory.COMBAT) {
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
-        if (ticks == 10) {
+        if (ticks == ticksAmount.get()) {
             mc.timer.timerSpeed = ChargeAmount.get()
             ticks --
         } else if (ticks > 1) {
