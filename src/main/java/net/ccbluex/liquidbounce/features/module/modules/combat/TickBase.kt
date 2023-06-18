1fc61901e41dd6651d2da3d1d875d29de81e314c
David <101277472+Davidisnotavailable@users.noreply.github.com>
2023-06-18 23:21:40 +0200

    Added boostticks (how long it should boost)

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/TickBase.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/TickBase.kt
@@ -9,18 +9,20 @@ import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.value.FloatValue
+import net.ccbluex.liquidbounce.features.value.IntegerValue
 import net.minecraft.entity.EntityLivingBase
 
 object TickBase : Module(name = "TickBase", category = ModuleCategory.COMBAT) {
 
     private var ticks = 0
+    val BoostTicks = IntegerValue("BoostTicks", 10, 3, 20)
     val BoostAmount = FloatValue("BoostTimer", 20f, 1f, 50f)
     val ChargeAmount = FloatValue("ChargeTimer", 0.3f, 0.05f, 1f)
 
     @EventTarget
     fun onAttack(event: AttackEvent) {
         if (event.targetEntity is EntityLivingBase && ticks == 0) {
-            ticks = 10
+            ticks = BoostTicks.get()
         }
     }
 
@@ -35,7 +37,7 @@ object TickBase : Module(name = "TickBase", category = ModuleCategory.COMBAT) {
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
-        if (ticks == 10) {
+        if (ticks == BoostTicks.get()) {
             mc.timer.timerSpeed = ChargeAmount.get()
             ticks --
         } else if (ticks > 1) {
