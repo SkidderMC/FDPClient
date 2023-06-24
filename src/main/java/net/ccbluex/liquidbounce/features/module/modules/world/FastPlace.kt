c745bf1003ede20bfab47d9652b910c732ceca94
shxp3 <shapebruhbruh@gmail.com>
2023-06-24 15:46:36 +0700

    when ghost category

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/FastPlace.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/FastPlace.kt
@@ -5,10 +5,21 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.world
 
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.ccbluex.liquidbounce.features.value.IntegerValue
+import net.minecraft.item.ItemBlock
 
 object FastPlace : Module(name = "FastPlace", category = ModuleCategory.WORLD, defaultOn = false) {
     val speedValue = IntegerValue("Speed", 0, 0, 4)
+    val blockonlyValue = BoolValue("BlocksOnly", false)
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+        if (!blockonlyValue.get() || mc.thePlayer.heldItem.item is ItemBlock) {
+            mc.rightClickDelayTimer = speedValue.get()
+        }
+    }
 }
