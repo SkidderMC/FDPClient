ce516ed66a2d5f95ac172ab0cc068bfbc3d54895
shxp3 <shapebruhbruh@gmail.com>
2023-06-24 15:53:48 +0700

    automatic place

--- src/main/java/net/ccbluex/liquidbounce/utils/MouseUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/MouseUtils.kt
@@ -5,7 +5,25 @@
  */
 package net.ccbluex.liquidbounce.utils
 
+import net.minecraftforge.client.event.MouseEvent
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper
+import org.lwjgl.input.Mouse
+import java.nio.ByteBuffer
+
 object MouseUtils {
     @JvmStatic
     fun mouseWithinBounds(mouseX: Int, mouseY: Int, x: Float, y: Float, x2: Float, y2: Float) = mouseX >= x && mouseX < x2 && mouseY >= y && mouseY < y2
+
+    fun setMouseButtonState(mouseButton: Int, held: Boolean) {
+        val m = MouseEvent()
+        ObfuscationReflectionHelper.setPrivateValue(MouseEvent::class.java, m, mouseButton, "button")
+        ObfuscationReflectionHelper.setPrivateValue(MouseEvent::class.java, m, held, "buttonstate")
+        MinecraftForge.EVENT_BUS.post(m)
+        val buttons = ObfuscationReflectionHelper.getPrivateValue<ByteBuffer, Mouse?>(
+            Mouse::class.java, null, "buttons"
+        )
+        buttons.put(mouseButton, (if (held) 1 else 0).toByte())
+        ObfuscationReflectionHelper.setPrivateValue<Mouse?, ByteBuffer>(Mouse::class.java, null, buttons, "buttons")
+    }
 }
