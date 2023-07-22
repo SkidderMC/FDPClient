3550b843209009398fded6469430ed8207aaa68c
NCPDisabler <86876065+NCPUser@users.noreply.github.com>
2023-07-23 00:34:36 +0300

    1
    1

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/other/GrimDamage.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/other/GrimDamage.kt
@@ -1,13 +1,11 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement.flys.other
 
-import net.ccbluex.liquidbounce.FDPClient
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
-import net.ccbluex.liquidbounce.ui.client.hud.element.elements.Notification
-import net.ccbluex.liquidbounce.ui.client.hud.element.elements.NotifyType
 
 class GrimDamage : FlyMode("GrimDamage") {
-    override fun onEnable() {
-        if(mc.isSingleplayer) {
+    override fun onUpdate(event: UpdateEvent) {
             FDPClient.hud.addNotification(Notification("GrimDamage", "Damage GrimAC Fly (TNT ONLY Is A Test!)", NotifyType.ERROR))
             fly.state = false
         }
