92999a5f098dfe1e28c6b8ab953d21af85959778
RtxOP <137506596+RtxOP@users.noreply.github.com>
2023-07-23 12:21:55 -0300

    save

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/verus/VerusFloatSpeed.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/verus/VerusFloatSpeed.kt
@@ -7,30 +7,47 @@ class VerusFloatSpeed : SpeedMode("VerusFloat") {
     private var ticks = 0
     private var verusBypass = false
     private var isFloating = false
+    private var lastStopped = true
     
     override fun onEnable() {
         verusBypass = false
+        lastStopped = false
     }
 
     override fun onPreMotion() {
         ticks++
+
+        if (!(mc.thePlayer.moveForward > 0)){
+            lastStopped = true
+            return
+        }
+        
         if (!mc.gameSettings.keyBindJump.isKeyDown) {
             if (mc.thePlayer.onGround) {
+                
+                if (MovementUtils.speed() > 0.3){
+                    lastStopped = false
+                }
+
                 ticks = 0
-                MovementUtils.strafe(0.44f)
+                MovementUtils.strafe(0.41f)
                 mc.thePlayer.motionY = 0.42
                 mc.timer.timerSpeed = 2.1f
                 isFloating = true
             } else if (isFloating) {
                 if (ticks >= 10) {
                     verusBypass = true
-                    MovementUtils.strafe(0.2865f)
+                    MovementUtils.strafe(0.35f)
                     isFloating = false
                 }
 
                 if (verusBypass) {
+                    if (lastStopped){
+                        MovementUtils.strafe(0.2f)
+                    }
+
                     if (ticks <= 1) {
-                        MovementUtils.strafe(0.45f)
+                        MovementUtils.strafe(0.35f)
                     }
 
                     if (ticks >= 2) {
