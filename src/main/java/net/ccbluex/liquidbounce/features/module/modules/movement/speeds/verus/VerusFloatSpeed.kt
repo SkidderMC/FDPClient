b1e10caf7355d84fc640b46940532619da981f79
RtxOP <137506596+RtxOP@users.noreply.github.com>
2023-07-25 12:34:04 -0300

    kek

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/verus/VerusFloatSpeed.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/verus/VerusFloatSpeed.kt
@@ -6,7 +6,6 @@ class VerusFloatSpeed : SpeedMode("VerusFloat") {
 
     private var ticks = 0
     private var verusBypass = false
-    private var isFloating = false
     private var lastStopped = true
     
     override fun onEnable() {
@@ -15,52 +14,55 @@ class VerusFloatSpeed : SpeedMode("VerusFloat") {
     }
 
     override fun onPreMotion() {
-        ticks++
+        
+        if (mc.gameSettings.keyBindJump.isKeyDown) {
+            return
+        }
 
         if (!(mc.thePlayer.moveForward > 0)){
             lastStopped = true
             return;
         }
         
-        if (!mc.gameSettings.keyBindJump.isKeyDown) {
-            if (mc.thePlayer.onGround) {
+        if (mc.thePlayer.onGround) {
+            
+            if (MovementUtils.getSpeed() > 0.3){
+                lastStopped = false
                 
-                if (MovementUtils.getSpeed() > 0.3){
-                    lastStopped = false
-                    
-                }
-
-                ticks = 0
-                MovementUtils.strafe(0.41f)
-                mc.thePlayer.motionY = 0.42
-                mc.timer.timerSpeed = 2.1f
-                isFloating = true
-            } else{
-                if (ticks >= 10) {
-                    verusBypass = true
-                    MovementUtils.strafe(0.35f)
-                    return
-                }
-
-                if (verusBypass) {
-                    if (lastStopped){
-                        MovementUtils.strafe(0.2f)
-                    }
-
-                    if (ticks <= 1) {
-                        MovementUtils.strafe(0.35f)
-                    }
-
-                    if (ticks >= 2) {
-                        MovementUtils.strafe(0.69f - (ticks - 2) * 0.019f)
-                    }
-                }
-
-                mc.thePlayer.motionY = 0.0
-                mc.timer.timerSpeed = 0.9f
-
-                mc.thePlayer.onGround = true
             }
+
+            ticks = 0
+            MovementUtils.strafe(0.41f)
+            mc.thePlayer.motionY = 0.42
+            mc.timer.timerSpeed = 2.1f
+            
+        } else{
+            if (ticks >= 10) {
+                verusBypass = true
+                MovementUtils.strafe(0.35f)
+                return
+            }
+
+            if (verusBypass) {
+                if (lastStopped){
+                    MovementUtils.strafe(0.2f)
+                }
+
+                if (ticks <= 1) {
+                    MovementUtils.strafe(0.35f)
+                }
+
+                if (ticks >= 2) {
+                    MovementUtils.strafe(0.69f - (ticks - 2) * 0.019f)
+                }
+            }
+
+            mc.thePlayer.motionY = 0.0
+            mc.timer.timerSpeed = 0.9f
+
+            mc.thePlayer.onGround = true
         }
+        
     }
+    ticks++
 } 
