60eb51a0e857ecd6dbb7227761b31f5bb4566b2b
RtxOP <137506596+RtxOP@users.noreply.github.com>
2023-07-26 08:47:07 -0300

    AWOGA

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/other/GrimDamage.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flys/other/GrimDamage.kt
@@ -6,7 +6,7 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
 import net.ccbluex.liquidbounce.ui.client.hud.element.elements.Notification
 import net.ccbluex.liquidbounce.ui.client.hud.element.elements.NotifyType
 import net.minecraft.network.play.client.C03PacketPlayer
-import net.minecraft.block.tnt
+// import net.minecraft.block.tnt
 import net.minecraft.network.Packet
 
 
