1e7ba4b4db2998f57a52af71bab85d9706dfbc58
RtxOP <137506596+RtxOP@users.noreply.github.com>
2023-07-26 14:38:03 -0300

    Added Murder Mystery if anyone still playes that

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/MurderDetector.kt
@@ -0,0 +1,32 @@
+package net.ccbluex.liquidbounce.features.module.modules.misc
+
+import net.ccbluex.liquidbounce.FDPClient
+import net.ccbluex.liquidbounce.utils.ClientUtils
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.minecraft.entity.player.EntityPlayer
+
+@ModuleInfo(name="MurderDetector", category = ModuleCategory.MISC)
+class MurderDetector() : Module(){
+    
+    private EntityPlayer murderer;
+
+    override fun onPreMotion() {
+    
+        if (mc.thePlayer.ticksExisted % 2 == 0 || this.murderer != null) {
+                return;
+            }
+
+        for (player in mc.theWorld.playerEntities){
+            if (player.getHeldItem() != null){
+                if (player.getHeldItem().getDisplayName().contains("Knife")) {
+                    ClientUtils.displayChatMessage(player.getCommandSenderName() + " is The Murderer.");
+                    this.murderer = player;
+                }
+            }
+        }
+
+    }
+}
+
