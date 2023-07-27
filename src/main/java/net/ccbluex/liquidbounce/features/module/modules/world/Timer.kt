f0045214a2b7eb510178b31061f226fba31e32b9
RtxOP <137506596+RtxOP@users.noreply.github.com>
2023-07-27 08:41:49 -0300

    fix v2

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Timer.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/Timer.kt
@@ -14,13 +14,14 @@ import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.ccbluex.liquidbounce.features.value.FloatValue
+import net.ccbluex.liquidbounce.utils.MathUtils
 
 @ModuleInfo(name = "Timer", category = ModuleCategory.WORLD, autoDisable = EnumAutoDisableType.RESPAWN)
 object Timer : Module() {
 
     // private val minSpeedValue = FloatValue("Speed", 2F, 0.1F, 10F)
     private val maxSpeedValue: FloatValue = object : FloatValue("Max-Timer", 2F, 0.1F, 10F) {
-        override fun onChanged(oldValue: Int, newValue: Int) {
+        fun onChanged(oldValue: Int, newValue: Int) {
             val minTimer = minSpeedValue.get()
             if (minTimer > newValue) {
                 set(minTimer)
@@ -28,7 +29,7 @@ object Timer : Module() {
         }
     }
     private val minSpeedValue: FloatValue = object : FloatValue("Min-Timer", 2F, 0.1F, 10F)  {
-        override fun onChanged(oldValue: Int, newValue: Int) {
+        fun onChanged(oldValue: Int, newValue: Int) {
             val maxTimer = maxSpeedValue.get()
             if (maxTimer < newValue) {
                 set(maxTimer)
