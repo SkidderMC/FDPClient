c37db82818dd1abaaffac917ab12c47ce23fdc04
NCPDisabler <86876065+NCPUser@users.noreply.github.com>
2023-07-28 14:02:05 +0300

    Create MatrixLowHopSpeed.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/matrix/MatrixLowHopSpeed.kt
@@ -0,0 +1,20 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.matrix
+
+import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
+import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.minecraft.client.settings.GameSettings
+import net.ccbluex.liquidbounce.event.MoveEvent
+
+class MatrixLowHopSpeed : SpeedMode("MatrixLowHop") {
+
+    override fun onMotion() {
+
+    }
+    override fun onUpdate() {
+        if (mc.thePlayer.onGround && MovementUtils.isMoving()) {
+            mc.thePlayer.jump()
+            mc.thePlayer.motionY -= 0.116 * 0.03;
+        }
+    }
+    override fun onMove(event: MoveEvent?) {}
+}
