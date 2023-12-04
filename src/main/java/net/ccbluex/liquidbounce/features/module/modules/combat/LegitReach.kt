2d27fc9d2680c54180475e1c42a2cbb3b7748492
Dg636 <dg636gamez@gmail.com>
2023-12-04 17:37:25 -0500

    Update LegitReach.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/LegitReach.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/LegitReach.kt
@@ -1,4 +1,4 @@
-e/*
+/*
  * FDPClient Hacked Client
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
  * https://github.com/SkidderMC/FDPClient/
