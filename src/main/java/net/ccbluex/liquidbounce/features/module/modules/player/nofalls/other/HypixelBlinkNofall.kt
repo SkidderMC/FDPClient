899c229658ac6a5e20be4787de1b4472a4865780
Dg636 <dg636gamez@gmail.com>
2023-12-04 22:35:42 -0500

    Update and rename HypSpoofNofall.kt to HypixelBlinkNofall.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/nofalls/other/HypixelBlinkNofall.kt
@@ -0,0 +1,38 @@
+package net.ccbluex.liquidbounce.features.module.modules.player.nofalls.other
+
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.features.module.modules.player.nofalls.NoFallMode
+import net.ccbluex.liquidbounce.utils.PacketUtils
+import net.minecraft.network.play.client.C03PacketPlayer
+import net.ccbluex.liquidbounce.utils.BlinkUtils
+
+class HypixelBlinkNofall : NoFallMode("HypixelBlink") {
+    val blink = LiquidBounce.moduleManager[Blink::class.java]!!
+
+    private var enabled = false
+
+    override fun onEnable() {
+        enabled = false
+    }
+    
+    override fun onPacket(event: PacketEvent) {
+        if (mc.thePlayer.fallDistance > 1.5) {
+            if (!enabled) {
+                BlinkUtils.setBlinkState(all = true)
+                enabed = true
+            }
+        } else {
+            enbaed = false
+        }
+        if(enabled && event.packet is C03PacketPlayer) {
+            event.packet.onGround = true
+        }
+
+        if (mc.thePlayer.onGround) {
+            if (enabled) {
+                BlinkUtils.setBlinkState(off = true, release = true)
+                enabled = false
+            }
+        }
+    }
+}
