98f9c4dc5bb61301279a6dafb730fa920d9e54b5
Zywl <86253343+opZywl@users.noreply.github.com>
2023-12-25 21:16:33 -0300

    Update ConfigCommand.kt

--- src/main/java/net/ccbluex/liquidbounce/features/command/commands/ConfigCommand.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/command/commands/ConfigCommand.kt
@@ -92,7 +92,7 @@ class ConfigCommand : Command("config", arrayOf("cfg")) {
                 }
 
                 "save" -> {
-                    FDPClient.configManager.save(true)
+                    FDPClient.configManager.save(true, true)
                     alert("Saved config ${FDPClient.configManager.nowConfig}")
                 }
 
