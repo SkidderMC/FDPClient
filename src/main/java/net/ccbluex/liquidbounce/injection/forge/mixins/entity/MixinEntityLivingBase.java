94d5cae0e6f46197108312e87531152b2d0b95e7
Zywl <86253343+opZywl@users.noreply.github.com>
2024-01-06 10:16:00 -0300

    Update MixinEntityLivingBase.java

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/entity/MixinEntityLivingBase.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/entity/MixinEntityLivingBase.java
@@ -8,11 +8,8 @@ package net.ccbluex.liquidbounce.injection.forge.mixins.entity;
 import net.ccbluex.liquidbounce.FDPClient;
 import net.ccbluex.liquidbounce.event.JumpEvent;
 import net.ccbluex.liquidbounce.features.module.modules.client.Animations;
+import net.ccbluex.liquidbounce.features.module.modules.movement.*;
 import net.ccbluex.liquidbounce.features.module.modules.visual.VanillaTweaks;
-import net.ccbluex.liquidbounce.features.module.modules.movement.Jesus;
-import net.ccbluex.liquidbounce.features.module.modules.movement.NoJumpDelay;
-import net.ccbluex.liquidbounce.features.module.modules.movement.Sprint;
-import net.ccbluex.liquidbounce.features.module.modules.movement.Strafe;
 import net.ccbluex.liquidbounce.handler.protocol.ProtocolBase;
 import net.ccbluex.liquidbounce.utils.MinecraftInstance;
 import net.ccbluex.liquidbounce.utils.MovementUtils;
@@ -97,7 +94,7 @@ public abstract class MixinEntityLivingBase extends MixinEntity {
 
         this.motionY = jumpEvent.getMotion();
         final Sprint sprint = FDPClient.moduleManager.getModule(Sprint.class);
-        final Strafe strafeFix = FDPClient.moduleManager.getModule(Strafe.class);
+        final StrafeFix strafeFix = FDPClient.moduleManager.getModule(StrafeFix.class);
 
         if (this.isSprinting()) {
             float fixedYaw = this.rotationYaw;
