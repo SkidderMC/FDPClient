675556fed408d59fad09ca6a81face396e64128e
Zywl <86253343+opZywl@users.noreply.github.com>
2024-01-16 10:43:27 -0300

    New button by breadcat

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/ClientSpoof.java
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/ClientSpoof.java
@@ -18,7 +18,7 @@ public class ClientSpoof extends Module {
 
     public final ListValue modeValue = new ListValue("Payloads", new String[]{"Vanilla", "Fabric", "Lunar", "LabyMod", "Custom", "CheatBreaker", "PvPLounge"}, "Lunar");
     public final TextValue CustomClient = new TextValue("CustomClientSpoof", "CustomClient");
-    public final ListValue buttonValue = new ListValue("Button", new String[]{"Better", "RGBRounded", "Wolfram", "Rounded", "Hyperium", "RGB", "Badlion", "Flat", "FLine", "Rise", "Vanilla"}, "FLine");
+    public final ListValue buttonValue = new ListValue("Button", new String[]{"Better", "RGBRounded", "Wolfram", "Rounded", "Hyperium", "RGB", "Badlion", "PVP", "Flat", "FLine", "Rise", "Vanilla"}, "FLine");
     @Override
     public String getTag() {
         return modeValue.get();
@@ -44,6 +44,8 @@ public class ClientSpoof extends Module {
                 return new RGBRoundedButtonRenderer(button);
             case "wolfram":
                 return new WolframButtonRenderer(button);
+            case "pvp":
+                return new PvPClientButtonRenderer(button);
             default:
                 return null; // vanilla or unknown
         }
---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/button/PvPClientButtonRenderer.kt
@@ -0,0 +1,29 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/SkidderMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.client.button
+
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.Gui
+import net.minecraft.client.gui.GuiButton
+import java.awt.Color
+
+// remember to add it to the place where all the buttons are set
+// if you want to add the defualt mc font you will need to edit abstract button
+class PvPClientButtonRenderer(button: GuiButton) : AbstractButtonRenderer(button) {
+    override fun render(mouseX: Int, mouseY: Int, mc: Minecraft) {
+        if (!button.visible) { return }
+        Button(button.xPosition - 1, button.yPosition - 1, button.width + 2, 1, Color(140, 140, 140, 79).rgb) //t
+        Button(button.xPosition - 1, button.yPosition + button.height, button.width + 2, 1, Color(140, 140, 140, 79).rgb) //b
+        Button(button.xPosition - 1, button.yPosition, 1, button.height, Color(140, 140, 140, 79).rgb) //l
+        Button(button.xPosition + button.width, button.yPosition, 1, button.height, Color(140, 140, 140, 79).rgb) //r
+        Button(button.xPosition, button.yPosition, button.width, button.height, if (button.hovered) Color(0, 0, 0, 180).rgb else Color(0, 0, 0, 128).rgb)
+   }
+    // i swear if this goes into prod lmaoooo
+    // old util added the x to the pos while this one doesn't so you have to add it manually, u can fix it if you want i cant be arsed
+    fun Button(x: Int, y: Int, width: Int, height: Int, colour: Int){
+        Gui.drawRect(x, y, x + width, y + height, colour)
+    }
+}
