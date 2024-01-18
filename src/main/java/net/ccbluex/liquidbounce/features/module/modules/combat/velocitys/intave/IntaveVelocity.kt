ef1a14e376bc7239f04422dd44f00d637d42c06b
David <101277472+Davidisnotavailable@users.noreply.github.com>
2024-01-19 00:41:46 +0100

    IntaveVelocity.kt bug fix #1066

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/intave/IntaveVelocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/intave/IntaveVelocity.kt
@@ -3,22 +3,29 @@
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
  * https://github.com/SkidderMC/FDPClient/
  */
-package net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.intave
+package net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.other
 
 import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.VelocityMode
 import net.minecraft.client.settings.GameSettings
+
 class IntaveVelocity : VelocityMode("Intave") {
 
     private var jumped = 0
+
+    override fun onUpdate(event: UpdateEvent) {
+        if (mc.currentScreen != null) return
+    }
+    
     override fun onPacket(event: PacketEvent) {
-    if (mc.thePlayer.hurtTime == 9) {
-        if (++jumped % 2 == 0 && mc.thePlayer.onGround && mc.thePlayer.isSprinting && mc.currentScreen == null) {
-            mc.gameSettings.keyBindJump.pressed = true
-            jumped = 0 // reset
-        }
-    } else {
-        mc.gameSettings.keyBindJump.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindJump)
+        if (mc.thePlayer.hurtTime == 9) {
+            if (++jumped % 2 == 0 && mc.thePlayer.onGround && mc.thePlayer.isSprinting && mc.currentScreen == null) {
+                mc.gameSettings.keyBindJump.pressed = true
+                jumped = 0 // reset
+            }
+        } else {
+            mc.gameSettings.keyBindJump.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindJump)
         }
     }
 
