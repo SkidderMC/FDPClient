4e6c12a5ff731465995e0ffc4257c4de88b5b15e
Zywl <86253343+opZywl@users.noreply.github.com>
2024-01-18 21:39:48 -0300

    Update IntaveVelocity.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/intave/IntaveVelocity.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/intave/IntaveVelocity.kt
@@ -3,7 +3,7 @@
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
  * https://github.com/SkidderMC/FDPClient/
  */
-package net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.other
+package net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.intave
 
 import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
