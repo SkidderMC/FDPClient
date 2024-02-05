49ad7fc7add59c68d6b91c0d9ed2a239132fc93d
Dg636 <dg636gamez@gmail.com>
2024-02-04 23:44:46 -0500

    e

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/grim/GrimDamage.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/velocitys/grim/GrimDamage.kt
@@ -6,29 +6,27 @@
 package net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.grim
 
 import net.ccbluex.liquidbounce.FDPClient
-import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.combat.velocitys.VelocityMode
 import net.minecraft.network.play.client.C02PacketUseEntity
 import net.minecraft.network.play.client.C0APacketAnimation
 
 class GrimDamage : VelocityMode("GrimDamage") {
 
-    private var velocityInput = false
-    override fun onPacket(event: PacketEvent) {
-    if (velocityInput) {
-        val target = FDPClient.combatManager.getNearByEntity(3f)
-        repeat(12) {
-            mc.thePlayer.sendQueue.addToSendQueue(
-                C02PacketUseEntity(
-                    target,
-                    C02PacketUseEntity.Action.ATTACK
+    override fun onUpdate(event: UpdateEvent) {
+        if (mc.thePlayer.hurtTime == 9) {
+            val target = FDPClient.combatManager.getNearByEntity(3f)
+            repeat(12) {
+                mc.thePlayer.sendQueue.addToSendQueue(
+                    C02PacketUseEntity(
+                        target,
+                        C02PacketUseEntity.Action.ATTACK
+                    )
                 )
-            )
-            mc.thePlayer.sendQueue.addToSendQueue(C0APacketAnimation())
-        }
-        mc.thePlayer.motionX *= 0.077760000
-        mc.thePlayer.motionZ *= 0.077760000
-        velocityInput = false
+                mc.thePlayer.sendQueue.addToSendQueue(C0APacketAnimation())
+            }
+            mc.thePlayer.motionX *= 0.077760000
+            mc.thePlayer.motionZ *= 0.077760000
         }
     }
 
