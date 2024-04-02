506df1c8b8832992d187a56137705ad0b5f797f7
opZywl <lifeerwz@gmail.com>
2024-04-02 18:21:07 -0300

    memory fix for u computer

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/other/MemoryFix.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/other/MemoryFix.kt
@@ -5,20 +5,59 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.other
 
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.MotionEvent
+import net.ccbluex.liquidbounce.event.TickEvent
+import net.ccbluex.liquidbounce.event.WorldEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
-import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.event.WorldEvent
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.utils.timer.MSTimer
+import net.ccbluex.liquidbounce.value.FloatValue
 
-@ModuleInfo(name = "MemoryFix",  category = ModuleCategory.OTHER)
+@ModuleInfo(name = "MemoryFix", category = ModuleCategory.OTHER)
 object MemoryFix : Module() {
+    private const val DEFAULT_DELAY_MS = 120000L
+    private const val DEFAULT_LIMIT = 80.0f
+    private const val DEFAULT_SPEED = 0.05f
+
+    private val delay = FloatValue("Delay", DEFAULT_DELAY_MS.toFloat(), 10000f, 600000f)
+    private val limit = FloatValue("Limit", DEFAULT_LIMIT, 20.0f, 95.0f)
+    private val speed = FloatValue("Speed", DEFAULT_SPEED, 0.0f, 1.0f)
+
+    private val timer = MSTimer()
+
     override fun onEnable() {
+        super.onEnable()
         Runtime.getRuntime().gc()
     }
-    
+
     @EventTarget
-    fun onWorld(event: WorldEvent) {
-        Runtime.getRuntime().gc()
+    fun onTick(tickEvent: TickEvent?) {
+        val maxMemory = Runtime.getRuntime().maxMemory().toFloat()
+        val usedMemory = (maxMemory - Runtime.getRuntime().freeMemory()).toFloat()
+        val memoryUsagePercentage = usedMemory * 100.0f / maxMemory
+
+        if (timer.hasTimePassed(delay.get().toLong()) && memoryUsagePercentage >= limit.get()) {
+            System.gc()
+            timer.reset()
+        }
+    }
+
+    @EventTarget
+    fun onMotion(event: MotionEvent?) {
+        val keyBind = mc.gameSettings.keyBindForward
+        val player = mc.thePlayer
+
+        if (keyBind.pressed && player.onGround) {
+            player.cameraPitch = speed.get()
+        } else {
+            player.cameraPitch = 0.0f
+        }
+    }
+
+    @EventTarget
+    fun onWorld(event: WorldEvent?) {
+        System.gc()
     }
 }
