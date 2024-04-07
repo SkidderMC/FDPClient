29b82ca22c6007db80edde50cbc086ba614a800b
Dg636 <dg636gamez@gmail.com>
2024-04-06 21:43:47 -0400

    Create LagCommand.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/command/commands/LagCommand.kt
@@ -0,0 +1,35 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/SkidderMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.command.commands
+
+import net.ccbluex.liquidbounce.features.command.Command
+import net.ccbluex.liquidbounce.utils.misc.StringUtils
+
+class LagCommand : Command("lag", emptyArray()) {
+    /**
+     * Execute commands with provided [args]
+     */
+    override fun execute(args: Array<String>) {
+        if (args.size > 1) {
+            var f = 1
+            try {
+                var time = args[1].toLong()
+            } catch (e: NumberFormatException) {
+                chatSyntaxError()
+                return
+            }
+            var startTime = System.currentTimeMillis()
+            while (System.currentTimeMillis() < startTime + time) {
+              f = 1 - f
+            }
+            
+            chat("Lagged")
+            return
+        }
+
+        chatSyntax("lag <milliseconds>")
+    }
+}
