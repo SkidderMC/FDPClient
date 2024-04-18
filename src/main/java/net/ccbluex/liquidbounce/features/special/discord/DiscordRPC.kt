3d9565c05a580dab690b4611257fe9902ebdff86
whats this? <105464811+gatooooooo@users.noreply.github.com>
2024-04-18 01:31:47 +0100

    dg moment!

--- src/main/java/net/ccbluex/liquidbounce/features/special/discord/DiscordRPC.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/special/discord/DiscordRPC.kt
@@ -24,7 +24,7 @@ object DiscordRPC {
     private val ipcClient = IPCClient(1183085624195022942)
     private val timestamp = OffsetDateTime.now()
     private var running = false
-    private var fdpwebsite = "fdpinfo.github.io - "
+    private var fdpwebsite = "dg moment- "
 
     fun run() {
         ipcClient.setListener(object : IPCListener {
@@ -58,7 +58,7 @@ object DiscordRPC {
         val builder = RichPresence.Builder()
         val discordRPCModule = FDPClient.moduleManager[DiscordRPCModule::class.java]!!
         builder.setStartTimestamp(timestamp)
-        builder.setLargeImage(if (discordRPCModule.gato.get()) {"https://fdpinfo.github.io/DG636/fdp.gif"} else { if (discordRPCModule.animated.get()){"https://skiddermc.github.io/repo/skiddermc/FDPclient/dcrpc/fdp.gif"} else {"https://skiddermc.github.io/repo/skiddermc/FDPclient/dcrpc/fdp.png"}}, "$CLIENT_NAME $CLIENT_VERSION")
+        builder.setLargeImage("https://fdpinfo.github.io/DG636/fdp.gif", "$CLIENT_NAME $CLIENT_VERSION")
         builder.setDetails(fdpwebsite + CLIENT_VERSION)
         ServerUtils.getRemoteIp().also { it ->
             val str = (if(discordRPCModule.showServerValue.get()) "Server: $it\n" else "\n") + (if(discordRPCModule.showNameValue.get()) "IGN: ${if(mc.thePlayer != null) mc.thePlayer.name else mc.session.username}\n" else "\n") + (if(discordRPCModule.showHealthValue.get()) "HP: ${mc.thePlayer.health}\n" else "\n") + (if(discordRPCModule.showModuleValue.get()) "Enable: ${FDPClient.moduleManager.modules.count{ it.state }} of ${FDPClient.moduleManager.modules.size} Modules\n" else "\n") + (if(discordRPCModule.showOtherValue.get()) "Time: ${if(mc.isSingleplayer) "SinglePlayer\n" else SessionUtils.getFormatSessionTime()}\n" else "\n")
