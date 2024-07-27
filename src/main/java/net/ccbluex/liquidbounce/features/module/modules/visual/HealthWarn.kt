e7f89bb9a285c76dfe8198db5eac659afc244708
opZywl <lifeerwz@gmail.com>
2024-07-26 21:21:41 -0300

    feat: HealthWarn Module

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/visual/HealthWarn.kt
@@ -0,0 +1,43 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/SkidderMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.visual
+
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.ui.client.hud.HUD.addNotification
+import net.ccbluex.liquidbounce.ui.client.hud.element.elements.Notification
+import net.ccbluex.liquidbounce.ui.client.hud.element.elements.Type
+import net.ccbluex.liquidbounce.value.IntegerValue
+
+object HealthWarn: Module("HealthWarn", Category.VISUAL, hideModule = false) {
+
+    private val healthValue by IntegerValue("Health", 7, 1.. 20)
+
+    private var canWarn = true
+
+    override fun onEnable() {
+        canWarn = true
+    }
+
+    override fun onDisable() {
+        canWarn = true
+    }
+
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+        if (mc.thePlayer.health <= healthValue) {
+            if (canWarn) {
+                addNotification(Notification("HP Warning","YOU ARE AT LOW HP!", Type.ERROR))
+
+                canWarn = false
+            }
+        } else {
+            canWarn = true
+        }
+    }
+}
