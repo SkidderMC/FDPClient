10eb66782b0588591ba855a22868ea76b57ca59a
opZywl <lifeerwz@gmail.com>
2024-08-09 20:15:38 -0300

    fix: black hotbar option not being aligned correctly.

--- src/main/java/net/ccbluex/liquidbounce/utils/render/FakeItemRender.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/render/FakeItemRender.kt
@@ -1,6 +1,9 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/SkidderMC/FDPClient/
+ */
 package net.ccbluex.liquidbounce.utils.render
-
-import net.minecraft.item.ItemStack
 
 object FakeItemRender {
     var fakeItem: Int = -1
