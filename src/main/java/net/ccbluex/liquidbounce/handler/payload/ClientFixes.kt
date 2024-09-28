ce1ddbb97852d0adbf2305d750c24f339183de11
opZywl <lifeerwz@gmail.com>
2024-09-28 11:16:35 -0300

    refactor: removed mixinguiIngameforge class

--- src/main/java/net/ccbluex/liquidbounce/handler/payload/ClientFixes.kt
+++ src/main/java/net/ccbluex/liquidbounce/handler/payload/ClientFixes.kt
@@ -45,7 +45,7 @@ object ClientFixes : MinecraftInstance(), Listenable {
                     packet.data = PacketBuffer(Unpooled.buffer()).writeString(
                         when (possibleBrands.get()) {
                             "Vanilla" -> "vanilla"
-                            "LunarClient" -> "lunarclient:v2.15.6-2422"
+                            "LunarClient" -> "lunarclient:v2.17.2-2437"
                             "OptiFine" -> "optifine"
                             "CheatBreaker" -> "CB"
                             "Fabric" -> "fabric"
