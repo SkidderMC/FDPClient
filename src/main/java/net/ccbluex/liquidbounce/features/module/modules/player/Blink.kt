0832f90ea9bc59a650fb02ac5fa73f48c9cf9edc
opZywl <lifeerwz@gmail.com>
2024-10-13 10:03:28 -0300

    feat: improvements blink utils / outgoingValue blink mode

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Blink.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Blink.kt
@@ -10,41 +10,93 @@ import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.modules.visual.Breadcrumbs
 import net.ccbluex.liquidbounce.utils.BlinkUtils
+import net.ccbluex.liquidbounce.utils.PacketUtils
+import net.ccbluex.liquidbounce.utils.PacketUtils.handlePacket
+import net.ccbluex.liquidbounce.utils.misc.RandomUtils
 import net.ccbluex.liquidbounce.utils.render.ColorUtils.rainbow
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.glColor
 import net.ccbluex.liquidbounce.utils.timing.MSTimer
 import net.ccbluex.liquidbounce.value.BoolValue
 import net.ccbluex.liquidbounce.value.IntegerValue
 import net.ccbluex.liquidbounce.value.ListValue
+import net.minecraft.client.entity.EntityOtherPlayerMP
+import net.minecraft.network.Packet
 import net.minecraft.network.play.server.S02PacketChat
 import net.minecraft.network.play.server.S40PacketDisconnect
 import net.minecraft.network.status.client.C01PacketPing
 import net.minecraft.network.handshake.client.C00Handshake
+import net.minecraft.network.play.INetHandlerPlayClient
 import net.minecraft.network.status.client.C00PacketServerQuery
 import org.lwjgl.opengl.GL11.*
 import java.awt.Color
+import java.util.*
+import java.util.concurrent.LinkedBlockingQueue
 
 object Blink : Module("Blink", Category.PLAYER, gameDetecting = false, hideModule = false) {
 
 	private val mode by ListValue("Mode", arrayOf("Sent", "Received", "Both"), "Sent")
 
+    private val outgoingValue = BoolValue("OutGoing", true)
+
     private val pulse by BoolValue("Pulse", false)
-		private val pulseDelay by IntegerValue("PulseDelay", 1000, 500..5000) { pulse }
+		private var pulseDelay by IntegerValue("PulseDelay", 1000, 500..5000) { pulse }
+
+    private val outgoingMin by IntegerValue("MinOutGoing", 1000, 100.. 5000) { pulse }
+    private val outgoingMax by IntegerValue("MaxOutGoing", 1500, 100.. 5000) { pulse }
 
     private val fakePlayerMenu by BoolValue("FakePlayer", true)
 
     private val pulseTimer = MSTimer()
+    private var fakePlayer: EntityOtherPlayerMP? = null
+    private val positions = LinkedList<DoubleArray>()
+
+    private val packets = LinkedBlockingQueue<Packet<INetHandlerPlayClient>>()
+
 
     override fun onEnable() {
+        if (mc.thePlayer == null) return
+        if (outgoingValue.get()) {
+            BlinkUtils.setBlinkState(all = true)
+            if (!pulse) {
+                fakePlayer = EntityOtherPlayerMP(mc.theWorld, mc.thePlayer.gameProfile)
+                fakePlayer!!.clonePlayer(mc.thePlayer, true)
+                fakePlayer!!.copyLocationAndAnglesFrom(mc.thePlayer)
+                fakePlayer!!.rotationYawHead = mc.thePlayer.rotationYawHead
+                mc.theWorld.addEntityToWorld(-1337, fakePlayer)
+            } else {
+                pulseDelay = RandomUtils.nextInt(outgoingMin, outgoingMax)
+            }
+        }
+        packets.clear()
+        synchronized(positions) {
+            positions.add(doubleArrayOf(mc.thePlayer.posX, mc.thePlayer.entityBoundingBox.minY + mc.thePlayer.getEyeHeight() / 2, mc.thePlayer.posZ))
+            positions.add(doubleArrayOf(mc.thePlayer.posX, mc.thePlayer.entityBoundingBox.minY, mc.thePlayer.posZ))
+        }
+
         pulseTimer.reset()
 
         if (fakePlayerMenu)
             BlinkUtils.addFakePlayer()
     }
 
+    private fun clearPackets() {
+        while (!packets.isEmpty()) {
+            handlePacket(packets.take() as Packet<INetHandlerPlayClient?>)
+        }
+    }
+
     override fun onDisable() {
         if (mc.thePlayer == null)
             return
+
+        synchronized(positions) { positions.clear() }
+        if (mc.thePlayer == null) return
+        BlinkUtils.setBlinkState(off = true, release = true)
+        clearPackets()
+        if (fakePlayer != null) {
+            mc.theWorld.removeEntityFromWorld(fakePlayer!!.entityId)
+            fakePlayer = null
+        }
 
         BlinkUtils.unblink()
     }
--- src/main/java/net/ccbluex/liquidbounce/utils/BlinkUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/BlinkUtils.kt
@@ -13,6 +13,7 @@ import net.ccbluex.liquidbounce.utils.misc.RandomUtils
 import net.minecraft.client.entity.EntityOtherPlayerMP
 import net.minecraft.network.Packet
 import net.minecraft.network.handshake.client.C00Handshake
+import net.minecraft.network.play.INetHandlerPlayServer
 import net.minecraft.network.play.client.C01PacketChatMessage
 import net.minecraft.network.play.client.C03PacketPlayer
 import net.minecraft.network.play.server.S02PacketChat
@@ -20,6 +21,8 @@ import net.minecraft.network.play.server.S29PacketSoundEffect
 import net.minecraft.network.status.client.C00PacketServerQuery
 import net.minecraft.network.status.client.C01PacketPing
 import net.minecraft.util.Vec3
+import java.math.BigInteger
+import java.util.*
 
 object BlinkUtils {
 
@@ -28,8 +31,28 @@ object BlinkUtils {
     val packetsReceived = mutableListOf<Packet<*>>()
     private var fakePlayer: EntityOtherPlayerMP? = null
     val positions = mutableListOf<Vec3>()
+    private val playerBuffer = LinkedList<Packet<INetHandlerPlayServer>>()
+    const val Invalid_Type = -301
+    const val MisMatch_Type = -302
+    var movingPacketStat = false
+    var transactionStat = false
+    var keepAliveStat = false
+    var actionStat = false
+    var abilitiesStat = false
+    var invStat = false
+    var interactStat = false
+    var otherPacket = false
     val isBlinking
         get() = (packets.size + packetsReceived.size) > 0
+
+    private var packetToggleStat = booleanArrayOf(false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false)
+    init {
+        setBlinkState(
+            off = true,
+            release = true
+        )
+        clearPacket()
+    }
 
     // TODO: Make better & more reliable BlinkUtils.
     fun blink(packet: Packet<*>, event: PacketEvent, sent: Boolean? = true, receive: Boolean? = true) {
@@ -122,6 +145,137 @@ object BlinkUtils {
 
         if (sent == false && receive == false)
             unblink()
+    }
+
+    fun releasePacket(packetType: String? = null, onlySelected: Boolean = false, amount: Int = -1, minBuff: Int = 0) {
+        var count = 0
+        when(packetType) {
+            null -> {
+                count = -1
+                for (packets in playerBuffer) {
+                    val packetID = BigInteger(packets.javaClass.simpleName.substring(1..2), 16).toInt()
+                    if (packetToggleStat[packetID] || !onlySelected) {
+                        sendPacket(packets)
+                    }
+                }
+            }
+            else -> {
+                val tempBuffer = LinkedList<Packet<INetHandlerPlayServer>>()
+                for (packets in playerBuffer) {
+                    val className = packets.javaClass.simpleName
+                    if (className.equals(packetType, ignoreCase = true)) {
+                        tempBuffer.add(packets)
+                    }
+                }
+                while(tempBuffer.size > minBuff && (count < amount || amount <= 0)) {
+                    sendPacket(tempBuffer.pop())
+                    count++
+                }
+            }
+        }
+        clearPacket(packetType = packetType, onlySelected = onlySelected, amount = count)
+    }
+
+    fun clearPacket(packetType: String? = null, onlySelected: Boolean = false, amount: Int = -1) {
+        when(packetType) {
+            null -> {
+                val tempBuffer = LinkedList<Packet<INetHandlerPlayServer>>()
+                for (packets in playerBuffer) {
+                    val packetID = BigInteger(packets.javaClass.simpleName.substring(1..2), 16).toInt()
+                    if (!packetToggleStat[packetID] && onlySelected) {
+                        tempBuffer.add(packets)
+                    }
+                }
+                playerBuffer.clear()
+                for (packets in tempBuffer) {
+                    playerBuffer.add(packets)
+                }
+            }
+            else -> {
+                var count = 0
+                val tempBuffer = LinkedList<Packet<INetHandlerPlayServer>>()
+                for (packets in playerBuffer) {
+                    val className = packets.javaClass.simpleName
+                    if (!className.equals(packetType, ignoreCase = true)) {
+                        tempBuffer.add(packets)
+                    } else {
+                        count++
+                        if (count > amount) {
+                            tempBuffer.add(packets)
+                        }
+                    }
+                }
+                playerBuffer.clear()
+                for (packets in tempBuffer) {
+                    playerBuffer.add(packets)
+                }
+            }
+        }
+    }
+
+    fun pushPacket(packets: Packet<*>):Boolean {
+        val packetID = BigInteger(packets.javaClass.simpleName.substring(1..2), 16).toInt()
+        if (packetToggleStat[packetID] && !isBlacklisted(packets.javaClass.simpleName)) {
+            playerBuffer.add(packets as Packet<INetHandlerPlayServer>)
+            return true
+        }
+        return false
+    }
+
+    fun isBlacklisted(packetType: String = ""):Boolean {
+        return when(packetType) {
+            "C00Handshake", "C00PacketLoginStart", "C00PacketServerQuery", "C01PacketChatMessage", "C01PacketEncryptionResponse", "C01PacketPing" -> true
+            else -> false
+        }
+    }
+
+    fun setBlinkState() {
+        setBlinkState(off = true)
+        clearPacket()
+    }
+
+    fun setBlinkState(
+        off: Boolean = false,
+        release: Boolean = false,
+        all: Boolean = false,
+        packetMoving: Boolean = movingPacketStat,
+        packetTransaction: Boolean = transactionStat,
+        packetKeepAlive: Boolean = keepAliveStat,
+        packetAction: Boolean = actionStat,
+        packetAbilities: Boolean = abilitiesStat,
+        packetInventory: Boolean = invStat,
+        packetInteract: Boolean = interactStat,
+        other: Boolean = otherPacket
+    ) {
+        if (release) {
+            releasePacket()
+        }
+        movingPacketStat = (packetMoving && !off) || all
+        transactionStat = (packetTransaction && !off) || all
+        keepAliveStat = (packetKeepAlive && !off) || all
+        actionStat = (packetAction && !off) || all
+        abilitiesStat = (packetAbilities && !off) || all
+        invStat = (packetInventory && !off )|| all
+        interactStat = (packetInteract && !off) || all
+        otherPacket = (other && !off) || all
+        if (all) {
+            for(i in packetToggleStat.indices) {
+                packetToggleStat[i] = true
+            }
+        } else {
+            for(i in packetToggleStat.indices) {
+                when(i) {
+                    0x00 -> packetToggleStat[i] = keepAliveStat
+                    0x01, 0x11, 0x12, 0x14, 0x15, 0x17, 0x18, 0x19 ->  packetToggleStat[i] = otherPacket
+                    0x03, 0x04, 0x05, 0x06 -> packetToggleStat[i] = movingPacketStat
+                    0x0F -> packetToggleStat[i] = transactionStat
+                    0x02, 0x09, 0x0A, 0x0B -> packetToggleStat[i] = actionStat
+                    0x0C, 0x13 -> packetToggleStat[i] = abilitiesStat
+                    0x0D, 0x0E, 0x10, 0x16 -> packetToggleStat[i] = invStat
+                    0x07, 0x08 -> packetToggleStat[i] = interactStat
+                }
+            }
+        }
     }
 
     @EventTarget
