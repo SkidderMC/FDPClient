204b9340b96bea9ede9b0d83238afe8bb69f5cb9
Zywl <86253343+opZywl@users.noreply.github.com>
2024-12-01 20:36:06 -0300

    refactor: improved locks in MiniMapRegister

--- src/main/java/net/ccbluex/liquidbounce/utils/render/MiniMapRegister.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/render/MiniMapRegister.kt
@@ -12,69 +12,66 @@ import net.ccbluex.liquidbounce.utils.MinecraftInstance
 import net.minecraft.client.renderer.texture.DynamicTexture
 import net.minecraft.util.BlockPos
 import net.minecraft.world.chunk.Chunk
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.concurrent.read
+import kotlin.concurrent.write
 
 object MiniMapRegister : MinecraftInstance(), Listenable {
 
-    private val chunkTextureMap = hashMapOf<ChunkLocation, MiniMapTexture>()
+    private val chunkTextureMap = HashMap<ChunkLocation, MiniMapTexture>(256)
     private val queuedChunkUpdates = HashSet<Chunk>(256)
     private val queuedChunkDeletions = HashSet<ChunkLocation>(256)
-    private val deleteAllChunks = AtomicBoolean(false)
+    private var deleteAllChunks = false
+
+    private val lock = ReentrantReadWriteLock()
 
     fun updateChunk(chunk: Chunk) {
-        synchronized(queuedChunkUpdates) {
+        lock.write {
             queuedChunkUpdates += chunk
         }
     }
 
-    fun getChunkTextureAt(x: Int, z: Int) = chunkTextureMap[ChunkLocation(x, z)]
+    fun getChunkTextureAt(x: Int, z: Int) = lock.read { chunkTextureMap[ChunkLocation(x, z)] }
 
     @EventTarget
-    fun onRender2D(render2DEvent: Render2DEvent) {
+    fun onRender2D(event: Render2DEvent) {
         updateChunks()
     }
 
-    fun updateChunks() {
-        synchronized(queuedChunkUpdates) {
-            if (deleteAllChunks.get()) {
-                synchronized(queuedChunkDeletions) {
-                    queuedChunkDeletions.clear()
-                }
+    private fun updateChunks() {
+        lock.write {
+            if (deleteAllChunks) {
+                queuedChunkDeletions.clear()
                 queuedChunkUpdates.clear()
 
-                chunkTextureMap.forEach { it.value.delete() }
-
+                chunkTextureMap.values.forEach { it.delete() }
                 chunkTextureMap.clear()
 
-                deleteAllChunks.set(false)
+                deleteAllChunks = false
             } else {
-                synchronized(queuedChunkDeletions) {
-                    queuedChunkDeletions.forEach {
-                        chunkTextureMap.remove(it)?.delete()
-                    }
-                    queuedChunkDeletions.clear()
+                queuedChunkDeletions.forEach {
+                    chunkTextureMap.remove(it)?.delete()
                 }
+                queuedChunkDeletions.clear()
             }
 
             queuedChunkUpdates.forEach {
-                chunkTextureMap.computeIfAbsent(ChunkLocation(it.xPosition, it.zPosition)) {
-                    MiniMapTexture()
-                }.updateChunkData(it)
+                chunkTextureMap.getOrPut(it.location, ::MiniMapTexture).updateChunkData(it)
             }
 
             queuedChunkUpdates.clear()
         }
     }
 
-    fun getLoadedChunkCount() = chunkTextureMap.size
+    fun getLoadedChunkCount() = lock.read { chunkTextureMap.size }
 
     fun unloadChunk(x: Int, z: Int) {
-        synchronized(queuedChunkDeletions) {
+        lock.write {
             queuedChunkDeletions += ChunkLocation(x, z)
         }
     }
 
-    fun unloadAllChunks() = deleteAllChunks.set(true)
+    fun unloadAllChunks() = lock.write { deleteAllChunks = true }
 
     class MiniMapTexture {
         val texture = DynamicTexture(16, 16)
@@ -89,7 +86,7 @@ object MiniMapRegister : MinecraftInstance(), Listenable {
                     val bp = pos.set(x, chunk.getHeightValue(x, z) - 1, z)
                     val blockState = chunk.getBlockState(bp)
 
-                    rgbValues[rgbValues.size - (z * 16 + x + 1)] = blockState.block.getMapColor(blockState).colorValue or (0xFF shl 24)
+                    rgbValues[rgbValues.size - 1 - (z shl 4 or x)] = blockState.block.getMapColor(blockState).colorValue or (0xFF shl 24)
                 }
             }
 
@@ -110,5 +107,9 @@ object MiniMapRegister : MinecraftInstance(), Listenable {
         }
     }
 
+    private val Chunk.location: ChunkLocation
+        get() = ChunkLocation(xPosition, zPosition)
+
     data class ChunkLocation(val x: Int, val z: Int)
+
 }
