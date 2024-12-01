20b85181768492e384ed704b8f79a9baa9775eb4
Zywl <86253343+opZywl@users.noreply.github.com>
2024-12-01 20:57:04 -0300

    fix: Yaw/Pitch randomization range options not disappearing when their float parent option value is set to max.

--- src/main/java/net/ccbluex/liquidbounce/utils/RandomizationSettings.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/RandomizationSettings.kt
@@ -14,10 +14,10 @@ class RandomizationSettings(owner: Module, generalApply: () -> Boolean = { true 
     val randomize by boolean("RandomizeRotations", false) { generalApply() }
     val yawRandomizationChance by floatRange("YawRandomizationChance", 0.8f..1.0f, 0f..1f) { randomize }
     val yawRandomizationRange by floatRange("YawRandomizationRange", 5f..10f, 0f..30f)
-    { randomize && yawRandomizationChance.endInclusive != 0f }
+    { randomize && yawRandomizationChance.start != 1F }
     val pitchRandomizationChance by floatRange("PitchRandomizationChance", 0.8f..1.0f, 0f..1f) { randomize }
     val pitchRandomizationRange by floatRange("PitchRandomizationRange", 5f..10f, 0f..30f)
-    { randomize && pitchRandomizationChance.endInclusive != 0f }
+    { randomize && pitchRandomizationChance.start != 1F }
 
     init {
         owner.addConfigurable(this)
