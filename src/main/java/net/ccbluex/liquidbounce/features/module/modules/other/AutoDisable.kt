7d28cd1081b5417135ae8998325d37fa06dbfd3c
Zywl <86253343+opZywl@users.noreply.github.com>
2024-12-17 20:07:11 -0300

    fix: AutoDisable duplicated function call `onDisable` will be called when we set state to false.

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/other/AutoDisable.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/other/AutoDisable.kt
@@ -55,14 +55,11 @@ object AutoDisable : Module("AutoDisable", Category.OTHER, gameDetecting = false
     }
 
     private fun disabled(reason: String) {
-        val anyModuleEnabled = modulesList.any { it.state }
+        val enabledModules = modulesList.filter { it.state }
 
-        if (anyModuleEnabled) {
-            modulesList.forEach { module ->
-                if (module.state) {
-                    module.state = false
-                    module.onDisable()
-                }
+        if (enabledModules.isNotEmpty()) {
+            enabledModules.forEach { module ->
+                module.state = false
             }
 
             if (warn == "Chat") {
@@ -84,6 +81,6 @@ object AutoDisable : Module("AutoDisable", Category.OTHER, gameDetecting = false
     }
 
     fun getModules(): List<Module> {
-        return modulesList.toList()
+        return modulesList
     }
 }
