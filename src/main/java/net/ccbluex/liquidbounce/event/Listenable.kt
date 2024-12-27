30d2d005f77fab86bd774e894143bea41315bc88
Zywl <86253343+opZywl@users.noreply.github.com>
2024-12-27 20:11:55 -0300

    refactor: default implementation of Listenable handleEvents

--- src/main/java/net/ccbluex/liquidbounce/event/Listenable.kt
+++ src/main/java/net/ccbluex/liquidbounce/event/Listenable.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 
 interface Listenable {
-    fun handleEvents() = true
+    fun handleEvents(): Boolean = parent?.handleEvents() ?: true
 
     val subListeners: Array<Listenable>
         get() = emptyArray()
