dd1afa727a5d18ba1c09b57ce7031db343feac04
Zywl <86253343+opZywl@users.noreply.github.com>
2025-01-08 20:38:43 -0300

    feat/chore: InvMove Grim Bypass

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/InvMove.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/InvMove.kt
@@ -5,19 +5,21 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
+import net.ccbluex.liquidbounce.config.boolean
+import net.ccbluex.liquidbounce.config.float
 import net.ccbluex.liquidbounce.event.*
-import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.ui.client.clickgui.ClickGui
 import net.ccbluex.liquidbounce.ui.client.hud.designer.GuiHudDesigner
-import net.ccbluex.liquidbounce.utils.movement.MovementUtils
+import net.ccbluex.liquidbounce.utils.client.PacketUtils
 import net.ccbluex.liquidbounce.utils.extensions.isMoving
 import net.ccbluex.liquidbounce.utils.inventory.InventoryManager
 import net.ccbluex.liquidbounce.utils.inventory.InventoryManager.canClickInventory
 import net.ccbluex.liquidbounce.utils.inventory.InventoryManager.hasScheduledInLastLoop
+import net.ccbluex.liquidbounce.utils.inventory.InventoryUtils.serverOpenContainer
 import net.ccbluex.liquidbounce.utils.inventory.InventoryUtils.serverOpenInventory
-import net.ccbluex.liquidbounce.config.boolean
-import net.ccbluex.liquidbounce.config.float
+import net.ccbluex.liquidbounce.utils.movement.MovementUtils
 import net.minecraft.client.gui.GuiChat
 import net.minecraft.client.gui.GuiIngameMenu
 import net.minecraft.client.gui.inventory.GuiChest
@@ -25,22 +27,26 @@ import net.minecraft.client.gui.inventory.GuiContainer
 import net.minecraft.client.gui.inventory.GuiInventory
 import net.minecraft.client.settings.GameSettings
 import net.minecraft.client.settings.KeyBinding
+import net.minecraft.network.play.client.C0DPacketCloseWindow
+import net.minecraft.network.play.client.C0EPacketClickWindow
 import org.lwjgl.input.Keyboard
 import org.lwjgl.input.Mouse
 
 object InvMove : Module("InvMove", Category.MOVEMENT, gameDetecting = false, hideModule = false) {
 
-    private val fullMovements by boolean("FullMovements", false)
     private val notInChests by boolean("NotInChests", false)
+    val aacAdditionPro by boolean("AACAdditionPro", false)
+    private val intave by boolean("Intave", false)
+    private val saveC0E by boolean("SaveC0E", false)
+    private val noSprintWhenClosed by boolean("NoSprintWhenClosed", false) { saveC0E }
+
     private val noDetectableValue by boolean("NoDetectable", false)
     private val noMoveClicksValue by boolean("NoMoveClicks", false)
 
     private val rotate by boolean("Rotate", false)
 
-    val aacAdditionPro by boolean("AACAdditionPro", false)
-    private val intave by boolean("Intave", false)
-
     private val isIntave = (mc.currentScreen is GuiInventory || mc.currentScreen is GuiChest) && intave
+    private val clickWindowList = ArrayDeque<C0EPacketClickWindow>()
 
     private val noMove by InventoryManager.noMoveValue
     private val noMoveAir by InventoryManager.noMoveAirValue
@@ -66,7 +72,7 @@ object InvMove : Module("InvMove", Category.MOVEMENT, gameDetecting = false, hid
         mc.gameSettings.keyBindSprint
     )
 
-    val onUpdate = handler<UpdateEvent> { event ->
+    val onUpdate = handler<UpdateEvent> {
         val player = mc.thePlayer ?: return@handler
         val screen = mc.currentScreen
 
@@ -85,16 +91,14 @@ object InvMove : Module("InvMove", Category.MOVEMENT, gameDetecting = false, hid
             player.motionZ *= inventoryMotion
         }
 
-        if (!fullMovements && (screen is GuiChat || screen is GuiIngameMenu)) return@handler
-
         if (silentlyCloseAndReopen && screen is GuiInventory) {
             if (canClickInventory(closeWhenViolating = true) && !reopenOnClick)
                 serverOpenInventory = true
         }
 
         for (affectedBinding in affectedBindings)
-            affectedBinding.pressed = isButtonPressed(affectedBinding)
-                    || (affectedBinding == mc.gameSettings.keyBindSprint && handleEvents() && Sprint.mode == "Legit" && (!Sprint.onlyOnSprintPress || mc.thePlayer.isSprinting))
+            affectedBinding.pressed =
+                isButtonPressed(affectedBinding) || (affectedBinding == mc.gameSettings.keyBindSprint && Sprint.handleEvents() && Sprint.mode == "Legit" && (!Sprint.onlyOnSprintPress || mc.thePlayer.isSprinting))
     }
 
     private fun updateKeyState() {
@@ -122,7 +126,7 @@ object InvMove : Module("InvMove", Category.MOVEMENT, gameDetecting = false, hid
         }
     }
 
-    val onStrafe = handler<StrafeEvent> { event ->
+    val onStrafe = handler<StrafeEvent> {
         if (isIntave) {
             mc.gameSettings.keyBindSneak.pressed = true
         }
@@ -150,6 +154,37 @@ object InvMove : Module("InvMove", Category.MOVEMENT, gameDetecting = false, hid
 
     val onScreen = handler<ScreenEvent> { event ->
         updateKeyState()
+    }
+
+    val onPacket = handler<PacketEvent> { event ->
+        val packet = event.packet
+        val player = mc.thePlayer ?: return@handler
+
+        if (!saveC0E) return@handler
+
+        if (noSprintWhenClosed) {
+            if (clickWindowList.isNotEmpty() && !(serverOpenInventory || serverOpenContainer))
+                player.isSprinting = false
+
+            if (packet is C0DPacketCloseWindow) {
+                event.cancelEvent()
+                player.isSprinting = false
+                if (!player.serverSprintState)
+                    PacketUtils.sendPacket(C0DPacketCloseWindow(),false)
+            }
+        }
+
+        if (serverOpenInventory || serverOpenContainer) {
+            if (packet is C0EPacketClickWindow) {
+                clickWindowList.add(packet)
+                event.cancelEvent()
+            }
+        } else if (clickWindowList.isNotEmpty()) {
+            clickWindowList.forEach {
+                PacketUtils.sendPacket(it,false)
+            }
+            clickWindowList.clear()
+        }
     }
 
     override fun onDisable() {
