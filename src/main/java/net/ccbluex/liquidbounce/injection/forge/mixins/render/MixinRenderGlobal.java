c4018a3e214f4a4b5144fcbd4bc59b51ca55d69a
Zywl <86253343+opZywl@users.noreply.github.com>
2025-01-19 22:16:38 -0300

    fix: True positions not updating when target is out of screen.

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinRenderGlobal.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinRenderGlobal.java
@@ -6,6 +6,8 @@
 package net.ccbluex.liquidbounce.injection.forge.mixins.render;
 
 import net.ccbluex.liquidbounce.features.module.modules.visual.FreeCam;
+import net.ccbluex.liquidbounce.injection.implementations.IMixinEntity;
+import net.ccbluex.liquidbounce.utils.client.PacketUtilsKt;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.renderer.culling.ICamera;
 import net.minecraft.client.renderer.entity.RenderManager;
@@ -25,6 +27,14 @@ public class MixinRenderGlobal {
 
     @Redirect(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/RenderManager;shouldRender(Lnet/minecraft/entity/Entity;Lnet/minecraft/client/renderer/culling/ICamera;DDD)Z"))
     private boolean injectFreeCamB(RenderManager instance, Entity entity, ICamera camera, double x, double y, double z) {
+
+        if (entity instanceof EntityLivingBase) {
+            IMixinEntity iEntity = (IMixinEntity) entity;
+            if (iEntity.getTruePos()) {
+                PacketUtilsKt.interpolatePosition(iEntity);
+            }
+        }
+
         return FreeCam.INSTANCE.handleEvents() || instance.shouldRender(entity, camera, x, y, z);
     }
 }
--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinRenderManager.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinRenderManager.java
@@ -7,12 +7,8 @@ package net.ccbluex.liquidbounce.injection.forge.mixins.render;
 
 import net.ccbluex.liquidbounce.features.module.modules.combat.HitBox;
 import net.ccbluex.liquidbounce.features.module.modules.visual.FreeCam;
-import net.ccbluex.liquidbounce.injection.implementations.IMixinEntity;
-import net.ccbluex.liquidbounce.utils.client.PacketUtilsKt;
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -52,16 +48,6 @@ public abstract class MixinRenderManager {
     private void renderEntityStatic(Entity entity, float tickDelta, boolean bool, CallbackInfoReturnable<Boolean> cir) {
         FreeCam.INSTANCE.restoreOriginalPosition();
 
-        if (entity instanceof EntityPlayerSP)
-            return;
-
-        if (entity instanceof EntityLivingBase) {
-            IMixinEntity iEntity = (IMixinEntity) entity;
-
-            if (iEntity.getTruePos()) {
-                PacketUtilsKt.interpolatePosition(iEntity);
-            }
-        }
     }
 
     @Inject(method = "renderEntityStatic", at = @At("TAIL"))
