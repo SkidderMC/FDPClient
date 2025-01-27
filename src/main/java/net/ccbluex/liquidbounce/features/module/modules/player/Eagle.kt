565465d6d2e0e5fb01ccd467319f078ff42ed532
Zywl <86253343+opZywl@users.noreply.github.com>
2025-01-27 20:10:21 -0300

    refactor/fix: recode eagle maxsneaktime

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Eagle.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Eagle.kt
@@ -5,60 +5,42 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.player
 
-import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.event.loopHandler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.utils.block.block
-import net.ccbluex.liquidbounce.utils.timing.MSTimer
-import net.ccbluex.liquidbounce.event.handler
-import net.minecraft.client.gui.Gui
+import net.ccbluex.liquidbounce.utils.timing.TickTimer
 import net.minecraft.client.settings.GameSettings
 import net.minecraft.init.Blocks.air
 import net.minecraft.util.BlockPos
 
 object Eagle : Module("Eagle", Category.PLAYER) {
 
-    private val sneakDelay by int("SneakDelay", 0, 0..100)
+    private val maxSneakTime by intRange("MaxSneakTime", 1..5, 0..20)
     private val onlyWhenLookingDown by boolean("OnlyWhenLookingDown", false)
     private val lookDownThreshold by float("LookDownThreshold", 45f, 0f..90f) { onlyWhenLookingDown }
 
-    private val sneakTimer = MSTimer()
-    private var sneakOn = false
+    private val sneakTimer = TickTimer()
 
+    val onUpdate = loopHandler {
+        val thePlayer = mc.thePlayer ?: return@loopHandler
 
-    val onUpdate = handler<UpdateEvent> {
-        val thePlayer = mc.thePlayer ?: return@handler
+        if (GameSettings.isKeyDown(mc.gameSettings.keyBindSneak)) return@loopHandler
 
         if (thePlayer.onGround && BlockPos(thePlayer).down().block == air) {
             val shouldSneak = !onlyWhenLookingDown || thePlayer.rotationPitch >= lookDownThreshold
 
-            if (shouldSneak && !GameSettings.isKeyDown(mc.gameSettings.keyBindSneak)) {
-                if (sneakTimer.hasTimePassed(sneakDelay)) {
-                    mc.gameSettings.keyBindSneak.pressed = true
-                    sneakTimer.reset()
-                    sneakOn = false
-                }
-            } else {
-                mc.gameSettings.keyBindSneak.pressed = false
-            }
-
-            sneakOn = true
+            mc.gameSettings.keyBindSneak.pressed = shouldSneak && !GameSettings.isKeyDown(mc.gameSettings.keyBindSneak)
         } else {
-            if (sneakOn) {
+            if (sneakTimer.hasTimePassed(maxSneakTime.random())) {
                 mc.gameSettings.keyBindSneak.pressed = false
-                sneakOn = false
-            }
+                sneakTimer.reset()
+            } else sneakTimer.update()
         }
-
-        if (!sneakOn && mc.currentScreen !is Gui) mc.gameSettings.keyBindSneak.pressed =
-            GameSettings.isKeyDown(mc.gameSettings.keyBindSneak)
     }
 
     override fun onDisable() {
-        if (mc.thePlayer == null)
-            return
-
-        sneakOn = false
+        sneakTimer.reset()
 
         if (!GameSettings.isKeyDown(mc.gameSettings.keyBindSneak))
             mc.gameSettings.keyBindSneak.pressed = false
