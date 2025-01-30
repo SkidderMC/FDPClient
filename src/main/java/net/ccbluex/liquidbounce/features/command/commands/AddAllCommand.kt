661ff4875782520010dfc7a6a6be4b83a3f27950
Davi Lopes <profissionaldavilopes@gmail.com>
2025-01-30 20:06:40 -0300

    fixed: addall remove incorrect tag true/false name

--- src/main/java/net/ccbluex/liquidbounce/features/command/commands/AddAllCommand.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/command/commands/AddAllCommand.kt
@@ -5,42 +5,48 @@
  */
 package net.ccbluex.liquidbounce.features.command.commands
 
-import net.ccbluex.liquidbounce.FDPClient
 import net.ccbluex.liquidbounce.features.command.Command
-import net.ccbluex.liquidbounce.file.configs.FriendsConfig
-import net.ccbluex.liquidbounce.script.api.global.Chat
-import net.ccbluex.liquidbounce.utils.render.ColorUtils.stripColor
+import net.ccbluex.liquidbounce.utils.render.ColorUtils
 import net.ccbluex.liquidbounce.utils.render.ColorUtils.translateAlternateColorCodes
 import net.minecraft.client.network.NetworkPlayerInfo
 import net.minecraft.util.EnumChatFormatting
+import net.ccbluex.liquidbounce.file.FileManager.friendsConfig
+import net.ccbluex.liquidbounce.script.api.global.Chat
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.function.Consumer
 
-object AddAllCommand : Command("addall", *arrayOf<String>("")) {
+object AddAllCommand : Command("addall", arrayOf("").toString()) {
     override fun execute(arguments: Array<String>) {
         if (arguments.size == 2) {
             val tag = translateAlternateColorCodes(arguments[1])
             val count = AtomicInteger(0)
-            val config: FriendsConfig = FDPClient.fileManager.friendsConfig
+            val friendManager = friendsConfig
+
             val presistent = arguments[0].contains("")
 
             mc.thePlayer.sendQueue.playerInfoMap
-                .forEach(Consumer { player: NetworkPlayerInfo ->
-                    val team = checkNotNull(player.playerTeam)
-                    if (stripColor(team.colorPrefix).contains(tag)
-                        || stripColor(team.colorSuffix).contains(tag)
-                    ) {
-                        val name = player.gameProfile.name
+                .forEach(Consumer<NetworkPlayerInfo> { player: NetworkPlayerInfo ->
+                    val team = player.playerTeam
+                    if (team != null) {
+                        if (ColorUtils.stripColor(team.colorPrefix)!!
+                                .contains(tag) || ColorUtils.stripColor(team.colorSuffix)!!
+                                .contains(tag)
+                        ) {
+                            val name = player.gameProfile.name
 
-                        config.addFriend(name, presistent.toString())
-
-                        count.incrementAndGet()
+                            if (!friendManager.isFriend(name)) {
+                                friendManager.addFriend(name)
+                                count.incrementAndGet()
+                                Chat.print("§b[§b!§b]§b ADDED: $name")
+                            }
+                        }
                     }
                 })
 
-            Chat.print("Were added " + EnumChatFormatting.WHITE + count.get() + EnumChatFormatting.GRAY + "§7 players.")
+            chat("§bFoi adicionado " + EnumChatFormatting.DARK_RED + count.get() + EnumChatFormatting.GRAY + " players.")
+            // Leaked by Lope$
         } else {
-            Chat.print(EnumChatFormatting.GRAY.toString() + "Sintax: .addall <tag>")
+            chat("§bUse: addall <tag>")
         }
     }
 }
