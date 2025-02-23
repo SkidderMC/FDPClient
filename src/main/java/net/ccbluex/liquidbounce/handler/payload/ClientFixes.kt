e79c18f7ce8786e24e60f87c15285943727ad197
Davi Lopes <profissionaldavilopes@gmail.com>
2025-02-23 17:09:27 -0300

    fix: lunar brand

--- src/main/java/net/ccbluex/liquidbounce/handler/payload/ClientFixes.kt
+++ src/main/java/net/ccbluex/liquidbounce/handler/payload/ClientFixes.kt
@@ -52,7 +52,7 @@ object ClientFixes : Configurable("Features"), MinecraftInstance, Listenable {
                     packet.data = PacketBuffer(Unpooled.buffer()).writeString(
                         when (possibleBrands.get()) {
                             "Vanilla" -> "vanilla"
-                            "LunarClient" -> "lunarclient:v2.18.2-2452"
+                            "LunarClient" -> "lunarclient:v2.18.9-2508"
                             "OptiFine" -> "optifine"
                             "CheatBreaker" -> "CB"
                             "LabyMod" -> "labymod"
