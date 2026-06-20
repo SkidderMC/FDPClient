3789af2f207171269a2ee82ebd48e2558053c786
Zywl <lifeerwz@gmail.com>
2026-06-20 10:57:16 -0300

    feat: (visual) add NoBooks block-writable option

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/visual/NoBooks.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/visual/NoBooks.kt
@@ -6,15 +6,26 @@
 package net.ccbluex.liquidbounce.features.module.modules.visual
 
 import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.ScreenEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.Category
+import net.minecraft.client.gui.GuiScreenBook
+import net.minecraft.client.gui.inventory.GuiEditSign
 import net.minecraft.network.play.server.S3FPacketCustomPayload
 
 object NoBooks : Module("NoBooks", Category.VISUAL, Category.SubCategory.RENDER_SELF, gameDetecting = false) {
 
+    private val blockWritable by boolean("BlockWritable", false)
+
     val onPacket = handler<PacketEvent> { event ->
         val packet = event.packet
         if (packet is S3FPacketCustomPayload && packet.channelName == "MC|BOpen") event.cancelEvent()
+    }
+
+    val onScreen = handler<ScreenEvent> { event ->
+        if (!blockWritable) return@handler
+        val screen = event.guiScreen
+        if (screen is GuiScreenBook || screen is GuiEditSign) mc.displayGuiScreen(null)
     }
 }
