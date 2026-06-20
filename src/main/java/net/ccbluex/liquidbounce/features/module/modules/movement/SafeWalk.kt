3808597c7000c87e530feaae0e69636b12cef37c
Zywl <lifeerwz@gmail.com>
2026-06-20 00:17:05 -0300

    feat: (movement) add SafeWalk edge-sneak distance and mode

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/SafeWalk.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/SafeWalk.kt
@@ -6,12 +6,17 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
 import net.ccbluex.liquidbounce.event.MoveEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.utils.block.block
+import net.ccbluex.liquidbounce.utils.client.PacketUtils.sendPacket
+import net.ccbluex.liquidbounce.utils.extensions.isNearEdge
 import net.ccbluex.liquidbounce.utils.movement.FallingPlayer
 import net.minecraft.block.BlockAir
+import net.minecraft.client.settings.GameSettings
+import net.minecraft.network.play.client.C0BPacketEntityAction
 import net.minecraft.util.BlockPos
 
 object SafeWalk : Module("SafeWalk", Category.MOVEMENT, Category.SubCategory.MOVEMENT_EXTRAS) {
@@ -19,8 +24,14 @@ object SafeWalk : Module("SafeWalk", Category.MOVEMENT, Category.SubCategory.MOV
     private val airSafe by boolean("AirSafe", false)
     private val maxFallDistanceValue = int("MaxFallDistance", 5, 0..100)
 
+    private val edgeSneak by boolean("EdgeSneak", false)
+    private val edgeSneakDistance by float("EdgeSneakDistance", 0.7F, 0.1F..3F) { edgeSneak }
+    private val edgeSneakMode by choices("EdgeSneakMode", arrayOf("Normal", "Packet"), "Normal") { edgeSneak }
+
     private var lastGroundY: Double? = null
     private var lastCollisionY: Int? = null
+
+    private var edgeSneaking = false
 
     val onMove = handler<MoveEvent> { event ->
         val player = mc.thePlayer ?: return@handler
@@ -37,6 +48,53 @@ object SafeWalk : Module("SafeWalk", Category.MOVEMENT, Category.SubCategory.MOV
             event.isSafeWalk = maxFallDistanceValue.isMinimal()
                     || (lastGroundY != null && lastCollisionY != null
                     && lastGroundY!! - lastCollisionY!! > maxFallDistanceValue.get() + 1)
+        }
+    }
+
+    val onUpdate = handler<UpdateEvent> {
+        if (!edgeSneak) return@handler
+
+        val player = mc.thePlayer ?: return@handler
+        if (player.capabilities.allowFlying || player.capabilities.isFlying
+            || !mc.playerController.gameIsSurvivalOrAdventure()
+        ) {
+            releaseEdgeSneak()
+            return@handler
+        }
+
+        val shouldSneak = player.onGround && player.isNearEdge(edgeSneakDistance)
+
+        if (shouldSneak) {
+            if (!edgeSneaking) {
+                edgeSneaking = true
+                if (edgeSneakMode == "Packet") {
+                    sendPacket(C0BPacketEntityAction(player, C0BPacketEntityAction.Action.START_SNEAKING))
+                }
+            }
+
+            if (edgeSneakMode == "Normal") {
+                mc.gameSettings.keyBindSneak.pressed = true
+            }
+        } else {
+            releaseEdgeSneak()
+        }
+    }
+
+    override fun onDisable() {
+        releaseEdgeSneak()
+    }
+
+    private fun releaseEdgeSneak() {
+        if (!edgeSneaking) return
+        edgeSneaking = false
+
+        val player = mc.thePlayer
+        if (player != null && edgeSneakMode == "Packet") {
+            sendPacket(C0BPacketEntityAction(player, C0BPacketEntityAction.Action.STOP_SNEAKING))
+        }
+
+        if (!GameSettings.isKeyDown(mc.gameSettings.keyBindSneak)) {
+            mc.gameSettings.keyBindSneak.pressed = false
         }
     }
 }
