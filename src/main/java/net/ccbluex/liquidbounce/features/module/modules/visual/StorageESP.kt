8fcf345c4c2acc9db490412401f7ec46ecee1318
Zywl <lifeerwz@gmail.com>
2026-06-19 21:10:31 -0300

    feat: (visual) add StorageESP options
    Tracers, outline, merge-adjacent, color picker and requires-chest-stealer; defaults preserve look.

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/visual/StorageESP.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/visual/StorageESP.kt
@@ -12,16 +12,18 @@ import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.modules.other.ChestAura.clickedTileEntities
+import net.ccbluex.liquidbounce.features.module.modules.other.ChestStealer
 import net.ccbluex.liquidbounce.utils.client.ClientUtils.LOGGER
 import net.ccbluex.liquidbounce.utils.client.ClientUtils.disableFastRender
 import net.ccbluex.liquidbounce.utils.rotation.RotationUtils.isEntityHeightVisible
 import net.ccbluex.liquidbounce.utils.extensions.*
 import net.ccbluex.liquidbounce.utils.block.toVec
-import net.ccbluex.liquidbounce.utils.render.ColorSettingsInteger
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.checkSetupFBO
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.draw2D
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.drawBlockBox
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.drawEntityBox
+import net.ccbluex.liquidbounce.utils.render.RenderUtils.drawFilledBox
+import net.ccbluex.liquidbounce.utils.render.RenderUtils.drawSelectionBoundingBox
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.glColor
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.renderFive
 import net.ccbluex.liquidbounce.utils.render.RenderUtils.renderFour
@@ -32,6 +34,9 @@ import net.ccbluex.liquidbounce.utils.render.RenderUtils.setColor
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher
 import net.minecraft.entity.item.EntityMinecartChest
 import net.minecraft.tileentity.*
+import net.minecraft.util.AxisAlignedBB
+import net.minecraft.util.BlockPos
+import net.minecraft.util.Vec3
 import org.lwjgl.opengl.GL11.*
 import java.awt.Color
 
@@ -42,8 +47,13 @@ object StorageESP : Module("StorageESP", Category.VISUAL, Category.SubCategory.R
     private val glowSettings = GlowRenderSettings(isSupported = { mode == "Glow" }).also { addValues(it.values) }
 
     private val espColorMode by choices("ESP-Color", arrayOf("None", "Custom"), "None")
-    private val espColor = ColorSettingsInteger(this, "ESP")
-    { espColorMode == "Custom" }.with(255, 179, 72)
+    private val boxColor by color("Color", Color(255, 179, 72)) { espColorMode == "Custom" }
+
+    private val outline by boolean("Outline", true)
+    private val mergeAdjacent by boolean("MergeAdjacent", false)
+    private val tracers by boolean("Tracers", false)
+    private val tracerThickness by float("TracerThickness", 2F, 1F..5F) { tracers }
+    private val requiresChestStealer by boolean("RequiresChestStealer", false)
 
     private val renderFilters = RenderFilterSettings(100, 1..500).also { addValues(it.values) }
 
@@ -59,18 +69,14 @@ object StorageESP : Module("StorageESP", Category.VISUAL, Category.SubCategory.R
     private fun getColor(tileEntity: TileEntity): Color? {
         return if (espColorMode == "Custom") {
             when {
-                chest && tileEntity is TileEntityChest && tileEntity !in clickedTileEntities ->
-                    Color(espColor.color().rgb)
-
-                enderChest && tileEntity is TileEntityEnderChest && tileEntity !in clickedTileEntities ->
-                    Color(espColor.color().rgb)
-
-                furnace && tileEntity is TileEntityFurnace -> Color(espColor.color().rgb)
-                dispenser && tileEntity is TileEntityDispenser -> Color(espColor.color().rgb)
-                hopper && tileEntity is TileEntityHopper -> Color(espColor.color().rgb)
-                enchantmentTable && tileEntity is TileEntityEnchantmentTable -> Color(espColor.color().rgb)
-                brewingStand && tileEntity is TileEntityBrewingStand -> Color(espColor.color().rgb)
-                sign && tileEntity is TileEntitySign -> Color(espColor.color().rgb)
+                chest && tileEntity is TileEntityChest && tileEntity !in clickedTileEntities -> boxColor
+                enderChest && tileEntity is TileEntityEnderChest && tileEntity !in clickedTileEntities -> boxColor
+                furnace && tileEntity is TileEntityFurnace -> boxColor
+                dispenser && tileEntity is TileEntityDispenser -> boxColor
+                hopper && tileEntity is TileEntityHopper -> boxColor
+                enchantmentTable && tileEntity is TileEntityEnchantmentTable -> boxColor
+                brewingStand && tileEntity is TileEntityBrewingStand -> boxColor
+                sign && tileEntity is TileEntitySign -> boxColor
                 else -> null
             }
         } else {
@@ -89,6 +95,10 @@ object StorageESP : Module("StorageESP", Category.VISUAL, Category.SubCategory.R
     }
 
     val onRender3D = handler<Render3DEvent> { event ->
+        if (requiresChestStealer && !ChestStealer.state) {
+            return@handler
+        }
+
         try {
             if (mode == "Outline") {
                 disableFastRender()
@@ -99,6 +109,14 @@ object StorageESP : Module("StorageESP", Category.VISUAL, Category.SubCategory.R
 
             mc.gameSettings.gammaSetting = 100000f
 
+            if (tracers) {
+                renderTracers()
+            }
+
+            if (mergeAdjacent && (mode == "Box" || mode == "OtherBox")) {
+                renderMergedBoxes()
+            }
+
             for (tileEntity in mc.theWorld.loadedTileEntityList) {
                 val color = getColor(tileEntity) ?: continue
 
@@ -106,8 +124,14 @@ object StorageESP : Module("StorageESP", Category.VISUAL, Category.SubCategory.R
                     continue
                 }
 
+                val withOutline = outline && mode != "OtherBox"
+
+                if (mergeAdjacent && (mode == "Box" || mode == "OtherBox")) {
+                    continue
+                }
+
                 if (!(tileEntity is TileEntityChest || tileEntity is TileEntityEnderChest)) {
-                    drawBlockBox(tileEntity.pos, color, mode != "OtherBox")
+                    drawBlockBox(tileEntity.pos, color, withOutline)
 
                     if (tileEntity !is TileEntityEnchantmentTable) {
                         continue
@@ -115,7 +139,7 @@ object StorageESP : Module("StorageESP", Category.VISUAL, Category.SubCategory.R
                 }
 
                 when (mode) {
-                    "OtherBox", "Box" -> drawBlockBox(tileEntity.pos, color, mode != "OtherBox")
+                    "OtherBox", "Box" -> drawBlockBox(tileEntity.pos, color, withOutline)
                     "2D" -> draw2D(tileEntity.pos, color.rgb, Color.BLACK.rgb)
                     "Outline" -> renderOutline(color) {
                         TileEntityRendererDispatcher.instance.renderTileEntity(tileEntity, event.partialTicks, -1)
@@ -134,7 +158,7 @@ object StorageESP : Module("StorageESP", Category.VISUAL, Category.SubCategory.R
                 val minecartColor = Color(0, 66, 255)
 
                 when (mode) {
-                    "OtherBox", "Box" -> drawEntityBox(entity, minecartColor, mode != "OtherBox")
+                    "OtherBox", "Box" -> drawEntityBox(entity, minecartColor, outline && mode != "OtherBox")
                     "2D" -> draw2D(entity.position, minecartColor.rgb, Color.BLACK.rgb)
                     "Outline" -> renderWithEntityShadowsDisabled {
                         renderOutline(minecartColor) {
@@ -252,5 +276,152 @@ object StorageESP : Module("StorageESP", Category.VISUAL, Category.SubCategory.R
         } finally {
             mc.gameSettings.entityShadows = entityShadow
         }
+    }
+
+    private fun renderTracers() {
+        val player = mc.thePlayer ?: return
+        val world = mc.theWorld ?: return
+
+        val partial = mc.timer.renderPartialTicks
+        val yaw = (player.prevRotationYaw..player.rotationYaw).lerpWith(partial)
+        val pitch = (player.prevRotationPitch..player.rotationPitch).lerpWith(partial)
+        val eyeVector = Vec3(0.0, 0.0, 1.0).rotatePitch(-pitch.toRadians()).rotateYaw(-yaw.toRadians())
+        val renderPos = mc.renderManager.renderPos
+
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable(GL_BLEND)
+        glEnable(GL_LINE_SMOOTH)
+        glLineWidth(tracerThickness)
+        glDisable(GL_TEXTURE_2D)
+        glDisable(GL_DEPTH_TEST)
+        glDepthMask(false)
+        glBegin(GL_LINES)
+
+        for (tileEntity in world.loadedTileEntityList) {
+            val color = getColor(tileEntity) ?: continue
+            if (!shouldRender(tileEntity)) {
+                continue
+            }
+
+            val (x, y, z) = tileEntity.pos.toVec() - renderPos
+            glColor(color)
+            glVertex3d(eyeVector.xCoord, player.getEyeHeight() + eyeVector.yCoord, eyeVector.zCoord)
+            glVertex3d(x + 0.5, y + 0.5, z + 0.5)
+        }
+
+        for (entity in world.loadedEntityList) {
+            if (entity !is EntityMinecartChest || !shouldRender(entity)) {
+                continue
+            }
+
+            val (x, y, z) = entity.interpolatedPosition(entity.lastTickPos) - renderPos
+            glColor(Color(0, 66, 255))
+            glVertex3d(eyeVector.xCoord, player.getEyeHeight() + eyeVector.yCoord, eyeVector.zCoord)
+            glVertex3d(x, y, z)
+        }
+
+        glEnd()
+        glEnable(GL_TEXTURE_2D)
+        glDisable(GL_LINE_SMOOTH)
+        glEnable(GL_DEPTH_TEST)
+        glDepthMask(true)
+        glDisable(GL_BLEND)
+        glColor4f(1f, 1f, 1f, 1f)
+    }
+
+    private fun renderMergedBoxes() {
+        val world = mc.theWorld ?: return
+        val renderPos = mc.renderManager.renderPos
+        val withOutline = outline && mode != "OtherBox"
+
+        val byColor = HashMap<Int, MutableSet<BlockPos>>()
+        for (tileEntity in world.loadedTileEntityList) {
+            val color = getColor(tileEntity) ?: continue
+            if (!shouldRender(tileEntity)) {
+                continue
+            }
+            byColor.getOrPut(color.rgb) { HashSet() }.add(tileEntity.pos)
+        }
+
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable(GL_BLEND)
+        glDisable(GL_TEXTURE_2D)
+        glDisable(GL_DEPTH_TEST)
+        glDepthMask(false)
+
+        for ((rgb, positions) in byColor) {
+            val color = Color(rgb, true)
+
+            for (cluster in mergeClusters(positions)) {
+                var minX = Double.MAX_VALUE
+                var minY = Double.MAX_VALUE
+                var minZ = Double.MAX_VALUE
+                var maxX = -Double.MAX_VALUE
+                var maxY = -Double.MAX_VALUE
+                var maxZ = -Double.MAX_VALUE
+
+                for (pos in cluster) {
+                    val (x, y, z) = pos.toVec() - renderPos
+                    if (x < minX) minX = x
+                    if (y < minY) minY = y
+                    if (z < minZ) minZ = z
+                    if (x + 1.0 > maxX) maxX = x + 1.0
+                    if (y + 1.0 > maxY) maxY = y + 1.0
+                    if (z + 1.0 > maxZ) maxZ = z + 1.0
+                }
+
+                val box = AxisAlignedBB.fromBounds(minX, minY, minZ, maxX, maxY, maxZ)
+
+                glColor(color.red, color.green, color.blue, if (color.alpha != 255) color.alpha else if (withOutline) 26 else 35)
+                drawFilledBox(box)
+
+                if (withOutline) {
+                    glLineWidth(1f)
+                    glEnable(GL_LINE_SMOOTH)
+                    glColor(color)
+                    drawSelectionBoundingBox(box)
+                }
+            }
+        }
+
+        glEnable(GL_TEXTURE_2D)
+        glEnable(GL_DEPTH_TEST)
+        glDepthMask(true)
+        glDisable(GL_BLEND)
+        glColor4f(1f, 1f, 1f, 1f)
+    }
+
+    private fun mergeClusters(positions: Set<BlockPos>): List<List<BlockPos>> {
+        val remaining = HashSet(positions)
+        val clusters = mutableListOf<List<BlockPos>>()
+        val neighbors = arrayOf(
+            BlockPos(1, 0, 0), BlockPos(-1, 0, 0),
+            BlockPos(0, 1, 0), BlockPos(0, -1, 0),
+            BlockPos(0, 0, 1), BlockPos(0, 0, -1),
+        )
+
+        while (remaining.isNotEmpty()) {
+            val start = remaining.first()
+            remaining.remove(start)
+
+            val cluster = mutableListOf(start)
+            val queue = ArrayDeque<BlockPos>()
+            queue.add(start)
+
+            while (queue.isNotEmpty()) {
+                val current = queue.removeFirst()
+                for (offset in neighbors) {
+                    val neighbor = current.add(offset.x, offset.y, offset.z)
+                    if (remaining.remove(neighbor)) {
+                        cluster.add(neighbor)
+                        queue.add(neighbor)
+                    }
+                }
+            }
+
+            clusters.add(cluster)
+        }
+
+        return clusters
     }
 }
