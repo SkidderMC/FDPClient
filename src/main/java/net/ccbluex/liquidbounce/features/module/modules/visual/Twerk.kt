12d050db8748e93a593192f75c089525b0dae64e
Zywl <lifeerwz@gmail.com>
2026-06-21 02:31:13 -0300

    feat: (visual) port Twerk and Vomit cosmetic modules (1.8.9)

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/visual/Twerk.kt
@@ -0,0 +1,29 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/SkidderMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.visual
+
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.utils.client.PacketUtils.sendPacket
+import net.minecraft.network.play.client.C0BPacketEntityAction
+
+object Twerk : Module("Twerk", Category.VISUAL, Category.SubCategory.RENDER_SELF, gameDetecting = false) {
+
+    private val onlyOnGround by boolean("OnlyOnGround", true)
+
+    val onUpdate = handler<UpdateEvent> {
+        val player = mc.thePlayer ?: return@handler
+
+        if (onlyOnGround && !player.onGround) {
+            return@handler
+        }
+
+        sendPacket(C0BPacketEntityAction(player, C0BPacketEntityAction.Action.START_SNEAKING))
+        sendPacket(C0BPacketEntityAction(player, C0BPacketEntityAction.Action.STOP_SNEAKING))
+    }
+}
---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/visual/Vomit.kt
@@ -0,0 +1,44 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/SkidderMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.visual
+
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
+import net.minecraft.util.EnumParticleTypes
+
+object Vomit : Module("Vomit", Category.VISUAL, Category.SubCategory.RENDER_SELF, gameDetecting = false) {
+
+    private val amount by int("Amount", 8, 1..40)
+    private val onlySneaking by boolean("OnlySneaking", false)
+
+    val onUpdate = handler<UpdateEvent> {
+        val player = mc.thePlayer ?: return@handler
+        val world = mc.theWorld ?: return@handler
+
+        if (onlySneaking && !player.isSneaking) {
+            return@handler
+        }
+
+        val look = player.getLook(1f)
+        val baseX = player.posX + look.xCoord * 0.5
+        val baseY = player.posY + player.getEyeHeight() - 0.25
+        val baseZ = player.posZ + look.zCoord * 0.5
+
+        repeat(amount) {
+            world.spawnParticle(
+                EnumParticleTypes.SLIME,
+                baseX + (Math.random() - 0.5) * 0.3,
+                baseY + (Math.random() - 0.5) * 0.2,
+                baseZ + (Math.random() - 0.5) * 0.3,
+                look.xCoord * 0.35,
+                -0.15,
+                look.zCoord * 0.35
+            )
+        }
+    }
+}
